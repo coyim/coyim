c13a2b466d804f71a4f8efc8560d145a076cb419
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-09-21 13:21:42 -0500

    Add sample roster with fixed data

--- gui.go
+++ gui.go
@@ -33,6 +33,41 @@ func NewGTK() UI {
 		window: gtk.NewWindow(gtk.WINDOW_TOPLEVEL),
 	}
 
+	scrolledwin := gtk.NewScrolledWindow(nil, nil)
+	scrolledwin.SetPolicy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+
+	//Add a list widget
+	rosterList := gtk.NewTreeView()
+	rosterList.SetHeadersVisible(false)
+
+	rosterList.AppendColumn(
+		gtk.NewTreeViewColumnWithAttributes("user",
+			gtk.NewCellRendererText(), "text", 0),
+	)
+
+	rosterModel := gtk.NewListStore(
+		gtk.TYPE_STRING, // user
+		gtk.TYPE_INT,    // id
+	)
+
+	iter := &gtk.TreeIter{}
+	rosterModel.Append(iter)
+	rosterModel.Set(iter,
+		0, "alice@riseup.net",
+		1, 111,
+	)
+
+	rosterModel.Append(iter)
+	rosterModel.Set(iter,
+		0, "bob@riseup.net",
+		1, 222,
+	)
+
+	rosterList.SetModel(rosterModel)
+
+	scrolledwin.Add(rosterList)
+	ui.window.Add(scrolledwin)
+
 	ui.window.SetTitle("Coy")
 	ui.window.Connect("destroy", gtk.MainQuit)
 	ui.window.SetSizeRequest(200, 600)

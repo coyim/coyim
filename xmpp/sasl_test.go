176a6f58234175eed5afbc6c81424825acec36fe
Ola Bini <ola@olabini.se>
2015-10-07 11:25:46 -0500

    Add tests for SASL functionality

---
+++ xmpp/sasl_test.go
@@ -0,0 +1,72 @@
+package xmpp
+
+import (
+	"encoding/xml"
+
+	. "gopkg.in/check.v1"
+)
+
+type SaslXmppSuite struct{}
+
+var _ = Suite(&SaslXmppSuite{})
+
+func (s *SaslXmppSuite) Test_authenticate_failsIfPlainIsNotAnOption(c *C) {
+	conn := Conn{}
+	stream := streamFeatures{}
+
+	e := conn.authenticate(stream, "", "")
+
+	c.Assert(e.Error(), Equals, "xmpp: PLAIN authentication is not an option")
+}
+
+func (s *SaslXmppSuite) Test_authenticate_authenticatesWithUsernameAndPassword(c *C) {
+	out := &mockConnIOReaderWriter{}
+	mockIn := mockConnIOReaderWriter{read: []byte("<sasl:success xmlns:sasl='urn:ietf:params:xml:ns:xmpp-sasl'></sasl:success>")}
+	conn := Conn{
+		rawOut: out,
+		in:     xml.NewDecoder(mockIn),
+	}
+	stream := streamFeatures{
+		Mechanisms: saslMechanisms{
+			Mechanism: []string{"FOO", "PLAIN"},
+		},
+	}
+
+	e := conn.authenticate(stream, "foo", "bar")
+	c.Assert(e, IsNil)
+	c.Assert(string(out.write), Equals, "<auth xmlns='urn:ietf:params:xml:ns:xmpp-sasl' mechanism='PLAIN'>AGZvbwBiYXI=</auth>\n")
+}
+
+func (s *SaslXmppSuite) Test_authenticate_handlesFailure(c *C) {
+	out := &mockConnIOReaderWriter{}
+	mockIn := mockConnIOReaderWriter{read: []byte("<sasl:failure xmlns:sasl='urn:ietf:params:xml:ns:xmpp-sasl'><foobar></foobar></sasl:failure>")}
+	conn := Conn{
+		rawOut: out,
+		in:     xml.NewDecoder(mockIn),
+	}
+	stream := streamFeatures{
+		Mechanisms: saslMechanisms{
+			Mechanism: []string{"FOO", "PLAIN"},
+		},
+	}
+
+	e := conn.authenticate(stream, "foo", "bar")
+	c.Assert(e.Error(), Equals, "xmpp: authentication failure: foobar")
+}
+
+func (s *SaslXmppSuite) Test_authenticate_handlesWrongResponses(c *C) {
+	out := &mockConnIOReaderWriter{}
+	mockIn := mockConnIOReaderWriter{read: []byte("<sasl:something xmlns:sasl='urn:ietf:params:xml:ns:xmpp-sasl'></sasl:something>")}
+	conn := Conn{
+		rawOut: out,
+		in:     xml.NewDecoder(mockIn),
+	}
+	stream := streamFeatures{
+		Mechanisms: saslMechanisms{
+			Mechanism: []string{"FOO", "PLAIN"},
+		},
+	}
+
+	e := conn.authenticate(stream, "foo", "bar")
+	c.Assert(e.Error(), Equals, "expected <success> or <failure>, got <> in ")
+}

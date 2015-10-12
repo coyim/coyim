387233006c47dcad2c2f0a81bd13f276f1139d0c
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-10-11 23:34:18 -0500

    Simplify installation instruction for Tails so it can be copied and pasted

--- install_coyim_tails.sh
+++ install_coyim_tails.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
- 
+
+set -xe
+
 sudo apt-get update && sudo apt-get -y --force-yes install -t testing golang
-sudo apt-get install -y libgtk2.0-dev
-sudo apt-get install -y gcc
+sudo apt-get install -y gcc libgtk2.0-dev
 
-cd $HOME/Persistent && mkdir go
 export GOPATH=$HOME/Persistent/go
-cd $GOPATH && mkdir src 
-cd src
-mkdir golang.org && cd golang.org && mkdir x && cd x
-git clone https://github.com/golang/net.git
-git clone https://github.com/golang/crypto.git
+mkdir -p $GOPATH
 
-go get -v github.com/twstrike/go-gtk/gdk
-go get -v github.com/twstrike/go-gtk/glib
-go get -v github.com/twstrike/go-gtk/gtk
-go get -v github.com/twstrike/coyim
+git clone https://github.com/golang/net.git $GOPATH/src/golang.org/x/net
+git clone https://github.com/golang/crypto.git $GOPATH/src/golang.org/x/crypto
 
-cd $GOPATH/src/github.com/twstrike/coyim
-go build -tags nocli
+go get -u -v github.com/twstrike/go-gtk/gdk
+go get -u -v github.com/twstrike/go-gtk/glib
+go get -u -v github.com/twstrike/go-gtk/gtk
+go get -u -v -tags nocli github.com/twstrike/coyim
 
-./coyim
+$GOPATH/bin/coyim

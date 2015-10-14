2d1759b3afe85c6ba8251801d19f76789ed14005
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-10-14 16:22:03 -0500

    Add support to build from a path provided

--- build/test.sh
+++ build/test.sh
@@ -6,7 +6,11 @@ export PATH="/root/go/bin:$GOPATH/bin:$PATH"
 
 shasum /root/go/bin/*
 
-/root/go/bin/go get -d $GO_PKG
+# Use source code from current volume
+# /root/go/bin/go get -d $GO_PKG
+mkdir -p $(echo "${GOPATH}/src/${GO_PKG}" | rev | cut -d '/' -f 2- | rev) &&\
+  ln -s /src "${GOPATH}/src/${GO_PKG}"
+
 cd $GOPATH/src && source /root/setup-reproducible
 
 cd $GO_PKG

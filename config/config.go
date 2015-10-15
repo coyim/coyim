799671dd89c40ba54ee93b15346bc4078e77f4e3
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-10-15 01:12:31 -0500

    Add support to socks5+unix proxy - #71

--- config/config.go
+++ config/config.go
@@ -23,6 +23,23 @@ var (
 	CreateAccount        *bool   = flag.Bool("create", false, "If true, attempt to create account")
 )
 
+func init() {
+	proxy.RegisterDialerType("socks5+unix", func(u *url.URL, d proxy.Dialer) (proxy.Dialer, error) {
+		var auth *proxy.Auth
+		if u.User != nil {
+			auth = &proxy.Auth{
+				User: u.User.Username(),
+			}
+
+			if p, ok := u.User.Password(); ok {
+				auth.Password = p
+			}
+		}
+
+		return proxy.SOCKS5("unix", u.Path, auth, d)
+	})
+}
+
 type Config struct {
 	Filename string `json:"-"`
 

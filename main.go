b3c711ce057603197f8ecef2008695f6d165533e
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-10-15 15:18:45 -0500

    Add log and debug mode

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"flag"
+	"log"
 	"os"
 
 	"github.com/twstrike/coyim/client"
 	"github.com/twstrike/coyim/config"
 )
+
+func init() {
+	if !*config.DebugFlag {
+		return
+	}
+
+	flags := log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile
+	log.SetFlags(flags)
+
+	devNull, _ := os.OpenFile(os.DevNull, os.O_RDWR, 0777)
+	log.SetOutput(devNull)
+}
 
 func main() {
 	flag.Parse()

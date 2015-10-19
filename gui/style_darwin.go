ebd37519cf668ccae7445a349e0c91665c4242d5
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-10-19 12:20:10 -0500

    Refactor CSS

--- gui/style_darwin.go
+++ gui/style_darwin.go
@@ -8,12 +8,7 @@ import (
 
 // This is how the gtkrc file should look like on Mac OS
 // https://git.gnome.org/browse/gtk+/commit/?id=4ff709c24b8d4b3e26b3d513fde0676e9c43f897
-func (u *gtkUI) applyStyle() {
-	style := `
-/*
-	gtk-enable-mnemonics = 0
-*/
-
+var style = `
 @binding-set gtk-mac-alt-arrows
 {
   bind "<alt>Right"           { "move-cursor" (words, 1, 0) };
@@ -26,8 +21,33 @@ func (u *gtkUI) applyStyle() {
   bind "<shift><alt>KP_Left"  { "move-cursor" (words, -1, 1) };
 }
 
+@binding-set gtk-mac-cmd-arrows
+{
+  bind "<meta>Left" { "move-cursor" (paragraph-ends, -1, 0) };
+  bind "<meta>KP_Left" { "move-cursor" (paragraph-ends, -1, 0) };
+  bind "<shift><meta>Left" { "move-cursor" (paragraph-ends, -1, 1) };
+  bind "<shift><meta>KP_Left" { "move-cursor" (paragraph-ends, -1, 1) };
+  bind "<meta>Right" { "move-cursor" (paragraph-ends, 1, 0) };
+  bind "<meta>KP_Right" { "move-cursor" (paragraph-ends, 1, 0) };
+  bind "<shift><meta>Right" { "move-cursor" (paragraph-ends, 1, 1) };
+  bind "<shift><meta>KP_Right" { "move-cursor" (paragraph-ends, 1, 1) };
+}
+
+@binding-set gtk-mac-emacs-like
+{
+  bind "<ctrl>a" { "move-cursor" (paragraph-ends, -1, 0) };
+  bind "<shift><ctrl>a" { "move-cursor" (paragraph-ends, -1, 1) };
+  bind "<ctrl>e" { "move-cursor" (paragraph-ends, 1, 0) };
+  bind "<shift><ctrl>e" { "move-cursor" (paragraph-ends, 1, 1) };
+
+  bind "<ctrl>b" { "move-cursor" (logical-positions, -1, 0) };
+  bind "<shift><ctrl>b" { "move-cursor" (logical-positions, -1, 1) };
+  bind "<ctrl>f" { "move-cursor" (logical-positions, 1, 0) };
+  bind "<shift><ctrl>f" { "move-cursor" (logical-positions, 1, 1) };
+}
+
 GtkTextView, GtkLabel, GtkEntry {
-	gtk-key-bindings: gtk-mac-alt-arrows
+	gtk-key-bindings: gtk-mac-alt-arrows, gtk-mac-cmd-arrows, gtk-mac-emacs-like;
 }
 
 @binding-set gtk-mac-alt-delete
@@ -35,10 +55,6 @@ GtkTextView, GtkLabel, GtkEntry {
   bind "<alt>Delete" { "delete-from-cursor" (word-ends, 1) };
   bind "<alt>KP_Delete" { "delete-from-cursor" (word-ends, 1) };
   bind "<alt>BackSpace" { "delete-from-cursor" (word-ends, -1) };
-}
-
-GtkTextView, GtkEntry {
-	gtk-key-bindings: gtk-mac-alt-delete
 }
 
 @binding-set gtk-mac-cmd-c
@@ -52,7 +68,7 @@ GtkTextView, GtkEntry {
 }
 
 GtkTextView, GtkEntry {
-	gtk-key-bindings: gtk-mac-cmd-c
+	gtk-key-bindings: gtk-mac-alt-delete, gtk-mac-cmd-c;
 }
 
 @binding-set gtk-mac-text-view
@@ -100,39 +116,6 @@ GtkEntry {
 	gtk-key-bindings: gtk-mac-entry
 }
 
-@binding-set gtk-mac-cmd-arrows
-{
-  bind "<meta>Left" { "move-cursor" (paragraph-ends, -1, 0) };
-  bind "<meta>KP_Left" { "move-cursor" (paragraph-ends, -1, 0) };
-  bind "<shift><meta>Left" { "move-cursor" (paragraph-ends, -1, 1) };
-  bind "<shift><meta>KP_Left" { "move-cursor" (paragraph-ends, -1, 1) };
-  bind "<meta>Right" { "move-cursor" (paragraph-ends, 1, 0) };
-  bind "<meta>KP_Right" { "move-cursor" (paragraph-ends, 1, 0) };
-  bind "<shift><meta>Right" { "move-cursor" (paragraph-ends, 1, 1) };
-  bind "<shift><meta>KP_Right" { "move-cursor" (paragraph-ends, 1, 1) };
-}
-
-GtkTextView, GtkLabel, GtkEntry {
-	gtk-key-bindings: gtk-mac-cmd-arrows
-}
-
-@binding-set gtk-mac-emacs-like
-{
-  bind "<ctrl>a" { "move-cursor" (paragraph-ends, -1, 0) };
-  bind "<shift><ctrl>a" { "move-cursor" (paragraph-ends, -1, 1) };
-  bind "<ctrl>e" { "move-cursor" (paragraph-ends, 1, 0) };
-  bind "<shift><ctrl>e" { "move-cursor" (paragraph-ends, 1, 1) };
-
-  bind "<ctrl>b" { "move-cursor" (logical-positions, -1, 0) };
-  bind "<shift><ctrl>b" { "move-cursor" (logical-positions, -1, 1) };
-  bind "<ctrl>f" { "move-cursor" (logical-positions, 1, 0) };
-  bind "<shift><ctrl>f" { "move-cursor" (logical-positions, 1, 1) };
-}
-
-GtkTextView, GtkLabel, GtkEntry {
-	gtk-key-bindings: gtk-mac-emacs-like
-}
-
 @binding-set gtk-mac-file-chooser
 {
   bind "<meta>v" { "location-popup-on-paste" () };
@@ -174,8 +157,10 @@ GtkTreeView {
 GtkIconView {
 	gtk-key-bindings: gtk-mac-icon-view
 }
-	`
 
+`
+
+func (u *gtkUI) applyStyle() {
 	css, err := gtk.CssProviderGetDefault()
 	if err != nil {
 		log.Println(err)

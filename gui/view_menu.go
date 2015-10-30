303b6959264058f30ac0e934d5dfdd9121f1bae9
Fan Jiang <fanjiang@thoughtworks.com>
2015-10-30 11:17:49 -0500

    createViewMenu can be removed

--- gui/view_menu.go
+++ gui/view_menu.go
@@ -4,34 +4,11 @@ import (
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/twstrike/coyim/config"
-	"github.com/twstrike/coyim/i18n"
 )
 
 type viewMenu struct {
 	merge   *gtk.CheckMenuItem
 	offline *gtk.CheckMenuItem
-}
-
-func (u *gtkUI) createViewMenu(bar *gtk.MenuBar) {
-	u.viewMenu = new(viewMenu)
-
-	viewMenu, _ := gtk.MenuItemNewWithMnemonic(i18n.Local("_View"))
-	bar.Append(viewMenu)
-	viewSubmenu, _ := gtk.MenuNew()
-	viewMenu.SetSubmenu(viewSubmenu)
-
-	checkItemMerge, _ := gtk.CheckMenuItemNewWithMnemonic(i18n.Local("_Merge Accounts"))
-	u.viewMenu.merge = checkItemMerge
-	viewSubmenu.Append(checkItemMerge)
-	checkItemMerge.Connect("toggled", u.toggleMergeAccounts)
-
-	checkItemShowOffline, _ := gtk.CheckMenuItemNewWithMnemonic(i18n.Local("Show _Offline Contacts"))
-	u.viewMenu.offline = checkItemShowOffline
-	viewSubmenu.Append(checkItemShowOffline)
-	checkItemShowOffline.Connect("toggled", u.toggleShowOffline)
-
-	u.displaySettings.defaultSettingsOn(&checkItemMerge.MenuItem.Bin.Container.Widget)
-	u.displaySettings.defaultSettingsOn(&checkItemShowOffline.MenuItem.Bin.Container.Widget)
 }
 
 func (v *viewMenu) setFromConfig(c *config.Accounts) {

c7a79f07cf40c52ad48bde6b87bbf0974c07f638
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-11-13 17:34:54 -0200

    Reformat files

--- gui/account_details_definition.go
+++ gui/account_details_definition.go
@@ -7,64 +7,64 @@ func (w accountDetailsDefinition) getDefinition() string {
 	return `
 <interface>
   <object class="GtkDialog" id="AccountDetailsDialog">
-	<property name="title">$title</property>
+    <property name="title">$title</property>
     <child internal-child="vbox">
       <object class="GtkBox" id="Vbox">
         <property name="homogeneous">false</property>
-	    <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
+        <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
         <child>
           <object class="GtkLabel">
-	        <property name="label">$accountMessage</property>
-	      </object>
-	      <packing>
+            <property name="label">$accountMessage</property>
+          </object>
+          <packing>
             <property name="expand">false</property>
-	        <property name="fill">true</property>
-	        <property name="position">0</property>
-	      </packing>
-	    </child>
+            <property name="fill">true</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
         <child>
           <object class="GtkEntry" id="account">
-	        <property name="has-focus">true</property>
-	        <signal name="activate" handler="on_save_signal" />
-	      </object>
-	      <packing>
+            <property name="has-focus">true</property>
+            <signal name="activate" handler="on_save_signal" />
+          </object>
+          <packing>
             <property name="expand">false</property>
-	        <property name="fill">true</property>
-	        <property name="position">1</property>
-	      </packing>
-	    </child>
+            <property name="fill">true</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
         <child>
           <object class="GtkLabel">
-	        <property name="label">$pswMessage</property>
-	      </object>
-	      <packing>
+            <property name="label">$pswMessage</property>
+          </object>
+          <packing>
             <property name="expand">false</property>
-	        <property name="fill">true</property>
-	        <property name="position">2</property>
-	      </packing>
-	    </child>
+            <property name="fill">true</property>
+            <property name="position">2</property>
+          </packing>
+        </child>
         <child>
           <object class="GtkEntry" id="password">
             <property name="visibility">false</property>
             <signal name="activate" handler="on_save_signal" />
-	      </object>
-	      <packing>
+          </object>
+          <packing>
             <property name="expand">false</property>
-	        <property name="fill">true</property>
-	        <property name="position">3</property>
-	      </packing>
-	    </child>
-	    <child>
+            <property name="fill">true</property>
+            <property name="position">3</property>
+          </packing>
+        </child>
+        <child>
           <object class="GtkButton" id="save">
-	        <property name="label">$saveLabel</property>
+            <property name="label">$saveLabel</property>
             <signal name="clicked" handler="on_save_signal"/>
-	      </object>
-	      <packing>
+          </object>
+          <packing>
             <property name="expand">false</property>
-	        <property name="fill">true</property>
-	        <property name="position">4</property>
-	      </packing>
-	    </child>
+            <property name="fill">true</property>
+            <property name="position">4</property>
+          </packing>
+        </child>
       </object>
     </child>
   </object>

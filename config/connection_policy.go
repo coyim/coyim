447be57979d0cc2283a84b54e6d519db0271121a
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-11-13 17:45:13 -0200

    Typo

--- config/connection_policy.go
+++ config/connection_policy.go
@@ -182,7 +182,7 @@ func buildInOutLogs(rawLog io.Writer) (io.Writer, io.Writer) {
 	return &in, &out
 }
 
-// Connect to the serverand authenticates with the password
+// Connect to the server and authenticates with the password
 //TODO: it is weird that conf.Password is ignored and password is used
 func (p *ConnectionPolicy) Connect(password string, conf *Account) (*xmpp.Conn, error) {
 	dialer, err := p.buildDialerFor(conf)
@@ -208,7 +208,8 @@ func (p *ConnectionPolicy) RegisterAccount(createCallback xmpp.FormCallback, con
 func newTLSConfig() *tls.Config {
 	return &tls.Config{
 		MinVersion: tls.VersionTLS10,
-		CipherSuites: []uint16{tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,

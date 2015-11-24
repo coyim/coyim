15487a0cfb9c1b92df6ac32370ac3adc8350458d
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-11-24 15:15:06 -0500

    Add missing file

---
+++ cli/commands.go
@@ -0,0 +1,29 @@
+package cli
+
+import "github.com/twstrike/coyim/client"
+
+func (c *cliUI) ExecuteCmd(comm interface{}) {
+	c.commands <- comm
+}
+
+func (c *cliUI) watchClientCommands() {
+	for command := range c.commands {
+		switch comm := command.(type) {
+		case client.AuthorizeFingerprintCmd:
+			account := comm.Account
+			uid := comm.Peer
+			fpr := comm.Fingerprint
+
+			//TODO: it could be a different pointer,
+			//find the account by ID()
+			account.AuthorizeFingerprint(uid, fpr)
+			c.ExecuteCmd(client.SaveApplicationConfigCmd{})
+		case client.SaveInstanceTagCmd:
+			account := comm.Account
+			account.InstanceTag = comm.InstanceTag
+			c.ExecuteCmd(client.SaveApplicationConfigCmd{})
+		case client.SaveApplicationConfigCmd:
+			c.SaveConf()
+		}
+	}
+}

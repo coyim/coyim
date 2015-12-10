0a856493587ff31909e83c3c225450fbc1be8bd8
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-12-10 15:19:09 -0500

    Regenerate UI definition
    @tdruiva was testing the doctor cmd

--- gui/definitions/feedback.go
+++ gui/definitions/feedback.go
@@ -14,7 +14,7 @@ func (*defFeedback) String() string {
     <property name="title" translatable="yes">We would like to receive your feedback</property>
     <property name="modal">true</property>
     <property name="secondary-text" translatable="yes">Please, tell us how is going for you to use CoyIM.&#xA;Only in this way we can have a better tool to keep your conversation private.</property>
-    <property name="text" translatable="yes">coyim@thoughtworks.com</property>
+    <property name="text" translatable="yes">https://coy.im/feedback</property>
     <property name="buttons">GTK_BUTTONS_CLOSE</property>
   </object>
 </interface>

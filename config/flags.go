20e68ce6f7d486ea8549bb19f76b92cf82054e9d
brl <bruce@subgraph.com>
2016-02-08 23:59:08 -0500

    Added flag -single to enable single window UI layout

--- config/flags.go
+++ config/flags.go
@@ -4,9 +4,10 @@ import "flag"
 
 // These flags represent all the available command line flags
 var (
-	ConfigFile    = flag.String("config-file", "", "Location of the config file")
-	CreateAccount = flag.Bool("create", false, "If true, attempt to create account")
-	DebugFlag     = flag.Bool("debug", false, "Enable debug logging")
-	AccountFlag   = flag.String("account", "", "The account the CLI should connect to, if more than one is configured")
-	MultiFlag     = flag.Bool("multi", false, "If true, will not try to unify the windows, but create separate instances")
+	ConfigFile       = flag.String("config-file", "", "Location of the config file")
+	CreateAccount    = flag.Bool("create", false, "If true, attempt to create account")
+	DebugFlag        = flag.Bool("debug", false, "Enable debug logging")
+	AccountFlag      = flag.String("account", "", "The account the CLI should connect to, if more than one is configured")
+	MultiFlag        = flag.Bool("multi", false, "If true, will not try to unify the windows, but create separate instances")
+	SingleWindowFlag = flag.Bool("single", false, "Enable single window UI layout")
 )

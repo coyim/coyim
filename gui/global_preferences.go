4b60462f25f4441b143ceb37804356931f57c8cb
Ola Bini <ola@olabini.se>
2016-04-08 10:18:56 -0500

    Add basics for new preference pane

---
+++ gui/global_preferences.go
@@ -0,0 +1,10 @@
+package gui
+
+// Preference pane for things like:
+// - notification settings
+// - single window
+// - /me formatting
+// - RawLogFile
+// - NotifyCommand
+// - IdleSecondsBeforeNotification
+// - Bell

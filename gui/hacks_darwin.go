0bbd4662865bf6e8ff0b8feb6cad77e565713d46
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-08-03 11:55:16 -0500

    Check for errors

--- gui/hacks_darwin.go
+++ gui/hacks_darwin.go
@@ -8,12 +8,18 @@ func applyHacks() {
 
 // See #189
 func fixPopupMenusWithoutFocus() {
-	prov, _ := g.gtk.CssProviderNew()
+	prov, err := g.gtk.CssProviderNew()
+	if err != nil {
+		return
+	}
 	prov.LoadFromData("GtkMenu { margin: 0; }")
 
 	// It must be added to the screen.
 	// Adding to the main window has the same effect as putting the CSS in
 	// gtk-keys.css (it is overwritten by the theme)
-	screen, _ := g.gdk.ScreenGetDefault()
+	screen, err := g.gdk.ScreenGetDefault()
+	if err != nil {
+		return
+	}
 	g.gtk.AddProviderForScreen(screen, prov, uint(gtki.STYLE_PROVIDER_PRIORITY_USER))
 }

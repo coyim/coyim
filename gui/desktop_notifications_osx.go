c717ba19c2ee2a169a314e7479a7aa52e68a3056
Ola Bini <ola@olabini.se>
2016-08-20 16:41:58 -0500

    OK, add some more usethingies for notification on darwin

--- gui/desktop_notifications_osx.go
+++ gui/desktop_notifications_osx.go
@@ -2,7 +2,11 @@
 
 package gui
 
-import "github.com/twstrike/coyim/gui/settings"
+import (
+	"log"
+
+	"github.com/twstrike/coyim/gui/settings"
+)
 import "github.com/twstrike/gosx-notifier"
 
 type desktopNotifications struct {
@@ -30,6 +34,40 @@ func (dn *desktopNotifications) show(jid, from, message string) error {
 	if dn.notificationStyle == "off" {
 		return nil
 	}
-	gosxnotifier.NewNotification("Check your Apple Stock!")
+	note := gosxnotifier.NewNotification("Check your Apple Stock!")
+
+	// //Optionally, set a title
+	// note.Title = "It's money making time 💰"
+
+	// //Optionally, set a subtitle
+	// note.Subtitle = "My subtitle"
+
+	// //Optionally, set a sound from a predefined set.
+	// note.Sound = gosxnotifier.Basso
+
+	// //Optionally, set a group which ensures only one notification is ever shown replacing previous notification of same group id.
+	// note.Group = "com.unique.yourapp.identifier"
+
+	// //Optionally, set a sender (Notification will now use the Safari icon)
+	// note.Sender = "com.apple.Safari"
+
+	// //Optionally, specifiy a url or bundleid to open should the notification be
+	// //clicked.
+	// note.Link = "http://www.yahoo.com" //or BundleID like: com.apple.Terminal
+
+	// //Optionally, an app icon (10.9+ ONLY)
+	// note.AppIcon = "gopher.png"
+
+	// //Optionally, a content image (10.9+ ONLY)
+	// note.ContentImage = "gopher.png"
+
+	//Then, push the notification
+	err := note.Push()
+
+	//If necessary, check error
+	if err != nil {
+		log.Println("Uh oh!")
+	}
+
 	return nil
 }

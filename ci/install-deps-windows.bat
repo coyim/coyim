b4188a070ed194b914cfd6920527199fd5073076
fanjiang <fan.torchz@gmail.com>
2016-08-24 04:42:39 +0800

    use toast instead of powershell

--- ci/install-deps-windows.bat
+++ ci/install-deps-windows.bat
@@ -4,11 +4,11 @@ xcopy %APPVEYOR_BUILD_FOLDER%\.git %GOPATH%\src\github.com\twstrike\coyim\ /e /i
 dir %GOPATH%\src\github.com\twstrike\coyim
 if "%METHOD%"=="ci" SET MSYS_PATH=c:\msys64
 if "%METHOD%"=="cross" SET MSYS_PATH=%APPVEYOR_BUILD_FOLDER%\msys%MSYS2_BITS%
+SET PATH=%MSYS_PATH%\usr\bin;%PATH%
+SET PATH=%MSYS_PATH%\mingw%MSYS2_BITS%\bin;%PATH%
 if "%METHOD%"=="cross" appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/%MSYS2_ARCH%/msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz > nul
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar > nul
-SET PATH=%MSYS_PATH%\usr\bin;%PATH%
-SET PATH=%MSYS_PATH%\mingw%MSYS2_BITS%\bin;%PATH%
 %MSYS_PATH%\usr\bin\bash -lc "echo update-core starting..." 2> nul
 %MSYS_PATH%\usr\bin\bash -lc "update-core" > nul
 %MSYS_PATH%\usr\bin\bash -lc "echo install-deps starting..."
--- gui/desktop_notifications_windows.go
+++ gui/desktop_notifications_windows.go
@@ -1,14 +1,9 @@
 package gui
 
 import (
-	"bytes"
-	"io/ioutil"
-	"os"
 	"os/exec"
-	"path/filepath"
-	"text/template"
+	"syscall"
 
-	"github.com/nu7hatch/gouuid"
 	"github.com/twstrike/coyim/ui"
 )
 
@@ -29,92 +24,23 @@ func (dn *desktopNotifications) show(jid, from, message string) error {
 	summary, _ := dn.format(from, message, false)
 
 	notification := Notification{
-		AppID:   "im.coy.coyim",
 		Title:   "CoyIM",
 		Message: summary,
 		Icon:    coyimIcon.getPath(),
 	}
-	return notification.Push()
-}
-
-var toastTemplate *template.Template
-
-func init() {
-	toastTemplate = template.New("toast")
-	toastTemplate.Parse(`
-[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
-$APP_ID = '{{.AppID}}'
-$template = @"
-<toast>
-    <visual>
-        <binding template="ToastGeneric">
-            {{if .Icon}}
-            <image placement="appLogoOverride" src="{{.Icon}}" />
-            {{end}}
-            {{if .Title}}
-            <text>{{.Title}}</text>
-            {{end}}
-            {{if .Message}}
-            <text>{{.Message}}</text>
-            {{end}}
-        </binding>
-    </visual>
-    {{if .Actions}}
-    <actions>
-        {{range .Actions}}
-        <action activationType="{{.Type}}" content="{{.Label}}" arguments="{{.Arguments}}" />
-        {{end}}
-    </actions>
-    {{end}}
-</toast>
-"@
-$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
-$xml.LoadXml($template)
-$toast = New-Object Windows.UI.Notifications.ToastNotification $xml
-[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($APP_ID).Show($toast)
-    `)
+	return notification.Popup()
 }
 
 type Notification struct {
-	AppID   string
 	Title   string
 	Message string
 	Icon    string
-	Actions []Action
 }
 
-type Action struct {
-	Type      string
-	Label     string
-	Arguments string
-}
 
-func (n *Notification) BuildXML() (string, error) {
-	var out bytes.Buffer
-	err := toastTemplate.Execute(&out, n)
-	if err != nil {
-		return "", err
-	}
-	return out.String(), nil
-}
+func (n *Notification) Popup() error {
+	cmd := exec.Command("toast.exe", "-t", n.Title, "-m", n.Message, "-p", n.Icon)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	return cmd.Run()
 
-func (n *Notification) Push() error {
-	xml, _ := n.BuildXML()
-	return invokeTemporaryScript(xml)
-}
-
-func invokeTemporaryScript(content string) error {
-	id, _ := uuid.NewV4()
-	file := filepath.Join(os.TempDir(), id.String()+".ps1")
-	defer os.Remove(file)
-	err := ioutil.WriteFile(file, []byte(content), 0600)
-	if err != nil {
-		return err
-	}
-	if err = exec.Command("PowerShell", "-File", file).Run(); err != nil {
-		return err
-	}
-	return nil
 }

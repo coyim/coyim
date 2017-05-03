f30f03632803ef2bacf37243d89d0cec4dcde594
Sofia Celi <sceli@thoughtworks.com>
2017-05-03 14:41:28 -0300

    Change link on feedback window

--- gui/definitions/feedback.go
+++ gui/definitions/feedback.go
@@ -10,8 +10,8 @@ func (*defFeedback) String() string {
 	return `<interface>
   <object class="GtkMessageDialog" id="dialog">
     <property name="title" translatable="yes">We would like to receive your feedback</property>
-    <property name="secondary-text" translatable="yes">Please, tell us how it is going for you to use CoyIM.&#xA;This is the only way we can create a better tool that will keep your conversations private.</property>
-    <property name="text" translatable="yes">https://coy.im/feedback</property>
+    <property name="secondary-text" translatable="yes">Please, let us know what you think of CoyIM.&#xA;This is the only way we can create a better tool that will keep your conversations private.</property>
+    <property name="text" translatable="yes">https://coy.im</property>
     <property name="window-position">GTK_WIN_POS_CENTER</property>
     <property name="modal">true</property>
     <property name="message-type">GTK_MESSAGE_INFO</property>

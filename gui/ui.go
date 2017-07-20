e2f817e60322fe282de2807dc98ea10546c3279d
Sofia Celi <sceli@thoughtworks.com>
2017-07-20 00:03:51 -0500

    Make TorInstallerHelper a Gtk.Dialog

--- gui/ui.go
+++ gui/ui.go
@@ -155,10 +155,17 @@ func (u *gtkUI) installTor() {
 
 	obj := builder.getObj("dialog")
 	dialog := obj.(gtki.MessageDialog)
-	dialog.SetTransientFor(u.window)
 
-	dialog.Run()
-	dialog.Destroy()
+	builder.ConnectSignals(map[string]interface{}{
+		"on_close_signal": func() {
+			dialog.Destroy()
+		},
+	})
+
+	doInUIThread(func() {
+		dialog.SetTransientFor(u.window)
+		dialog.ShowAll()
+	})
 }
 
 func (u *gtkUI) wouldYouLikeToInstallTor(k func(bool)) {

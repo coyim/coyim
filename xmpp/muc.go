5d4a453a44a917a1c43e4903f885d5b0c4a66f06
Reinaldo de Souza Junior <reinaldo@autonomia.digital>
2017-11-07 09:17:21 -0200

    Add Occupant

--- xmpp/muc.go
+++ xmpp/muc.go
@@ -6,12 +6,20 @@ const (
 	mucSupport = "<x xmlns='http://jabber.org/protocol/muc'/>"
 )
 
+type Occupant struct {
+	Room, Service, Nick string
+}
+
+func (o *Occupant) JID() string {
+	return fmt.Sprintf("%s@%s/%s", o.Room, o.Service, o.Nick)
+}
+
 func (c *conn) enterRoom(roomID, service, nickname string) error {
-	occupantJID := fmt.Sprintf("%s@%s/%s", roomID, service, nickname)
-	return c.sendPresenceWithChildren(occupantJID, "", "", mucSupport)
+	occupant := Occupant{Room: roomID, Service: service, Nick: nickname}
+	return c.sendPresenceWithChildren(occupant.JID(), "", "", mucSupport)
 }
 
 func (c *conn) leaveRoom(roomID, service, nickname string) error {
-	occupantJID := fmt.Sprintf("%s@%s/%s", roomID, service, nickname)
-	return c.sendPresenceWithChildren(occupantJID, "unavailable", "", mucSupport)
+	occupant := Occupant{Room: roomID, Service: service, Nick: nickname}
+	return c.sendPresenceWithChildren(occupant.JID(), "unavailable", "", mucSupport)
 }

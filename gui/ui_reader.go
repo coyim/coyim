coyim/coyim

commit 1457edfac0d90d4745fa21324a0ca85759744db1

Use github.com/mjibson/esc to embed UI definition files

gui/ui_reader.go
@@ -1,16 +1,15 @@
+//go:generate esc -o definitions.go -modtime 1489449600 -pkg gui definitions/
+
 package gui
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"path/filepath"
+	"path"
 	"strings"
 
 	"reflect"
 
-	"github.com/twstrike/coyim/gui/definitions"
 	"github.com/twstrike/gotk3adapter/glibi"
 	"github.com/twstrike/gotk3adapter/gtki"
 )
@@ -29,16 +28,17 @@ func getActualDefsFolder() string {
 }
 
 func getDefinitionWithFileFallback(uiName string) string {
-	// this makes sure a missing definition wont break only when the app is released
-	uiDef := getDefinition(uiName)
-
-	fileName := filepath.Join(getActualDefsFolder(), uiName+xmlExtension)
-	if fileNotFound(fileName) {
-		log.Printf("gui: loading compiled definition %q\n", uiName)
-		return uiDef.String()
+	fname := path.Join("/definitions", uiName+xmlExtension)
+	embeddedFile, err := FSString(false, fname)
+	if err != nil {
+		panic(fmt.Sprintf("No definition found for %s", uiName))
 	}
 
-	return readFile(fileName)
+	if localFile, err := FSString(true, fname); err == nil {
+		return localFile
+	}
+
+	return embeddedFile
 }
 
 // This must be called from the UI thread - otherwise bad things will happen sooner or later
@@ -64,20 +64,6 @@ func builderForDefinition(uiName string) gtki.Builder {
 func fileNotFound(fileName string) bool {
 	_, fnf := os.Stat(fileName)
 	return os.IsNotExist(fnf)
-}
-
-func readFile(fileName string) string {
-	data, _ := ioutil.ReadFile(fileName)
-	return string(data)
-}
-
-func getDefinition(uiName string) fmt.Stringer {
-	def, ok := definitions.Get(uiName)
-	if !ok {
-		panic(fmt.Sprintf("No definition found for %s", uiName))
-	}
-
-	return def
 }
 
 type builder struct {

Makefile
@@ -15,7 +15,7 @@ build: build-cli build-gui
 # This should not be added as a requirement to build-gui because it may hide
 # build problems. build-gui is exactly what `go get` will do on a clean repo
 gen-ui-defs:
-	make -C ./gui/definitions
+	make -C ./gui
 
 gen-schema-defs:
 	make -C ./gui/settings/definitions
@@ -99,6 +99,7 @@ deps-dev:
 	go get -u github.com/golang/lint/golint
 	go get -u github.com/modocache/gover
 	go get -u github.com/tools/godep
+	go get -u github.com/mjibson/esc
 
 deps: deps-dev
 

ci/Makefile
@@ -28,7 +28,7 @@ endif
 
 # make sure the ui definitions checked in are in sync
 check-ui-defs:
-	make doctor -C ../gui/definitions
+	make doctor -C ../gui
 
 # send coverage data to coveralls
 coveralls:

gui/Makefile
@@ -1,8 +1,8 @@
 generate:
-	ruby ./generate.rb
+	go generate -x .
 
 touch:
-	ls *.xml | xargs -n1 touch
+	ls definitions/*.xml | xargs -n1 touch
 
 doctor: touch generate
 	git diff --exit-code .

gui/definitions/account_registration.go
@@ -1,87 +1,0 @@
-package definitions
-
-func init() {
-	add(`AccountRegistration`, &defAccountRegistration{})
-}
-
-type defAccountRegistration struct{}
-
-func (*defAccountRegistration) String() string {
-	return `<interface>
-  <object class="GtkDialog" id="dialog">
-    <property name="title" translatable="yes">Choose a server to register your account</property>
-    <signal name="close" handler="on_cancel_signal" />
-    <signal name="delete-event" handler="on_cancel_signal" />
-
-    <child internal-child="vbox">
-      <object class="GtkBox" id="Vbox">
-        <property name="margin">10</property>
-        <property name="spacing">10</property>
-        <child>
-          <object class="GtkLabel" id="message">
-            <property name="label" translatable="yes">Please choose the server where you want to register an account.</property>
-            <property name="wrap">true</property>
-          </object>
-          <packing>
-            <property name="expand">false</property>
-            <property name="fill">true</property>
-          </packing>
-        </child>
-
-        <child>
-          <object class="GtkGrid" id="grid">
-            <property name="margin-bottom">10</property>
-            <property name="row-spacing">12</property>
-            <property name="column-spacing">6</property>
-
-            <child>
-              <object class="GtkLabel" id="server-label">
-                <property name="label" translatable="yes">Server</property>
-                <property name="justify">GTK_JUSTIFY_RIGHT</property>
-              </object>
-              <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">0</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkComboBoxText" id="server">
-                <property name="has-entry">True</property>
-              </object>
-              <packing>
-                <property name="left-attach">1</property>
-                <property name="top-attach">0</property>
-              </packing>
-            </child>
-          </object>
-        </child>
-
-        <child internal-child="action_area">
-          <object class="GtkButtonBox" id="bbox">
-            <property name="orientation">GTK_ORIENTATION_HORIZONTAL</property>
-            <child>
-              <object class="GtkButton" id="btn-cancel">
-                <property name="label" translatable="yes">Cancel</property>
-                <signal name="clicked" handler="on_cancel_signal" />
-              </object>
-            </child>
-            <child>
-              <object class="GtkButton" id="btn-apply">
-                <property name="label" translatable="yes">Apply</property>
-                <property name="can-default">true</property>
-                <signal name="clicked" handler="on_save_signal" />
-              </object>
-            </child>
-          </object>
-        </child>
-      </object>
-    </child>
-
-    <action-widgets>
-      <action-widget response="cancel">btn-cancel</action-widget>
-      <action-widget response="apply" default="true">btn-apply</action-widget>
-    </action-widgets>
-  </object>
-</interface>
-`
-}

gui/definitions/add_contact.go
@@ -1,222 +1,0 @@
-package definitions
-
-func init() {
-	add(`AddContact`, &defAddContact{})
-}
-
-type defAddContact struct{}
-
-func (*defAddContact) String() string {
-	return `<interface>
-  <object class="GtkListStore" id="accounts-model">
-    <columns>
-      <!-- account name -->
-      <column type="gchararray"/>
-      <!-- account id -->
-      <column type="gchararray"/>
-    </columns>
-  </object>
-
-  <object class="GtkTextBuffer" id="subscriptionAskMessage">
-    <property name="text" translatable="yes">I would like to add you to my contact list.</property>
-  </object>
-  
-  <object class="GtkDialog" id="AddContact">
-    <property name="window-position">GTK_WIN_POS_CENTER</property>
-    <property name="border_width">6</property>
-    <property name="title" translatable="yes">Add new contact</property>
-    <property name="resizable">True</property>
-    <property name="default-height">400</property>
-    <property name="default-width">500</property>
-    <property name="destroy-with-parent">true</property>
-    <child internal-child="vbox">
-      <object class="GtkBox" id="Vbox">
-        <property name="homogeneous">false</property>
-        <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
-        <property name="spacing">6</property>
-        <child>
-          <object class="GtkBox" id="notification-area">
-            <property name="visible">true</property>
-            <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
-          </object>
-          <packing>
-            <property name="expand">false</property>
-            <property name="fill">true</property>
-            <property name="position">0</property>
-          </packing>
-        </child>
-
-        <child>
-          <object class="GtkGrid" id="grid">
-            <property name="margin-top">15</property>
-            <property name="margin-bottom">10</property>
-            <property name="margin-start">10</property>
-            <property name="margin-end">10</property>
-            <property name="row-spacing">12</property>
-            <property name="column-spacing">6</property>
-
-            <child>
-              <object class="GtkLabel" id="subscriptionInstructions">
-                <property name="label" translatable="yes">Please fill in the information for the peer you would like to add. If you have more than one account, ensure you choose the account you would like to add this peer to. You can optionally choose to save a nickname for this peer. You can also choose to automatically allow the peer to see you, without them having to ask permission first. Finally, you can also customize the message the peer will see when asking to see them. Remember that this message will not be end-to-end encrypted, so do not reveal any sensitive information in this message.</property>
-                <property name="visible">true</property>
-                <property name="wrap">true</property>
-                <property name="max-width-chars">65</property>
-              </object>
-              <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">0</property>
-                <property name="width">2</property>
-              </packing>
-            </child>
-
-            <child>
-              <object class="GtkLabel" id="accountsLabel" >
-                <property name="label" translatable="yes">Account:</property>
-                <property name="justify">GTK_JUSTIFY_RIGHT</property>
-                <property name="halign">GTK_ALIGN_END</property>
-              </object>
-              <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">1</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkComboBox" id="accounts">
-                <property name="model">accounts-model</property>
-                <property name="has-focus">true</property>
-                <property name="hexpand">True</property>
-                <child>
-                  <object class="GtkCellRendererText" id="account-name-rendered"/>
-                  <attributes>
-                    <attribute name="text">0</attribute>
-                  </attributes>
-                </child>
-              </object>
-              <packing>
-                <property name="left-attach">1</property>
-                <property name="top-attach">1</property>
-              </packing>
-            </child>
-
-            <child>
-              <object class="GtkLabel" id="accountLabel" >
-                <property name="label" translatable="yes">Contact to add:</property>
-                <property name="justify">GTK_JUSTIFY_RIGHT</property>
-                <property name="halign">GTK_ALIGN_END</property>
-              </object>
-              <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">2</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkEntry" id="address">
-                <property name="placeholder-text">someone@jabber.org</property>
-                <property name="hexpand">True</property>
-                <signal name="activate" handler="on_save_signal" />
-              </object>
-              <packing>
-                <property name="left-attach">1</property>
-                <property name="top-attach">2</property>
-              </packing>
-            </child>
-
-            <child>
-              <object class="GtkLabel" id="nicknameLabel" >
-                <property name="label" translatable="yes">Nickname:</property>
-                <property name="justify">GTK_JUSTIFY_RIGHT</property>
-                <property name="halign">GTK_ALIGN_END</property>
-              </object>
-              <packing>
-                <property name="left-attach">0</property>
-                <property name="top-attach">3</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkEntry" id="nickname">
-                <property name="hexpand">True</property>
-                <signal name="activate" handler="on_save_signal" />
-              </object>
-              <packing>
-                <property name="left-attach">1</property>
-                <property name="top-attach">3</property>
-              </packing>
-            </child>
-
-          </object>
-          <packing>
-            <property name="expand">true</property>
-            <property name="fill">true</property>
-            <property name="position">1</property>
-          </packing>
-        </child>
-
-        <child>
-          <object class="GtkCheckButton" id="auto_authorize_checkbutton">
-            <property name="label" translatable="yes">A_llow this contact to view my status</property>
-            <property name="use_action_appearance">False</property>
-            <property name="visible">True</property>
-            <property name="can_focus">True</property>
-            <property name="receives_default">False</property>
-            <property name="no_show_all">True</property>
-            <property name="use_underline">True</property>
-            <property name="xalign">0</property>
-            <property name="active">True</property>
-            <property name="draw_indicator">True</property>
-          </object>
-          <packing>
-            <property name="expand">False</property>
-            <property name="fill">False</property>
-            <property name="position">2</property>
-          </packing>
-        </child>
-        <child>
-          <object class="GtkScrolledWindow" id="message_scrolledwindow">
-            <property name="visible">True</property>
-            <property name="can_focus">True</property>
-            <property name="no_show_all">True</property>
-            <property name="border_width">6</property>
-            <property name="shadow_type">etched-in</property>
-            <property name="min_content_height">5</property>
-            <child>
-              <object class="GtkTextView" id="message_textview">
-                <property name="visible">True</property>
-                <property name="can_focus">True</property>
-                <property name="wrap_mode">word</property>
-                <property name="buffer">subscriptionAskMessage</property>
-              </object>
-            </child>
-          </object>
-          <packing>
-            <property name="expand">True</property>
-            <property name="fill">True</property>
-            <property name="position">3</property>
-          </packing>
-        </child>
-        
-        <child internal-child="action_area">
-          <object class="GtkButtonBox" id="button_box">
-            <property name="orientation">GTK_ORIENTATION_HORIZONTAL</property>
-            <child>
-              <object class="GtkButton" id="button_cancel">
-                <property name="label">_Cancel</property>
-                <property name="use-underline">True</property>
-                <signal name="clicked" handler="on_close_signal" />
-              </object>
-            </child>
-            <child>
-              <object class="GtkButton" id="button_ok">
-                <property name="label" translatable="yes">Add</property>
-                <property name="use-underline">True</property>
-                <property name="can-default">true</property>
-                <signal name="clicked" handler="on_save_signal" />
-              </object>
-            </child>
-          </object>
-        </child>
-      </object>
-    </child>
-  </object>
-</interface>
-`
-}

commit a70bee2b10e667c07aa99d9c80432f01d24f223f

Fix loading of UI definitions from local file.
It was broken since 1457edfa. We need getActualDefsFolder() to get the
actual folder at runtime.

gui/ui_reader.go
@@ -4,9 +4,11 @@ package gui
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 	"sync"
 
@@ -31,16 +33,25 @@ func getActualDefsFolder() string {
 
 func getDefinitionWithFileFallback(uiName string) string {
 	fname := path.Join("/definitions", uiName+xmlExtension)
+
 	embeddedFile, err := FSString(false, fname)
 	if err != nil {
+		//Enforce the file is embedded, but dont use it.
 		panic(fmt.Sprintf("No definition found for %s", uiName))
 	}
 
-	if localFile, err := FSString(true, fname); err == nil {
-		return localFile
+	fileName := filepath.Join(getActualDefsFolder(), uiName+xmlExtension)
+	if fileNotFound(fileName) {
+		return embeddedFile
 	}
 
-	return embeddedFile
+	log.Printf("Loading definition from local file: %q\n", fileName)
+	return readFile(fileName)
+}
+
+func readFile(fileName string) string {
+	data, _ := ioutil.ReadFile(fileName)
+	return string(data)
 }
 
 // This must be called from the UI thread - otherwise bad things will happen sooner or later
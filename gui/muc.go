9756c5dfce7ccb65d88c2e06742ffa1f12294c85
Reinaldo de Souza Junior <reinaldo@autonomia.digital>
2017-11-16 13:47:05 -0200

    Open room view when the form is submitted

--- gui/muc.go
+++ gui/muc.go
@@ -1,12 +1,144 @@
 package gui
 
 import (
+	"errors"
 	"log"
 
+	"github.com/coyim/coyim/xmpp"
 	"github.com/coyim/coyim/xmpp/interfaces"
 	"github.com/coyim/gotk3adapter/glibi"
 	"github.com/coyim/gotk3adapter/gtki"
 )
+
+type addChatView struct {
+	accountManager *accountManager
+
+	gtki.Dialog `gtk-widget:"add-chat-dialog"`
+
+	model   gtki.ListStore `gtk-widget:"accounts-model"`
+	account gtki.ComboBox  `gtk-widget:"accounts"`
+	service gtki.Entry     `gtk-widget:"service"`
+	room    gtki.Entry     `gtk-widget:"room"`
+	handle  gtki.Entry     `gtk-widget:"handle"`
+}
+
+func newChatView(accountManager *accountManager) gtki.Dialog {
+	view := &addChatView{
+		accountManager: accountManager,
+	}
+
+	builder := newBuilder("AddChat")
+	err := builder.bindObjects(view)
+	if err != nil {
+		panic(err)
+	}
+
+	builder.ConnectSignals(map[string]interface{}{
+		"join_room_handler": view.joinRoomHandler,
+		"cancel_handler":    view.Destroy,
+	})
+
+	view.populateModel()
+
+	return view
+}
+
+func (v *addChatView) populateModel() {
+	accs := v.accountManager.getAllConnectedAccounts()
+	for _, acc := range accs {
+		iter := v.model.Append()
+		v.model.SetValue(iter, 0, acc.session.GetConfig().Account)
+		v.model.SetValue(iter, 1, acc.session.GetConfig().ID())
+	}
+
+	if len(accs) > 0 {
+		v.account.SetActive(0)
+	}
+}
+
+//TODO: This is repeated on AddAccount logic, for example.
+func (v *addChatView) getAccount() (*account, bool) {
+	iter, err := v.account.GetActiveIter()
+	if err != nil {
+		return nil, false
+	}
+
+	val, err := v.model.GetValue(iter, 1)
+	if err != nil {
+		return nil, false
+	}
+
+	id, err := val.GetString()
+	if err != nil {
+		return nil, false
+	}
+
+	return v.accountManager.getAccountByID(id)
+}
+
+func (v *addChatView) validateForm() (*account, *xmpp.Occupant, error) {
+	account, ok := v.getAccount()
+	if !ok {
+		return nil, nil, errors.New("could not find account")
+	}
+
+	service, err := v.service.GetText()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	room, err := v.room.GetText()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	handle, err := v.handle.GetText()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	//TODO: VALIDATE!
+
+	occ := &xmpp.Occupant{
+		Room: xmpp.Room{
+			ID:      room,
+			Service: service,
+		},
+		Nick: handle,
+	}
+
+	return account, occ, nil
+}
+
+func (v *addChatView) joinRoomHandler() {
+	account, occupant, err := v.validateForm()
+	if err != nil {
+		//TODO: show error
+		return
+	}
+
+	doInUIThread(func() {
+		//TODO: the reference to this object should be kept
+		//otherwise it will be garbage collected before we are done
+		//with the window. We cant keep this goroutine blocked to avoid
+		//the view from leaving the scope, because it would block the
+		//glib main thread.
+		//It probably works because glib.IdleAdd never releases this fn
+		chatRoom := newMockupView(account, occupant)
+		if parent, err := v.GetTransientFor(); err != nil {
+			chatRoom.SetTransientFor(parent)
+		}
+
+		v.Destroy()
+		chatRoom.Show()
+	})
+}
+
+func (u *gtkUI) addChatRoom() {
+	view := newChatView(u.accountManager)
+	view.SetTransientFor(u.window)
+	view.Show()
+}
 
 type mucMockupView struct {
 	gtki.Window `gtk-widget:"muc-window"`
@@ -86,14 +218,6 @@ func (v *mucMockupView) onSendMessage(_ glibi.Object) {
 	v.entry.SetText("")
 
 	go v.connectOrSendMessage(msg)
-}
-
-func (u *gtkUI) addChatRoom() {
-	builder := newBuilder("AddChat")
-	dialog := builder.get("add-chat-dialog").(gtki.Dialog)
-
-	dialog.SetTransientFor(u.window)
-	dialog.Show()
 }
 
 func (u *gtkUI) openMUCMockup() {

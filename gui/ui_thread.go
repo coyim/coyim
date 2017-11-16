617c990b3e0a920845d06996cf8118aa8a2e01eb
Reinaldo de Souza Junior <reinaldo@autonomia.digital>
2017-11-16 13:43:04 -0200

    Add comments to explaing why we use doInUIThread

--- gui/ui_thread.go
+++ gui/ui_thread.go
@@ -1,11 +1,18 @@
 package gui
 
+//TODO: could this use a compiling flag to generate a noop function when released?
 func assertInUIThread() {
 	if g.glib.MainDepth() == 0 {
 		panic("This function has to be called from the UI thread")
 	}
 }
 
+//GTK process events in glib event loop (see [1]). In order to keep the UI
+//responsive, it is a good practice to not block long running tasks in a signal's
+//callback (you dont want a button to keep looking pressed for a couple of seconds).
+//doInUIThread schedule the function to run in the next
+//1 - https://developer.gnome.org/glib/unstable/glib-The-Main-Event-Loop.html
+//TODO: Try other patterns and expose them as API. Example: http://www.mono-project.com/docs/gui/gtksharp/responsive-applications/
 func doInUIThread(f func()) {
 	g.glib.IdleAdd(f)
 }

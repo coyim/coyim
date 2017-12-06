c9956aff41fbd621dbba07a78d957e024b487317
Giovane Liberato <giovane@riseup.net>
2017-12-06 16:11:17 -0200

    Move list MUC rooms code to another file #476

--- gui/chat.go
+++ gui/chat.go
@@ -33,16 +33,6 @@ type addChatView struct {
 	model gtki.ListStore `gtk-widget:"accounts-model"`
 }
 
-type listRoomsView struct {
-	accountManager *accountManager
-
-	gtki.Dialog `gtk-widget:"list-chat-rooms"`
-
-	service       gtki.Entry     `gtk-widget:"service"`
-	roomsModel    gtki.ListStore `gtk-widget:"rooms"`
-	roomsTreeView gtki.TreeView  `gtk-widget:"rooms-list-view"`
-}
-
 func newChatView(accountManager *accountManager) gtki.Dialog {
 	view := &addChatView{
 		accountManager: accountManager,
@@ -63,57 +53,6 @@ func newChatView(accountManager *accountManager) gtki.Dialog {
 	doInUIThread(view.populateModel)
 
 	return view
-}
-
-func newListRoomsView(accountManager *accountManager) gtki.Dialog {
-	view := &listRoomsView{
-		accountManager: accountManager,
-	}
-
-	builder := newBuilder("ListChatRooms")
-	err := builder.bindObjects(view)
-	if err != nil {
-		panic(err)
-	}
-
-	builder.ConnectSignals(map[string]interface{}{
-		"cancel_handler":             view.Destroy,
-		"join_selected_room_handler": view.joinSelectedRoom,
-		"fetch_rooms_handler":        view.fetchRoomsFromService,
-	})
-
-	return view
-}
-
-func (v *listRoomsView) fetchRoomsFromService() {
-	v.roomsModel.Clear()
-	service, _ := v.service.GetText()
-
-	//TODO: Be able to select account
-	account := v.accountManager.getAllAccounts()[0]
-
-	conn := account.session.Conn()
-	result, _ := conn.GetChatContext().QueryRooms(service)
-	//TODO: deal with empty results
-
-	doInUIThread(func() {
-		for _, room := range result {
-			iter := v.roomsModel.Append()
-			v.roomsModel.SetValue(iter, 0, room.Jid)
-			v.roomsModel.SetValue(iter, 1, room.Name)
-			v.roomsModel.SetValue(iter, 2, room.Name)
-		}
-	})
-}
-
-func (v *listRoomsView) joinSelectedRoom() {
-	ts, _ := v.roomsTreeView.GetSelection()
-	if _, iter, ok := ts.GetSelected(); ok {
-		value, _ := v.roomsModel.GetValue(iter, 0)
-		roomJid, _ := value.GetString()
-		log.Print("ROOM: ")
-		log.Print(roomJid)
-	}
 }
 
 func (v *addChatView) populateModel() {
@@ -341,12 +280,6 @@ func (v *chatRoomView) showRoomConfigDialog() {
 func (u *gtkUI) joinChatRoom() {
 	//pass message and presence channels
 	view := newChatView(u.accountManager)
-	view.SetTransientFor(u.window)
-	view.Show()
-}
-
-func (u *gtkUI) listChatRooms() {
-	view := newListRoomsView(u.accountManager)
 	view.SetTransientFor(u.window)
 	view.Show()
 }

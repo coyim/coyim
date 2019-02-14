e8d8d222a086477853d85a5286f5db4c68e46df8
Ola Bini <ola@olabini.se>
2019-02-14 15:34:18 -0500

    OK, manually remove the stupid package

--- ci/install-deps-windows.bat
+++ ci/install-deps-windows.bat
@@ -9,8 +9,9 @@ SET PATH=%MSYS_PATH%\mingw%MSYS2_BITS%\bin;%PATH%
 if "%METHOD%"=="cross" appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/%MSYS2_ARCH%/msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz > nul
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar > nul
+%MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -R catgets"
 %MSYS_PATH%\usr\bin\bash -lc "echo update-core starting..." 2> nul
-%MSYS_PATH%\usr\bin\bash -lc "yes|update-core"
+%MSYS_PATH%\usr\bin\bash -lc "update-core"
 %MSYS_PATH%\usr\bin\bash -lc "echo install-deps starting..."
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy autoconf" > nul
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy automake" > nul

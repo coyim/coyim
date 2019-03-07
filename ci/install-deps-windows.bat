5482fed8c26a32e4049b41e8b541c4485927037c
Ola Bini <ola@olabini.se>
2019-03-07 11:48:21 -0500

    Add a debug thing for appveyor

--- ci/install-deps-windows.bat
+++ ci/install-deps-windows.bat
@@ -24,4 +24,5 @@ if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar > nul
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy mingw-w64-%MSYS2_ARCH%-gtk3" > nul
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy mingw-w64-%MSYS2_ARCH%-pkg-config" > nul
 %MSYS_PATH%\usr\bin\bash -lc "yes|pacman --noconfirm -Sc" > nul
+%MSYS_PATH%\usr\bin\bash -lc "find / -name '*.dll'"
 if "%METHOD%"=="cross" %MSYS_PATH%\autorebase.bat > nul

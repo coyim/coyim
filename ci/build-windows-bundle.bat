c4e9c21c1ebae3db704999646329da15facf6a8a
Ola Bini <ola@olabini.se>
2019-03-08 06:27:26 -0500

    Explicitly copy files in bin and lib

--- ci/build-windows-bundle.bat
+++ ci/build-windows-bundle.bat
@@ -1,7 +1,8 @@
 mkdir %GOPATH%\src\github.com\coyim\coyim\release\
 mkdir %GOPATH%\src\github.com\coyim\coyim\release\share
 %MSYS_PATH%\usr\bin\bash -lc "find /mingw%MSYS2_BITS% -name '*.dll'"
-%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/**/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
+%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/bin/**/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
+%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/lib/**/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
 %MSYS_PATH%\usr\bin\bash -lc "cp -r /mingw%MSYS2_BITS%/share/icons /c/gopath/src/github.com/coyim/coyim/release/share/"
 %MSYS_PATH%\usr\bin\bash -lc "cp -r /mingw%MSYS2_BITS%/share/glib-2.0 /c/gopath/src/github.com/coyim/coyim/release/share/"
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy sed" > nul

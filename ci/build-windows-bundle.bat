85b84c419a6bca5c68b39e40a7095023f88796af
Ola Bini <ola@olabini.se>
2019-03-08 06:44:57 -0500

    This environment is pretty messed up...

--- ci/build-windows-bundle.bat
+++ ci/build-windows-bundle.bat
@@ -1,8 +1,15 @@
 mkdir %GOPATH%\src\github.com\coyim\coyim\release\
 mkdir %GOPATH%\src\github.com\coyim\coyim\release\share
+%MSYS_PATH%\usr\bin\bash -lc "echo 'FINDING ALL'"
 %MSYS_PATH%\usr\bin\bash -lc "find /mingw%MSYS2_BITS% -name '*.dll'"
-%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/bin/**/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
+%MSYS_PATH%\usr\bin\bash -lc "echo 'Copying bin/*.dll'"
+%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/bin/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
+%MSYS_PATH%\usr\bin\bash -lc "echo 'Copying lib/*.dll'"
+%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/lib/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
+%MSYS_PATH%\usr\bin\bash -lc "echo 'Copying lib/**/*.dll'"
 %MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/lib/**/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
+%MSYS_PATH%\usr\bin\bash -lc "echo 'Copying lib/gdk-pixbuf-2.0/*/loaders/*.dll'"
+%MSYS_PATH%\usr\bin\bash -lc "cp -v /mingw%MSYS2_BITS%/lib/gdk-pixbuf-2.0/*/loaders/*.dll /c/gopath/src/github.com/coyim/coyim/release/"
 %MSYS_PATH%\usr\bin\bash -lc "cp -r /mingw%MSYS2_BITS%/share/icons /c/gopath/src/github.com/coyim/coyim/release/share/"
 %MSYS_PATH%\usr\bin\bash -lc "cp -r /mingw%MSYS2_BITS%/share/glib-2.0 /c/gopath/src/github.com/coyim/coyim/release/share/"
 %MSYS_PATH%\usr\bin\bash -lc "pacman --noconfirm --needed -Sy sed" > nul

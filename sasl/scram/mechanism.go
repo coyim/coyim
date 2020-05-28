6af1a043539ee57db7ad159c0ff6f33d2950f364
Ola Bini <ola@autonomia.digital>
2020-05-28 18:59:30 +0000

    Rename file names to be more accurate

--- sasl/scram/mechanism.go
+++ sasl/scram/mechanism.go
@@ -1,11 +1,55 @@
 // Package scram implements the Salted Challenge Response Authentication Mechanism
-// according to RFC 5802.
+// according to RFC 5802 and RFC 7677.
 package scram
 
-import "github.com/coyim/coyim/sasl"
+import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"hash"
 
-// Register the SASL mechanisms
-func Register() {
-	sasl.RegisterMechanism(sha1Name, sha1Mechanism)
-	sasl.RegisterMechanism(sha256Name, sha256Mechanism)
+	"github.com/coyim/coyim/sasl"
+)
+
+var (
+	sha1Mechanism   sasl.Mechanism = &scramMechanism{sha1.New, sha1.Size, false}
+	sha256Mechanism sasl.Mechanism = &scramMechanism{sha256.New, sha256.Size, false}
+)
+
+const (
+	// Name is the authentication type associated with the SASL mechanism
+	sha1Name   = "SCRAM-SHA-1"
+	sha256Name = "SCRAM-SHA-256"
+)
+
+type scramMechanism struct {
+	hash     func() hash.Hash
+	hashSize int
+	plus     bool
+}
+
+func (m *scramMechanism) NewClient() sasl.Session {
+	return &scram{
+		state: start{m.hash, m.hashSize, m.plus},
+		props: make(sasl.Properties),
+	}
+}
+
+type scram struct {
+	state
+	props sasl.Properties
+}
+
+func (p *scram) SetProperty(prop sasl.Property, v string) error {
+	p.props[prop] = v
+	return nil
+}
+
+func (p *scram) Step(t sasl.Token) (ret sasl.Token, err error) {
+	pairs := sasl.ParseAttributeValuePairs(t)
+	p.state, ret, err = p.state.next(t, p.props, pairs)
+	return
+}
+
+func (p *scram) NeedsMore() bool {
+	return !p.state.finished()
 }
---
+++ sasl/scram/register.go
@@ -0,0 +1,11 @@
+// Package scram implements the Salted Challenge Response Authentication Mechanism
+// according to RFC 5802.
+package scram
+
+import "github.com/coyim/coyim/sasl"
+
+// Register the SASL mechanisms
+func Register() {
+	sasl.RegisterMechanism(sha1Name, sha1Mechanism)
+	sasl.RegisterMechanism(sha256Name, sha256Mechanism)
+}

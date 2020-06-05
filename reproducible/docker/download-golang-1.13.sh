2f79583a478c346096272e9c3c587fabf1545622
Ola Bini <ola@autonomia.digital>
2020-06-05 15:57:36 +0000

    Dammit, wrong sum

--- reproducible/docker/download-golang-1.13.sh
+++ reproducible/docker/download-golang-1.13.sh
@@ -3,11 +3,11 @@
 set -xe
 
 # Download Go 1.13.12
-# SHA256: 17ba2c4de4d78793a21cc659d9907f4356cd9c8de8b7d0899cdedcef712eba34
+# SHA256: 9cacc6653563771b458c13056265aa0c21b8a23ca9408278484e4efde4160618
 # https://golang.org/dl/
 mkdir -p /pkgs && cd /pkgs &&\
     curl https://dl.google.com/go/go1.13.12.linux-amd64.tar.gz -O &&\
-    echo "17ba2c4de4d78793a21cc659d9907f4356cd9c8de8b7d0899cdedcef712eba34 /pkgs/go1.13.12.linux-amd64.tar.gz" | sha256sum -c -
+    echo "9cacc6653563771b458c13056265aa0c21b8a23ca9408278484e4efde4160618 /pkgs/go1.13.12.linux-amd64.tar.gz" | sha256sum -c -
 
 rm -rf /root/go &&\
   mkdir -p /root/go &&\

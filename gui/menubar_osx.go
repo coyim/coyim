80bf9cdbb83f88bc65191a4ac0889af19a96d97a
Ola Bini <ola@autonomia.digital>
2020-06-10 21:00:43 +0000

    Add About and Preferences to the App Menu for OS X, to follow established patterns. This also removes the menuitems from the original menu

--- gui/menubar_osx.go
+++ gui/menubar_osx.go
@@ -5,21 +5,22 @@ package gui
 import "github.com/coyim/gotk3adapter/gtka"
 import "github.com/coyim/gotk3adapter/gtki"
 
-func (u *gtkUI) removeMenuItemsBestPlacedElsewhere(mb gtki.MenuBar) {
-	// TODO: we should remove the "settings" and "about" parts here
-	// They will show up in other places anyway
-}
-
 func (u *gtkUI) initializeMenus() {
 	mb := u.mainBuilder.getObj("menubar").(gtki.MenuBar)
-	u.removeMenuItemsBestPlacedElsewhere(mb)
-
 	mb.ShowAll()
 
 	app := u.hooks.(*osxHooks).app
 	app.SetMenuBar(gtka.UnwrapMenuShell(mb))
 	app.SetHelpMenu(nil)
 	app.SetWindowMenu(nil)
+
+	aboutMenuItem := u.mainBuilder.getObj("aboutMenu").(gtki.MenuItem)
+	prefsMenuItem := u.mainBuilder.getObj("preferencesMenuItem").(gtki.MenuItem)
+	sepMenuItem, _ := g.gtk.SeparatorMenuItemNew()
+
+	app.InsertAppMenuItem(gtka.UnwrapWidget(aboutMenuItem), 0)
+	app.InsertAppMenuItem(gtka.UnwrapWidget(sepMenuItem), 1)
+	app.InsertAppMenuItem(gtka.UnwrapWidget(prefsMenuItem), 2)
 
 	mb.SetNoShowAll(true)
 	mb.Hide()

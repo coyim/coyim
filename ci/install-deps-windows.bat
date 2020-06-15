330c8739759f62bb2ae36306a9ef60785965a6b7
Ola Bini <ola@autonomia.digital>
2020-06-15 15:41:05 +0000

    It seems downloading from this URL is not so stable, so let's use my own for now

--- ci/install-deps-windows.bat
+++ ci/install-deps-windows.bat
@@ -6,7 +6,7 @@ if "%METHOD%"=="ci" SET MSYS_PATH=c:\msys64
 if "%METHOD%"=="cross" SET MSYS_PATH=%APPVEYOR_BUILD_FOLDER%\msys%MSYS2_BITS%
 SET PATH=%MSYS_PATH%\usr\bin;%PATH%
 SET PATH=%MSYS_PATH%\mingw%MSYS2_BITS%\bin;%PATH%
-if "%METHOD%"=="cross" appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/%MSYS2_ARCH%/msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
+if "%METHOD%"=="cross" appveyor DownloadFile https://olabini.se/tmp/msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz > nul
 if "%METHOD%"=="cross" 7z x msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar > nul
 %MSYS_PATH%\usr\bin\bash -lc "echo update-core starting..."

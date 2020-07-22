6b924f3d7569361135ad08440b3fa8c083e2d2c5
Pedro Palau <pepalauisaac@gmail.com>
2020-07-22 14:32:56 -0500

    Part of #659 Show accounts demo contacts peers

--- gui/muc.go
+++ gui/muc.go
@@ -1,6 +1,11 @@
 package gui
 
-import "github.com/coyim/gotk3adapter/gtki"
+import (
+	"fmt"
+	"html"
+
+	"github.com/coyim/gotk3adapter/gtki"
+)
 
 type mucUI struct {
 	u              *gtkUI
@@ -17,13 +22,17 @@ var (
 	mucStatusOffline    mucAccountStatus = "offline"
 )
 
-type notificationStatus string
+type mucAccountContact struct {
+	id     string
+	name   string
+	status mucAccountStatus
+}
 
 type mucAccount struct {
 	id       string
 	name     string
 	status   mucAccountStatus
-	accounts []*mucAccountStatus
+	contacts []*mucAccountContact
 }
 
 type mucAccountManager struct {
@@ -65,10 +74,42 @@ func (m *mucUI) initDemoAccounts() {
 		&mucAccount{
 			id:     "sandy@autonomia.digital",
 			status: mucStatusOnline,
+			contacts: []*mucAccountContact{
+				&mucAccountContact{
+					id:     "pedro@autonomia.digital",
+					name:   "Pedro Enrique",
+					status: mucStatusOnline,
+				},
+				&mucAccountContact{
+					id:     "rafael@autonomia.digital",
+					status: mucStatusOnline,
+				},
+				&mucAccountContact{
+					id:     "cristina@autonomia.digital",
+					name:   "Cristina Salcedo",
+					status: mucStatusOffline,
+				},
+			},
 		},
 		&mucAccount{
 			id:     "pedro@autonomia.digital",
 			status: mucStatusOnline,
+			contacts: []*mucAccountContact{
+				&mucAccountContact{
+					id:     "sandy@autonomia.digital",
+					name:   "Sandy Acurio",
+					status: mucStatusOnline,
+				},
+				&mucAccountContact{
+					id:     "rafael@autonomia.digital",
+					status: mucStatusOnline,
+				},
+				&mucAccountContact{
+					id:     "cristina@autonomia.digital",
+					name:   "Cristina Salcedo",
+					status: mucStatusOffline,
+				},
+			},
 		},
 		&mucAccount{
 			id:     "pedro@coy.im",
@@ -118,6 +159,9 @@ func (r *mucRoster) addAccount(account *mucAccount) {
 
 	accountCounter := &counter{}
 
+	// Contacts for this account
+	r.addAccountContacts(account.contacts, parentIter, accountCounter)
+
 	displayName := account.displayName()
 
 	_ = r.model.SetValue(parentIter, indexParentJid, displayName)
@@ -133,6 +177,34 @@ func (r *mucRoster) addAccount(account *mucAccount) {
 
 	_ = r.model.SetValue(parentIter, indexStatusIcon, statusIcons[account.getStatus()].GetPixbuf())
 	_ = r.model.SetValue(parentIter, indexParentDisplayName, createGroupDisplayName(displayName, accountCounter, true))
+}
+
+func (r *mucRoster) addAccountContacts(contacts []*mucAccountContact, parentIter gtki.TreeIter, accountCounter *counter) {
+	groupCounter := &counter{}
+
+	for _, item := range contacts {
+		o := item.isOnline()
+		accountCounter.inc(true, o)
+		groupCounter.inc(true, o)
+		r.addItem(item)
+	}
+}
+
+func (r *mucRoster) addItem(item *mucAccountContact) {
+	cs := r.u.currentColorSet()
+	iter := r.model.Append()
+
+	setValues(
+		r.model,
+		iter,
+		item.id,
+		item.displayName(),
+		"BelongsTo",
+		decideColorForContact(cs, item),
+		cs.rosterPeerBackground,
+		nil,
+		createTooltipForContact(item),
+	)
 }
 
 func (a *mucAccount) isOffline() bool {
@@ -157,4 +229,40 @@ func (a *mucAccount) getStatus() string {
 	}
 
 	return "available"
+}
+
+func (c *mucAccountContact) displayName() string {
+	if c.name != "" {
+		return c.name
+	}
+
+	return c.id
+}
+
+func (c *mucAccountContact) isOnline() bool {
+	return c.status == mucStatusOnline
+}
+
+func setValues(v gtki.ListStore, iter gtki.TreeIter, values ...interface{}) {
+	for i, val := range values {
+		if val != nil {
+			_ = v.SetValue(iter, i, val)
+		}
+	}
+}
+
+func decideColorForContact(cs colorSet, c *mucAccountContact) string {
+	if !c.isOnline() {
+		return cs.rosterPeerOfflineForeground
+	}
+	return cs.rosterPeerOnlineForeground
+}
+
+func createTooltipForContact(c *mucAccountContact) string {
+	pname := html.EscapeString(c.displayName())
+	jid := html.EscapeString(c.id)
+	if pname != jid {
+		return fmt.Sprintf("%s (%s)", pname, jid)
+	}
+	return jid
 }

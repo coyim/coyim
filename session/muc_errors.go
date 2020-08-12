1a981adb4daca91c30ef61a8d2d23f223db73a73
Pedro Palau <pepalauisaac@gmail.com>
2020-08-12 16:14:42 -0500

    Updates MUC-error publishing by using the new logic and avoids using "super class" with pointers

--- session/muc_errors.go
+++ session/muc_errors.go
@@ -6,29 +6,29 @@ import (
 )
 
 func (s *session) publishMUCError(stanza *data.ClientPresence) {
-	e := events.MUCErrorEvent{
-		MUC: &events.MUC{
-			From: stanza.From,
-		},
+	e := events.MUCError{}
+	e.EventInfo = events.MUCInfo{
+		From: stanza.From,
 	}
 
 	switch {
 	case stanza.Error.MUCNotAuthorized != nil:
-		e.Event = events.MUCNotAuthorized
+		e.EventType = events.MUCNotAuthorized
 	case stanza.Error.MUCForbidden != nil:
-		e.Event = events.MUCForbidden
+		e.EventType = events.MUCForbidden
 	case stanza.Error.MUCItemNotFound != nil:
-		e.Event = events.MUCItemNotFound
+		e.EventType = events.MUCItemNotFound
 	case stanza.Error.MUCNotAllowed != nil:
-		e.Event = events.MUCNotAllowed
+		e.EventType = events.MUCNotAllowed
 	case stanza.Error.MUCNotAceptable != nil:
-		e.Event = events.MUCNotAceptable
+		e.EventType = events.MUCNotAceptable
 	case stanza.Error.MUCRegistrationRequired != nil:
-		e.Event = events.MUCRegistrationRequired
+		e.EventType = events.MUCRegistrationRequired
 	case stanza.Error.MUCConflict != nil:
-		e.Event = events.MUCConflict
+		e.EventType = events.MUCConflict
 	case stanza.Error.MUCServiceUnavailable != nil:
-		e.Event = events.MUCServiceUnavailable
+		e.EventType = events.MUCServiceUnavailable
 	}
+
 	s.publishEvent(e)
 }

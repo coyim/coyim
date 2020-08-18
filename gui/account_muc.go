8988d4953498e639d3df89ce0e10d8b64b816016
Pedro Palau <pepalauisaac@gmail.com>
2020-08-18 08:16:24 -0500

    Does small changes in MUC-specific events handlers

--- gui/account_muc.go
+++ gui/account_muc.go
@@ -19,16 +19,18 @@ func (a *account) getRoomView(rid jid.Bare) (*roomView, *muc.Room, error) {
 	return rv, room, nil
 }
 
-func (a *account) enrollNewOccupantRoomEvent(ev events.MUCOccupantJoined) {
-	rid := ev.From
-	rv, room, err := a.getRoomView(rid)
+func (a *account) enrollNewOccupantRoomEvent(from jid.Bare, ev events.MUCOccupantJoined) {
+	rv, room, err := a.getRoomView(from)
 	if err != nil {
-		a.log.WithError(err).Debug()
+		a.log.WithError(err).Error("An error ocurred while trying to change the room occupant status.")
+		return
 	}
-	// Updating the room occupant in the room manager
-	fjid := ev.From.WithResource(jid.Resource(ev.Nickname))
+
+	fjid := from.WithResource(jid.Resource(ev.Nickname))
 	realjid := ev.Jid
+
 	room.Roster().UpdatePresence(fjid, "", ev.Affiliation, ev.Role, "", ev.Status, "Room Joined", realjid)
+
 	rv.processOccupantJoinedEvent(err)
 }
 
@@ -37,13 +39,15 @@ func (a *account) updateOccupantRoomEvent(ev events.MUCOccupantUpdated) {
 	a.log.Debug("updateOccupantRoomEvent")
 }
 
-func (a *account) errorNewOccupantRoomEvent(ev events.MUCError) {
-	ridwr, nickname := ev.EventInfo.From.PotentialSplit()
+func (a *account) errorNewOccupantRoomEvent(ev events.MUC) {
+	ridwr, nickname := ev.From.PotentialSplit()
 	rid := ridwr.(jid.Bare)
 	rv, _, err := a.getRoomView(rid)
 	if err != nil {
-		a.log.WithError(err).Debug()
+		a.log.WithError(err).Error("An error occurred ")
+		return
 	}
+
 	err = muc.NewNicknameConflictError(nickname)
 	rv.processOccupantJoinedEvent(err)
 }

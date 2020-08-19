569264fc10219085b77c3564e12a01c7b305ecb2
Reinaldo Mendoza <rmendoza83@gmail.com>
2020-08-19 08:59:24 -0500

    Fixes on account_muc

--- gui/account_muc.go
+++ gui/account_muc.go
@@ -8,8 +8,6 @@ import (
 	"github.com/coyim/coyim/xmpp/jid"
 )
 
-// TODO[OB]-MUC: I think this filename should be muc_account instead of account_muc
-
 func (a *account) roomViewFor(rid jid.Bare) (*roomView, *muc.Room, error) {
 	room, exists := a.roomManager.GetRoom(rid)
 	if !exists {
@@ -21,22 +19,22 @@ func (a *account) roomViewFor(rid jid.Bare) (*roomView, *muc.Room, error) {
 	return rv, room, nil
 }
 
-// TODO[OB]-MUC: I'm not a huge fan of this method name
-
-func (a *account) enrollNewOccupantRoomEvent(from jid.Full, ev events.MUCOccupantJoined) {
-	rv, room, err := a.roomViewFor(ev.Room)
+func (a *account) addOccupantToRoster(from jid.Full, userj jid.Full, affiliation, role, status string) {
+	roomj := jid.NewBare(from.Local(), from.Host())
+	rv, room, err := a.roomViewFor(roomj)
 	if err != nil {
-		a.log.WithError(err).Error("An error occurred while trying to change the room occupant status.")
+		rv.lastErrorMessage = err.Error()
+		a.log.WithError(err).Error("An error occurred trying to get the room view")
+		rv.onJoin <- false
 		return
 	}
 
-	fjid := from.WithResource(ev.Nickname)
-	realjid := ev.Jid
-
-	// TODO[OB]-MUC: You should not ignore the results of this call
-	room.Roster().UpdatePresence(fjid, "", ev.Affiliation, ev.Role, "", ev.Status, "Room Joined", realjid)
-
-	rv.processOccupantJoinedEvent(err)
+	joined, _, err := room.Roster().UpdatePresence(from, "", affiliation, role, "", status, "Room Joined", userj)
+	if err != nil {
+		rv.lastErrorMessage = err.Error()
+		a.log.WithError(err).Error("An error occurred trying to add the occupant to the roster")
+	}
+	rv.onJoin <- joined
 }
 
 func (a *account) updateOccupantRoomEvent(ev events.MUCOccupantUpdated) {
@@ -44,23 +42,19 @@ func (a *account) updateOccupantRoomEvent(ev events.MUCOccupantUpdated) {
 	a.log.Debug("updateOccupantRoomEvent")
 }
 
-// TODO[OB]-MUC: I'm not a fan of this method name either
-
-func (a *account) errorNewOccupantRoomEvent(from jid.Full, ev events.MUCError) {
-	// TODO[OB]-MUC: It doesn't make sense to use PotentialSplit here, since you are assuming the nickname exists, you
-	// should make sure the From is a full JID and use Split() instead
-	ridwr, nickname := from.PotentialSplit()
+func (a *account) generateNicknameConflictError(from jid.Full) {
+	ridwr, nickname := from.Split()
 	rid := ridwr.(jid.Bare)
 	rv, _, err := a.roomViewFor(rid)
 	if err != nil {
-		// TODO[OB]-MUC: It might be better to say something about WHEN the error occurred... This is not Java, we don't have backtraces on exceptions
-		a.log.WithError(err).Error("An error occurred ")
+		rv.lastErrorMessage = err.Error()
+		a.log.WithError(err).Error("An error occurred trying to get the room view")
+		rv.onJoin <- false
 		return
 	}
-
+	// Generating a custom error for the nickname conflict event received
 	err = muc.NewNicknameConflictError(nickname)
-
-	// TODO[OB]-MUC: I don't understand this at all. Why are you sending in the err here instead of processing it here?
-	// The processOccupantJoinedEvent doesn't seem like it should take an error as argument. So it seems like the method is badly named?
-	rv.processOccupantJoinedEvent(err)
+	a.log.WithError(err).Error("Room join event received")
+	rv.lastErrorMessage = err.Error()
+	rv.onJoin <- false
 }

f1bdba3f0ab615928234b6ddabffd5563d08e0f3
Pedro Palau <pepalauisaac@gmail.com>
2020-08-20 13:59:09 -0500

    Fixes typo

--- gui/muc.go
+++ gui/muc.go
@@ -12,7 +12,7 @@ func initMUC() {
 var supportedCreateMUCErrors map[error]string
 
 // We should put here all MUC-related errors that we want to support
-// and have a custom a useful user message for each one
+// and have a custom and useful user message for each one
 func initMUCSupportedErrors() {
 	supportedCreateMUCErrors = map[error]string{
 		session.ErrInvalidInformationQueryRequest: i18n.Local("Couldn't send the information query to the server, please try again."),

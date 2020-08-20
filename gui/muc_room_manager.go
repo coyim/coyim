638b7fe6df4d4e79fc45b1d67980076da5d0975d
Pedro Palau <pepalauisaac@gmail.com>
2020-08-20 13:31:27 -0500

    We aren't using this anymore

--- gui/muc_room_manager.go
+++ gui/muc_room_manager.go
@@ -1,28 +1,9 @@
 package gui
 
 import (
-	"errors"
-
 	"github.com/coyim/coyim/session/muc"
-	"github.com/coyim/coyim/xmpp/jid"
 )
 
 func newRoomManager() *muc.RoomManager {
 	return muc.NewRoomManager()
-}
-
-func (u *gtkUI) addRoom(a *account, ident jid.Bare) (*muc.Room, error) {
-	// TODO[OB]-MUC: Something about the logic in this method seems weird
-
-	_, exists := a.roomManager.GetRoom(ident)
-	if exists {
-		return nil, errors.New("the room is already in the manager")
-	}
-
-	r := u.newRoom(ident)
-	if !a.roomManager.AddRoom(r) {
-		return nil, errors.New("the room is already in the manager")
-	}
-
-	return r, nil
 }

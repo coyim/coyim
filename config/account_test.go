20bf8590377d69038e7a1164f2c8ab21cff21792
Ola Bini <ola@olabini.se>
2020-08-21 17:51:35 +0000

    Add some testing for accounts

--- config/account_test.go
+++ config/account_test.go
@@ -19,7 +19,10 @@ func (s *AccountXMPPSuite) Test_Account_Is_recognizesJids(c *C) {
 
 func (s *AccountXMPPSuite) Test_Account_ShouldEncryptTo(c *C) {
 	a := &Account{Account: "hello@bar.com", AlwaysEncrypt: false, AlwaysEncryptWith: []string{"one@foo.com", "two@foo.com"}}
-	a2 := &Account{Account: "hello@bar.com", AlwaysEncrypt: true, AlwaysEncryptWith: []string{"one@foo.com", "two@foo.com"}}
+	a2 := &Account{Account: "hello@bar.com", AlwaysEncrypt: true, AlwaysEncryptWith: []string{"one@foo.com", "two@foo.com"}, Peers: []*Peer{
+		&Peer{UserID: "some@one.com", Nickname: "Hellois", EncryptionSettings: NeverEncrypt},
+		&Peer{UserID: "some2@one.com", Nickname: "Hellois", EncryptionSettings: AlwaysEncrypt},
+	}}
 	c.Check(a.ShouldEncryptTo("foo"), Equals, false)
 	c.Check(a.ShouldEncryptTo("hello@bar.com"), Equals, false)
 	c.Check(a.ShouldEncryptTo("one@foo.com"), Equals, true)
@@ -27,6 +30,9 @@ func (s *AccountXMPPSuite) Test_Account_ShouldEncryptTo(c *C) {
 	c.Check(a.ShouldEncryptTo("two@foo.com/blarg"), Equals, true)
 	c.Check(a2.ShouldEncryptTo("foo"), Equals, true)
 	c.Check(a2.ShouldEncryptTo("hello@bar.com"), Equals, true)
+
+	c.Assert(a2.ShouldEncryptTo("some@one.com"), Equals, false)
+	c.Assert(a2.ShouldEncryptTo("some2@one.com"), Equals, true)
 }
 
 func (s *AccountXMPPSuite) Test_NewAccount_ReturnsNewAccountWithSafeDefaults(c *C) {
@@ -100,4 +106,42 @@ func (s *AccountXMPPSuite) Test_ID_doesNotChangeID(c *C) {
 		id: "existing",
 	}
 	c.Check(a.ID(), Equals, "existing")
+}
+
+func (s *AccountXMPPSuite) Test_Account_AllPrivateKeys(c *C) {
+	a := &Account{
+		id: "existing",
+	}
+	_, _ = a.EnsurePrivateKey()
+	c.Assert(len(a.AllPrivateKeys()), Equals, 1)
+
+	a.DeprecatedPrivateKey = []byte{1, 2, 3}
+	c.Assert(len(a.AllPrivateKeys()), Equals, 2)
+}
+
+func (s *AccountXMPPSuite) Test_Account_ToggleAlwaysEncrypt(c *C) {
+	a := &Account{
+		id: "existing",
+	}
+
+	a.ToggleAlwaysEncrypt()
+	c.Assert(a.AlwaysEncrypt, Equals, true)
+}
+
+func (s *AccountXMPPSuite) Test_Account_ToggleConnectAutomatically(c *C) {
+	a := &Account{
+		id: "existing",
+	}
+
+	a.ToggleConnectAutomatically()
+	c.Assert(a.ConnectAutomatically, Equals, true)
+}
+
+func (s *AccountXMPPSuite) Test_Account_SaveCert(c *C) {
+	a := &Account{
+		id: "existing",
+	}
+
+	a.SaveCert("foo", "bar", []byte{1, 2, 3, 4})
+	c.Assert(a.Certificates[0], DeepEquals, &CertificatePin{Subject: "foo", Issuer: "bar", Fingerprint: []byte{0x1, 0x2, 0x3, 0x4}, FingerprintType: "SHA3-256"})
 }

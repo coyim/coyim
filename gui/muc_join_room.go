279d57a6f50d1e67e9ddaeb622c72109f0db4883
Pedro Palau <pepalauisaac@gmail.com>
2020-08-24 13:23:31 -0500

    Updates `join room` functionalities naming to be more consistent

--- gui/muc_join_room.go
+++ gui/muc_join_room.go
@@ -19,59 +19,59 @@ type mucJoinRoomView struct {
 	errorNotif       *errorNotification
 }
 
-func (jrv *mucJoinRoomView) clearErrors() {
-	jrv.errorNotif.Hide()
+func (v *mucJoinRoomView) clearErrors() {
+	v.errorNotif.Hide()
 }
 
-func (jrv *mucJoinRoomView) notifyOnError(err string) {
-	if jrv.notification != nil {
-		jrv.notificationArea.Remove(jrv.notification)
+func (v *mucJoinRoomView) notifyOnError(err string) {
+	if v.notification != nil {
+		v.notificationArea.Remove(v.notification)
 	}
 
-	jrv.errorNotif.ShowMessage(err)
+	v.errorNotif.ShowMessage(err)
 }
 
-func (jrv *mucJoinRoomView) startSpinner() {
-	jrv.spinner.Start()
-	jrv.spinner.SetVisible(true)
+func (v *mucJoinRoomView) startSpinner() {
+	v.spinner.Start()
+	v.spinner.SetVisible(true)
 }
 
-func (jrv *mucJoinRoomView) stopSpinner() {
-	jrv.spinner.Stop()
-	jrv.spinner.SetVisible(false)
+func (v *mucJoinRoomView) stopSpinner() {
+	v.spinner.Stop()
+	v.spinner.SetVisible(false)
 }
 
-func (jrv *mucJoinRoomView) hasValidRoomName() bool {
-	jrv.clearErrors()
-	roomName, _ := jrv.roomNameEntry.GetText()
+func (v *mucJoinRoomView) hasValidRoomName() bool {
+	v.clearErrors()
+	roomName, _ := v.roomNameEntry.GetText()
 	valid := jid.ValidBareJID(roomName)
 	if !valid {
 		if len(roomName) > 0 {
-			jrv.notifyOnError(i18n.Localf("\"%s\" is not a valid room identification", roomName))
+			v.notifyOnError(i18n.Localf("\"%s\" is not a valid room identification", roomName))
 		}
 	}
 	return valid
 }
 
-func (jrv *mucJoinRoomView) validateInput() {
-	sensitiveValue := jrv.hasValidRoomName()
-	jrv.joinButton.SetSensitive(sensitiveValue)
+func (v *mucJoinRoomView) validateInput() {
+	sensitiveValue := v.hasValidRoomName()
+	v.joinButton.SetSensitive(sensitiveValue)
 }
 
-func (jrv *mucJoinRoomView) notifyErrorServerUnavailable(a *account, roomName string) {
-	jrv.stopSpinner()
-	jrv.notifyOnError(i18n.Local("We can't get access to the server, please check your Internet connection or make sure the server exists."))
+func (v *mucJoinRoomView) notifyErrorServerUnavailable(a *account, roomName string) {
+	v.stopSpinner()
+	v.notifyOnError(i18n.Local("We can't get access to the server, please check your Internet connection or make sure the server exists."))
 	a.log.WithField("room", roomName).Warn("An error ocurred trying to find a room")
 }
 
-func (jrv *mucJoinRoomView) tryJoinRoom(a *account) {
+func (v *mucJoinRoomView) tryJoinRoom(a *account) {
 	// TODO[OB]-MUC: I don't think using a mutex here is a good idea
 	// Since this is in the UI thread, there are probably better ways to deal with it
-	jrv.clearErrors()
+	v.clearErrors()
 
-	roomName, _ := jrv.roomNameEntry.GetText()
+	roomName, _ := v.roomNameEntry.GetText()
 	rj := jid.ParseBare(roomName)
-	jrv.startSpinner()
+	v.startSpinner()
 
 	rc, ec := a.session.HasRoom(rj)
 	go func() {
@@ -79,31 +79,31 @@ func (jrv *mucJoinRoomView) tryJoinRoom(a *account) {
 		case value, ok := <-rc:
 			if !ok {
 				doInUIThread(func() {
-					jrv.notifyErrorServerUnavailable(a, roomName)
+					v.notifyErrorServerUnavailable(a, roomName)
 				})
 				return
 			}
 			doInUIThread(func() {
-				jrv.stopSpinner()
+				v.stopSpinner()
 				if !value {
-					jrv.notifyOnError(i18n.Localf("The room \"%s\" doesn't exist", roomName))
+					v.notifyOnError(i18n.Localf("The room \"%s\" doesn't exist", roomName))
 					a.log.WithField("room", roomName).Debug("The room doesn't exist")
 				} else {
-					jrv.dialog.Hide()
-					jrv.u.mucShowRoom(a, rj)
+					v.dialog.Hide()
+					v.u.mucShowRoom(a, rj)
 				}
 			})
 		case err, ok := <-ec:
 			if !ok {
 				doInUIThread(func() {
-					jrv.notifyErrorServerUnavailable(a, roomName)
+					v.notifyErrorServerUnavailable(a, roomName)
 				})
 				return
 			}
 			doInUIThread(func() {
-				jrv.stopSpinner()
+				v.stopSpinner()
 				if err != nil {
-					jrv.notifyOnError(i18n.Local("Looks like the server or the service doesn't exists, please verify the provided name."))
+					v.notifyOnError(i18n.Local("Looks like the server or the service doesn't exists, please verify the provided name."))
 					a.log.WithField("room", roomName).WithError(err).Warn("An error occurred trying to find the room")
 				}
 			})
@@ -111,24 +111,24 @@ func (jrv *mucJoinRoomView) tryJoinRoom(a *account) {
 	}()
 }
 
-func (jrv *mucJoinRoomView) init() {
-	jrv.builder = newBuilder("MUCJoinRoomDialog")
+func (v *mucJoinRoomView) init() {
+	v.builder = newBuilder("MUCJoinRoomDialog")
 
-	panicOnDevError(jrv.builder.bindObjects(jrv))
+	panicOnDevError(v.builder.bindObjects(v))
 
-	accountsInput := jrv.builder.get("accounts").(gtki.ComboBox)
-	ac := jrv.u.createConnectedAccountsComponent(accountsInput, jrv, nil, jrv.stopSpinner)
+	accountsInput := v.builder.get("accounts").(gtki.ComboBox)
+	ac := v.u.createConnectedAccountsComponent(accountsInput, v, nil, v.stopSpinner)
 
-	jrv.builder.ConnectSignals(map[string]interface{}{
+	v.builder.ConnectSignals(map[string]interface{}{
 		"on_close_window":     ac.onDestroy,
-		"on_roomname_changed": jrv.validateInput,
-		"on_cancel_clicked":   jrv.dialog.Destroy,
+		"on_roomname_changed": v.validateInput,
+		"on_cancel_clicked":   v.dialog.Destroy,
 		"on_join_clicked": func() {
-			jrv.tryJoinRoom(ac.currentAccount())
+			v.tryJoinRoom(ac.currentAccount())
 		},
 	})
 
-	jrv.errorNotif = newErrorNotification(jrv.notificationArea)
+	v.errorNotif = newErrorNotification(v.notificationArea)
 }
 
 func newMUCJoinRoomView(u *gtkUI) *mucJoinRoomView {

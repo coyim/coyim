790c719d171b78a891084b5669d68f52dd0d22c2
Pedro Palau <pepalauisaac@gmail.com>
2020-08-24 15:36:00 -0500

    Does more refactoring in the `join-room` functionality Now we use a specific "join-room-context" to have a better control over the functionality

--- gui/muc_join_room.go
+++ gui/muc_join_room.go
@@ -75,59 +75,107 @@ func (v *mucJoinRoomView) validateInput() {
 	v.joinButton.SetSensitive(v.hasValidRoomName())
 }
 
-func (v *mucJoinRoomView) notifyErrorServerUnavailable(a *account, roomName string) {
-	v.stopSpinner()
-	v.notifyOnError(i18n.Local("We can't get access to the server, please check your Internet connection or make sure the server exists."))
-	a.log.WithField("room", roomName).Warn("An error ocurred trying to find the room")
+func (v *mucJoinRoomView) onBeforeStart() {
+	doInUIThread(func() {
+		v.clearErrors()
+		v.startSpinner()
+	})
+}
+
+func (v *mucJoinRoomView) onJoinSuccess(a *account, ident jid.Bare, s bool) {
+	doInUIThread(v.stopSpinner)
+
+	if !s {
+		doInUIThread(func() {
+			v.notifyOnError(i18n.Localf("The room \"%s\" doesn't exist", ident))
+		})
+		a.log.WithField("room", ident).Debug("The room doesn't exist")
+		return
+	}
+
+	doInUIThread(func() {
+		v.dialog.Hide()
+		v.u.mucShowRoom(a, ident)
+	})
+}
+
+func (v *mucJoinRoomView) onJoinError(a *account, ident jid.Bare, err error) {
+	doInUIThread(func() {
+		v.stopSpinner()
+		if err != nil {
+			v.notifyOnError(i18n.Local("Looks like the server or the service doesn't exists, please verify the provided name."))
+			a.log.WithField("room", ident).WithError(err).Warn("An error occurred trying to find the room")
+		}
+	})
+}
+
+func (v *mucJoinRoomView) onJoinServerUnavailable(a *account, ident jid.Bare) {
+	doInUIThread(func() {
+		v.stopSpinner()
+		v.notifyOnError(i18n.Local("We can't get access to the server, please check your Internet connection or make sure the server exists."))
+	})
+	a.log.WithField("room", ident).Warn("An error ocurred trying to find the room")
+}
+
+type mucJoinRoomContext struct {
+	a     *account
+	v     *mucJoinRoomView
+	ident jid.Bare
+	// onBeforeStart IS called from the UI thread
+	onBeforeStart func()
+	// onSuccess is NOT called from the UI thread
+	onSuccess func(*account, jid.Bare, bool)
+	// onError is NOT called from the UI thread
+	onError func(*account, jid.Bare, error)
+	// onServiceUnavailable is NOT called from the UI thread
+	onServiceUnavailable func(*account, jid.Bare)
+}
+
+func (c *mucJoinRoomContext) onFinishWithResult(s, isChannelClosed bool) {
+	if !isChannelClosed {
+		c.onServiceUnavailable(c.a, c.ident)
+		return
+	}
+	c.onSuccess(c.a, c.ident, s)
+}
+
+func (c *mucJoinRoomContext) onFinishWithError(err error, isErrorChannelClosed bool) {
+	if !isErrorChannelClosed {
+		c.onServiceUnavailable(c.a, c.ident)
+		return
+	}
+	c.onError(c.a, c.ident, err)
+}
+
+func (c *mucJoinRoomContext) waitToFinish(resultChannel <-chan bool, errorChannel <-chan error) {
+	select {
+	case value, ok := <-resultChannel:
+		c.onFinishWithResult(value, ok)
+	case err, ok := <-errorChannel:
+		c.onFinishWithError(err, ok)
+	}
+}
+
+func (c *mucJoinRoomContext) exec() {
+	c.onBeforeStart()
+	resultChannel, errorChannel := c.a.session.HasRoom(c.ident)
+	go c.waitToFinish(resultChannel, errorChannel)
 }
 
 func (v *mucJoinRoomView) tryJoinRoom() {
 	// TODO[OB]-MUC: I don't think using a mutex here is a good idea
 	// Since this is in the UI thread, there are probably better ways to deal with it
-	a := v.ac.currentAccount()
+	c := &mucJoinRoomContext{
+		a:                    v.ac.currentAccount(),
+		v:                    v,
+		ident:                jid.ParseBare(v.typedRoomName()),
+		onBeforeStart:        v.onBeforeStart,
+		onSuccess:            v.onJoinSuccess,
+		onError:              v.onJoinError,
+		onServiceUnavailable: v.onJoinServerUnavailable,
+	}
 
-	v.clearErrors()
-
-	roomName, _ := v.roomNameEntry.GetText()
-	rj := jid.ParseBare(roomName)
-	v.startSpinner()
-
-	rc, ec := a.session.HasRoom(rj)
-	go func() {
-		select {
-		case value, ok := <-rc:
-			if !ok {
-				doInUIThread(func() {
-					v.notifyErrorServerUnavailable(a, roomName)
-				})
-				return
-			}
-			doInUIThread(func() {
-				v.stopSpinner()
-				if !value {
-					v.notifyOnError(i18n.Localf("The room \"%s\" doesn't exist", roomName))
-					a.log.WithField("room", roomName).Debug("The room doesn't exist")
-				} else {
-					v.dialog.Hide()
-					v.u.mucShowRoom(a, rj)
-				}
-			})
-		case err, ok := <-ec:
-			if !ok {
-				doInUIThread(func() {
-					v.notifyErrorServerUnavailable(a, roomName)
-				})
-				return
-			}
-			doInUIThread(func() {
-				v.stopSpinner()
-				if err != nil {
-					v.notifyOnError(i18n.Local("Looks like the server or the service doesn't exists, please verify the provided name."))
-					a.log.WithField("room", roomName).WithError(err).Warn("An error occurred trying to find the room")
-				}
-			})
-		}
-	}()
+	c.exec()
 }
 
 func (v *mucJoinRoomView) init() {

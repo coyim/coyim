283d0d4cf16327fbe778717afa6a098864c7facc
Pedro Palau <pepalauisaac@gmail.com>
2020-08-27 10:52:50 -0500

    Some refactoring work in the muc `public rooms` functionality

--- gui/muc_public_rooms.go
+++ gui/muc_public_rooms.go
@@ -36,7 +36,9 @@ func (u *gtkUI) updatedRoomListing(rl *muc.RoomListing, data interface{}) {
 }
 
 type mucPublicRoomsView struct {
+	u       *gtkUI
 	builder *builder
+	ac      *connectedAccountsComponent
 
 	generation    int
 	updateLock    sync.RWMutex
@@ -58,6 +60,251 @@ type mucPublicRoomsView struct {
 	errorNotif   *errorNotification
 }
 
+func newMUCPublicRoomsView(u *gtkUI) *mucPublicRoomsView {
+	view := &mucPublicRoomsView{u: u}
+	view.init()
+	return view
+}
+
+func (prv *mucPublicRoomsView) init() {
+	prv.initBuilder()
+	prv.initConnectedAccountsComponent()
+	prv.initEvents()
+	prv.initCommons()
+}
+
+func (prv *mucPublicRoomsView) initBuilder() {
+	prv.builder = newBuilder("MUCPublicRoomsDialog")
+	panicOnDevError(prv.builder.bindObjects(prv))
+	prv.errorNotif = newErrorNotification(prv.notificationArea)
+}
+
+func (prv *mucPublicRoomsView) initConnectedAccountsComponent() {
+	accountsInput := prv.builder.get("accounts").(gtki.ComboBox)
+	ac := prv.u.createConnectedAccountsComponent(accountsInput, prv,
+		func(acc *account) {
+			go prv.mucUpdatePublicRoomsOn(acc)
+		},
+		func() {
+			prv.rooms.SetVisible(false)
+			prv.spinner.Stop()
+			prv.spinner.SetVisible(false)
+			prv.roomsModel.Clear()
+			prv.refreshButton.SetSensitive(false)
+			prv.customServiceButton.SetSensitive(false)
+		},
+	)
+	prv.ac = ac
+}
+
+func (prv *mucPublicRoomsView) initEvents() {
+	prv.builder.ConnectSignals(map[string]interface{}{
+		"on_cancel":            prv.dialog.Destroy,
+		"on_close_window":      prv.ac.onDestroy,
+		"on_join":              prv.onJoinRoom,
+		"on_activate_room_row": prv.onActivateRoomRow,
+		"on_selection_changed": prv.onSelectionChanged,
+		"on_custom_service":    prv.onUpdatePublicRooms,
+		"on_refresh":           prv.onUpdatePublicRooms,
+	})
+}
+
+func (prv *mucPublicRoomsView) initCommons() {
+	prv.serviceGroups = make(map[string]gtki.TreeIter)
+	prv.joinButton.SetSensitive(false)
+}
+
+func (prv *mucPublicRoomsView) onJoinRoom() {
+	selection, err := prv.roomsTree.GetSelection()
+	if err != nil {
+		// TODO: we should inform the user
+		prv.u.log.WithError(err).Debug("couldn't join")
+		return
+	}
+
+	_, iter, ok := selection.GetSelected()
+	if !ok {
+		// TODO: we should inform the user
+		prv.u.log.Debug("nothing is selected")
+		return
+	}
+
+	val, _ := prv.roomsModel.GetValue(iter, mucListRoomsIndexJid)
+	v, _ := val.GetString()
+
+	_, ok = prv.serviceGroups[v]
+	if ok {
+		// TODO: we should inform the user
+		prv.u.log.Debug("a service is selected, not a room, so we can't activate it")
+		return
+	}
+
+	go prv.joinRoom(v)
+}
+
+func (prv *mucPublicRoomsView) onActivateRoomRow(_ gtki.TreeView, path gtki.TreePath) {
+	iter, err := prv.roomsModel.GetIter(path)
+	if err != nil {
+		prv.u.log.WithError(err).Debug("couldn't activate")
+		return
+	}
+	val, _ := prv.roomsModel.GetValue(iter, mucListRoomsIndexJid)
+	v, _ := val.GetString()
+	_, ok := prv.serviceGroups[v]
+	if ok {
+		prv.u.log.Debug("a service is selected, not a room, so we can't activate it")
+		return
+	}
+	go prv.joinRoom(v)
+}
+
+func (prv *mucPublicRoomsView) onSelectionChanged() {
+	selection, err := prv.roomsTree.GetSelection()
+	if err != nil {
+		prv.u.log.WithError(err).Debug("problem getting selection")
+		return
+	}
+	_, iter, ok := selection.GetSelected()
+	if !ok {
+		prv.joinButton.SetSensitive(false)
+		return
+	}
+
+	val, _ := prv.roomsModel.GetValue(iter, mucListRoomsIndexJid)
+	v, _ := val.GetString()
+	_, ok = prv.serviceGroups[v]
+	if ok {
+		prv.joinButton.SetSensitive(false)
+	} else {
+		prv.joinButton.SetSensitive(true)
+	}
+}
+
+func (prv *mucPublicRoomsView) onUpdatePublicRooms() {
+	go prv.mucUpdatePublicRoomsOn(prv.ac.currentAccount())
+}
+
+// mucUpdatePublicRoomsOn should NOT be called from the UI thread
+func (prv *mucPublicRoomsView) mucUpdatePublicRoomsOn(a *account) {
+	if prv.cancel != nil {
+		prv.cancel <- true
+	}
+
+	prv.updateLock.Lock()
+
+	doInUIThread(prv.clearErrors)
+
+	customService, _ := prv.customService.GetText()
+
+	prv.cancel = make(chan bool, 1)
+
+	doInUIThread(func() {
+		prv.rooms.SetVisible(false)
+		prv.spinner.Start()
+		prv.spinner.SetVisible(true)
+		prv.roomsModel.Clear()
+		prv.refreshButton.SetSensitive(true)
+		prv.customServiceButton.SetSensitive(true)
+	})
+	prv.generation++
+	prv.serviceGroups = make(map[string]gtki.TreeIter)
+
+	// We save the generation value here, in case it gets modified inside the view later
+	gen := prv.generation
+
+	res, resServices, ec := a.session.GetRooms(jid.Parse(a.Account()).Host(), customService)
+	go func() {
+		hasSomething := false
+
+		defer func() {
+			if !hasSomething {
+				doInUIThread(func() {
+					prv.spinner.Stop()
+					prv.spinner.SetVisible(false)
+					prv.rooms.SetVisible(true)
+					if customService != "" {
+						prv.notifyOnError(i18n.Local("That service doesn't seem to exist"))
+					} else {
+						prv.notifyOnError(i18n.Local("Your XMPP server doesn't seem to have any chat room services"))
+					}
+				})
+			}
+
+			prv.updateLock.Unlock()
+		}()
+		for {
+			select {
+			case sl, ok := <-resServices:
+				if !ok {
+					return
+				}
+				if !hasSomething {
+					hasSomething = true
+					doInUIThread(func() {
+						prv.spinner.Stop()
+						prv.spinner.SetVisible(false)
+						prv.rooms.SetVisible(true)
+					})
+				}
+
+				serv, ok := prv.serviceGroups[sl.Jid.String()]
+				if !ok {
+					doInUIThread(func() {
+						serv = prv.roomsModel.Append(nil)
+						prv.serviceGroups[sl.Jid.String()] = serv
+						_ = prv.roomsModel.SetValue(serv, mucListRoomsIndexJid, sl.Jid.String())
+						_ = prv.roomsModel.SetValue(serv, mucListRoomsIndexName, sl.Name)
+					})
+				}
+			case rl, ok := <-res:
+				if !ok || rl == nil {
+					return
+				}
+
+				if !hasSomething {
+					hasSomething = true
+					doInUIThread(func() {
+						prv.spinner.Stop()
+						prv.spinner.SetVisible(false)
+						prv.rooms.SetVisible(true)
+					})
+				}
+
+				serv, ok := prv.serviceGroups[rl.Service.String()]
+				doInUIThread(func() {
+					if !ok {
+						serv = prv.roomsModel.Append(nil)
+						prv.serviceGroups[rl.Service.String()] = serv
+						_ = prv.roomsModel.SetValue(serv, mucListRoomsIndexJid, rl.Service.String())
+						_ = prv.roomsModel.SetValue(serv, mucListRoomsIndexName, rl.ServiceName)
+					}
+
+					iter := prv.roomsModel.Append(serv)
+					_ = prv.roomsModel.SetValue(iter, mucListRoomsIndexJid, rl.Jid.Local().String())
+					_ = prv.roomsModel.SetValue(iter, mucListRoomsIndexName, rl.Name)
+					_ = prv.roomsModel.SetValue(iter, mucListRoomsIndexService, rl.Service.String())
+					rl.OnUpdate(prv.u.updatedRoomListing, &roomListingUpdateData{iter, prv, gen})
+
+					prv.roomsTree.ExpandAll()
+				})
+			case e, ok := <-ec:
+				if !ok {
+					return
+				}
+				if e != nil {
+					doInUIThread(func() {
+						prv.notifyOnError(i18n.Local("Something went wrong when trying to get chat rooms"))
+					})
+					prv.u.log.WithError(e).Debug("something went wrong trying to get chat rooms")
+				}
+				return
+			case _, _ = <-prv.cancel:
+				return
+			}
+		}
+	}()
+}
+
 func (prv *mucPublicRoomsView) clearErrors() {
 	prv.errorNotif.Hide()
 }
@@ -70,233 +317,25 @@ func (prv *mucPublicRoomsView) notifyOnError(err string) {
 	prv.errorNotif.ShowMessage(err)
 }
 
-func (prv *mucPublicRoomsView) init() {
-	prv.builder = newBuilder("MUCPublicRoomsDialog")
-	panicOnDevError(prv.builder.bindObjects(prv))
-	prv.serviceGroups = make(map[string]gtki.TreeIter)
-	prv.errorNotif = newErrorNotification(prv.notificationArea)
-}
-
-// mucUpdatePublicRoomsOn should NOT be called from the UI thread
-func (u *gtkUI) mucUpdatePublicRoomsOn(view *mucPublicRoomsView, a *account) {
-	if view.cancel != nil {
-		view.cancel <- true
-	}
-
-	view.updateLock.Lock()
-
-	doInUIThread(view.clearErrors)
-
-	customService, _ := view.customService.GetText()
-
-	view.cancel = make(chan bool, 1)
-
-	doInUIThread(func() {
-		view.rooms.SetVisible(false)
-		view.spinner.Start()
-		view.spinner.SetVisible(true)
-		view.roomsModel.Clear()
-		view.refreshButton.SetSensitive(true)
-		view.customServiceButton.SetSensitive(true)
-	})
-	view.generation++
-	view.serviceGroups = make(map[string]gtki.TreeIter)
-
-	// We save the generation value here, in case it gets modified inside the view later
-	gen := view.generation
-
-	res, resServices, ec := a.session.GetRooms(jid.Parse(a.Account()).Host(), customService)
-	go func() {
-		hasSomething := false
-
-		defer func() {
-			if !hasSomething {
-				doInUIThread(func() {
-					view.spinner.Stop()
-					view.spinner.SetVisible(false)
-					view.rooms.SetVisible(true)
-					if customService != "" {
-						view.notifyOnError(i18n.Local("That service doesn't seem to exist"))
-					} else {
-						view.notifyOnError(i18n.Local("Your XMPP server doesn't seem to have any chat room services"))
-					}
-				})
-			}
-
-			view.updateLock.Unlock()
-		}()
-		for {
-			select {
-			case sl, ok := <-resServices:
-				if !ok {
-					return
-				}
-				if !hasSomething {
-					hasSomething = true
-					doInUIThread(func() {
-						view.spinner.Stop()
-						view.spinner.SetVisible(false)
-						view.rooms.SetVisible(true)
-					})
-				}
-
-				serv, ok := view.serviceGroups[sl.Jid.String()]
-				if !ok {
-					doInUIThread(func() {
-						serv = view.roomsModel.Append(nil)
-						view.serviceGroups[sl.Jid.String()] = serv
-						_ = view.roomsModel.SetValue(serv, mucListRoomsIndexJid, sl.Jid.String())
-						_ = view.roomsModel.SetValue(serv, mucListRoomsIndexName, sl.Name)
-					})
-				}
-			case rl, ok := <-res:
-				if !ok || rl == nil {
-					return
-				}
-
-				if !hasSomething {
-					hasSomething = true
-					doInUIThread(func() {
-						view.spinner.Stop()
-						view.spinner.SetVisible(false)
-						view.rooms.SetVisible(true)
-					})
-				}
-
-				serv, ok := view.serviceGroups[rl.Service.String()]
-				doInUIThread(func() {
-					if !ok {
-						serv = view.roomsModel.Append(nil)
-						view.serviceGroups[rl.Service.String()] = serv
-						_ = view.roomsModel.SetValue(serv, mucListRoomsIndexJid, rl.Service.String())
-						_ = view.roomsModel.SetValue(serv, mucListRoomsIndexName, rl.ServiceName)
-					}
-
-					iter := view.roomsModel.Append(serv)
-					_ = view.roomsModel.SetValue(iter, mucListRoomsIndexJid, rl.Jid.Local().String())
-					_ = view.roomsModel.SetValue(iter, mucListRoomsIndexName, rl.Name)
-					_ = view.roomsModel.SetValue(iter, mucListRoomsIndexService, rl.Service.String())
-					rl.OnUpdate(u.updatedRoomListing, &roomListingUpdateData{iter, view, gen})
-
-					view.roomsTree.ExpandAll()
-				})
-			case e, ok := <-ec:
-				if !ok {
-					return
-				}
-				if e != nil {
-					doInUIThread(func() {
-						view.notifyOnError(i18n.Local("Something went wrong when trying to get chat rooms"))
-					})
-					u.log.WithError(e).Debug("something went wrong trying to get chat rooms")
-				}
-				return
-			case _, _ = <-view.cancel:
-				return
-			}
-		}
-	}()
-}
-
-// joinRoom should not be called from the UI thread
-func (u *gtkUI) joinRoom(roomJid string, a *account) {
-	a.log.WithField("room", roomJid).Debug("joinRoom()")
-	u.mucShowRoom(a, jid.Parse(roomJid).(jid.Bare))
-}
-
 // mucShowPublicRooms should be called from the UI thread
 func (u *gtkUI) mucShowPublicRooms() {
-	view := &mucPublicRoomsView{}
-	view.init()
-
-	accountsInput := view.builder.get("accounts").(gtki.ComboBox)
-	ac := u.createConnectedAccountsComponent(accountsInput, view,
-		func(acc *account) {
-			go u.mucUpdatePublicRoomsOn(view, acc)
-		},
-		func() {
-			view.rooms.SetVisible(false)
-			view.spinner.Stop()
-			view.spinner.SetVisible(false)
-			view.roomsModel.Clear()
-			view.refreshButton.SetSensitive(false)
-			view.customServiceButton.SetSensitive(false)
-		},
-	)
-
-	view.joinButton.SetSensitive(false)
-
-	view.builder.ConnectSignals(map[string]interface{}{
-		"on_cancel":       view.dialog.Destroy,
-		"on_close_window": ac.onDestroy,
-		"on_join": func() {
-			selection, err := view.roomsTree.GetSelection()
-			if err != nil {
-				u.log.WithError(err).Debug("couldn't join")
-				return
-			}
-			_, iter, ok := selection.GetSelected()
-			if !ok {
-				u.log.Debug("nothing is selected")
-				return
-			}
-
-			val, _ := view.roomsModel.GetValue(iter, mucListRoomsIndexJid)
-			v, _ := val.GetString()
-			_, ok = view.serviceGroups[v]
-
-			if ok {
-				u.log.Debug("a service is selected, not a room, so we can't activate it")
-				return
-			}
-			go u.joinRoom(v, ac.currentAccount())
-		},
-		"on_activate_room_row": func(_ gtki.TreeView, path gtki.TreePath) {
-			iter, err := view.roomsModel.GetIter(path)
-			if err != nil {
-				u.log.WithError(err).Debug("couldn't activate")
-				return
-			}
-			val, _ := view.roomsModel.GetValue(iter, mucListRoomsIndexJid)
-			v, _ := val.GetString()
-			_, ok := view.serviceGroups[v]
-			if ok {
-				u.log.Debug("a service is selected, not a room, so we can't activate it")
-				return
-			}
-			go u.joinRoom(v, ac.currentAccount())
-		},
-		"on_selection_changed": func() {
-			selection, err := view.roomsTree.GetSelection()
-			if err != nil {
-				u.log.WithError(err).Debug("problem getting selection")
-				return
-			}
-			_, iter, ok := selection.GetSelected()
-			if !ok {
-				view.joinButton.SetSensitive(false)
-				return
-			}
-
-			val, _ := view.roomsModel.GetValue(iter, mucListRoomsIndexJid)
-			v, _ := val.GetString()
-			_, ok = view.serviceGroups[v]
-			if ok {
-				view.joinButton.SetSensitive(false)
-			} else {
-				view.joinButton.SetSensitive(true)
-			}
-		},
-		"on_custom_service": func() {
-			go u.mucUpdatePublicRoomsOn(view, ac.currentAccount())
-		},
-		"on_refresh": func() {
-			go u.mucUpdatePublicRoomsOn(view, ac.currentAccount())
-		},
-	})
+	view := newMUCPublicRoomsView(u)
 
 	u.connectShortcutsChildWindow(view.dialog)
 
 	view.dialog.SetTransientFor(u.window)
 	view.dialog.Show()
+}
+
+// joinRoom should not be called from the UI thread
+func (prv *mucPublicRoomsView) joinRoom(roomJid string) {
+	a := prv.ac.currentAccount()
+	if a == nil {
+		// TODO: we should inform the user
+		prv.u.log.WithField("room", roomJid).Debug("joinRoom(): no account is selected")
+		return
+	}
+
+	a.log.WithField("room", roomJid).Debug("joinRoom()")
+	prv.u.mucShowRoom(a, jid.Parse(roomJid).(jid.Bare))
 }

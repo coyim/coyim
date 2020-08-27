90530797661b08394679f0059fb091707af10946
Pedro Palau <pepalauisaac@gmail.com>
2020-08-27 13:52:22 -0500

    Initializes unicode symbols depending of the operating system

---
+++ gui/unicode_symbols.go
@@ -0,0 +1,29 @@
+package gui
+
+import "runtime"
+
+type supportedSymbols struct {
+	bullet string
+}
+
+var symbols supportedSymbols
+
+func initUnicodeSymbols() {
+	if runtime.GOOS == "windows" {
+		symbols = windowsSymbols()
+	} else {
+		symbols = defaultSymbols()
+	}
+}
+
+func windowsSymbols() supportedSymbols {
+	return supportedSymbols{
+		bullet: "•",
+	}
+}
+
+func defaultSymbols() supportedSymbols {
+	return supportedSymbols{
+		bullet: "*",
+	}
+}

0b5f451bde5c1df8a269f90ad646fb9a4ba41c56
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-09-03 09:40:19 -0500

    Implements the MUC-roster view functionality in a proper way by creating it's own structure and doing all it's logic in it's own file for better organization of the code and to make easier to maintain it

---
+++ gui/muc_room_roster.go
@@ -0,0 +1,90 @@
+package gui
+
+import (
+	"runtime"
+
+	"github.com/coyim/coyim/i18n"
+	"github.com/coyim/coyim/session/muc"
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+const (
+	roomViewRosterStatusIconIndex  = 0
+	roomViewRosterNickNameIndex    = 1
+	roomViewRosterAffiliationIndex = 2
+	roomViewRosterRoleIndex        = 3
+)
+
+type roomViewRoster struct {
+	view  gtki.Box       `gtk-widget:"roomRosterBox"`
+	model gtki.ListStore `gtk-widget:"room-members-model"`
+	tree  gtki.TreeView  `gtk-widget:"room-members-tree"`
+}
+
+func newRoomViewRoster() *roomViewRoster {
+	r := &roomViewRoster{}
+
+	builder := newBuilder("MUCRoomRoster")
+	panicOnDevError(builder.bindObjects(r))
+
+	// r.model needs to be kept beyond the lifespan of the builder.
+	r.model.Ref()
+	runtime.SetFinalizer(r, func(ros interface{}) {
+		ros.(*roster).model.Unref()
+		ros.(*roster).model = nil
+	})
+
+	return r
+}
+
+func (v *roomViewRoster) updateRoomRoster(occupants []*muc.Occupant) {
+	v.model.Clear()
+
+	for _, o := range occupants {
+		v.addOccupantToRoster(o)
+	}
+
+	v.tree.ExpandAll()
+}
+
+func (v *roomViewRoster) addOccupantToRoster(o *muc.Occupant) {
+	iter := v.model.Append()
+
+	var icon Icon
+	if o.Role.HasVoice() {
+		icon = statusIcons["available"]
+	} else {
+		icon = statusIcons["offline"]
+	}
+
+	_ = v.model.SetValue(iter, roomViewRosterStatusIconIndex, icon.GetPixbuf())
+	_ = v.model.SetValue(iter, roomViewRosterNickNameIndex, o.Nick)
+	_ = v.model.SetValue(iter, roomViewRosterAffiliationIndex, v.affiliationDisplayName(o.Affiliation))
+	_ = v.model.SetValue(iter, roomViewRosterRoleIndex, v.roleDisplayName(o.Role))
+}
+
+func (v *roomViewRoster) affiliationDisplayName(a muc.Affiliation) string {
+	switch a.Name() {
+	case muc.AffiliationAdmin:
+		return i18n.Local("Admin")
+	case muc.AffiliationOwner:
+		return i18n.Local("Owner")
+	default:
+		return ""
+	}
+}
+
+func (v *roomViewRoster) roleDisplayName(r muc.Role) string {
+	switch r.Name() {
+	case muc.RoleNone:
+		return i18n.Local("None")
+	case muc.RoleParticipant:
+		return i18n.Local("Participant")
+	case muc.RoleVisitor:
+		return i18n.Local("Visitor")
+	case muc.RoleModerator:
+		return i18n.Local("Moderator")
+	default:
+		return ""
+	}
+}

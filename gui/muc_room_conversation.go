b6247d242ea3ce1158abfe1ddf416b7af45c7bd5
Reinaldo Mendoza <rmendoza83@gmail.com>
2020-09-03 14:38:05 -0500

    Adding text tags to conversation view

--- gui/muc_room_conversation.go
+++ gui/muc_room_conversation.go
@@ -6,11 +6,46 @@ import (
 	"github.com/coyim/coyim/i18n"
 	"github.com/coyim/coyim/xmpp/jid"
 	"github.com/coyim/gotk3adapter/gtki"
+	"github.com/coyim/gotk3adapter/pangoi"
 )
 
 type roomViewConversation struct {
-	view     gtki.Box        `gtk-widget:"roomConversation"`
-	messages gtki.TextBuffer `gtk-widget:"messages"`
+	view             gtki.Box      `gtk-widget:"roomConversation"`
+	roomChatTextView gtki.TextView `gtk-widget:"roomChatTextView"`
+
+	tags *mucStyleTags
+}
+
+type mucStyleTags struct {
+	table gtki.TextTagTable
+	buf   gtki.TextBuffer
+}
+
+func (v *roomViewConversation) getStyleTags() *mucStyleTags {
+	if v.tags == nil {
+		v.tags = v.newStyleTags()
+	}
+	return v.tags
+}
+
+func (v *roomViewConversation) newStyleTags() *mucStyleTags {
+	// TODO: for now we are using a default styles, but we can improve it
+	// if we define a structure with a predefined colors pallete based on kind
+	// of messages to show like entering a room, leaving the room, incoming
+	// message, etc
+	t := new(mucStyleTags)
+
+	t.table, _ = g.gtk.TextTagTableNew()
+
+	leftRoomTag, _ := g.gtk.TextTagNew("leftRoomText")
+	_ = leftRoomTag.SetProperty("foreground", "#EE0000")
+	_ = leftRoomTag.SetProperty("style", pangoi.STYLE_ITALIC)
+
+	t.table.Add(leftRoomTag)
+
+	t.buf, _ = g.gtk.TextBufferNew(t.table)
+
+	return t
 }
 
 func newRoomViewConversation() *roomViewConversation {
@@ -19,14 +54,33 @@ func newRoomViewConversation() *roomViewConversation {
 	builder := newBuilder("MUCRoomConversation")
 	panicOnDevError(builder.bindObjects(c))
 
+	t := c.getStyleTags()
+	c.roomChatTextView.SetBuffer(t.buf)
+
 	return c
 }
 
 func (v *roomViewConversation) showOccupantLeftRoom(nickname jid.Resource) {
-	v.addNewMessage(i18n.Localf("%s left the room", nickname))
+	doInUIThread(func() {
+		msg := i18n.Localf("%s left the room", nickname)
+		v.addLineToChatTextUsingTagID(msg, "leftRoomText")
+	})
 }
 
-func (v *roomViewConversation) addNewMessage(text string) {
-	i := v.messages.GetEndIter()
-	v.messages.Insert(i, fmt.Sprintf("%s\n", text))
+func (v *roomViewConversation) addLineToChatText(text string) {
+	buf, _ := v.roomChatTextView.GetBuffer()
+	i := buf.GetEndIter()
+
+	buf.Insert(i, fmt.Sprintf("%s\n", text))
+}
+
+func (v *roomViewConversation) addLineToChatTextUsingTagID(text string, tag string) {
+	buf, _ := v.roomChatTextView.GetBuffer()
+
+	charCount := buf.GetCharCount()
+	v.addLineToChatText(text)
+	oldIterEnd := buf.GetIterAtOffset(charCount)
+	newIterEnd := buf.GetEndIter()
+
+	buf.ApplyTagByName(tag, oldIterEnd, newIterEnd)
 }

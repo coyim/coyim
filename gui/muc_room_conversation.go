e9e64280a77f650b5db20a4c9f82a7407cedbc42
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-09-03 09:42:41 -0500

    This is a very basic implementation of a MUC conversation but it was made to improve the organization of all MUC gui-related functionalities

---
+++ gui/muc_room_conversation.go
@@ -0,0 +1,34 @@
+package gui
+
+import (
+	"fmt"
+
+	"github.com/coyim/coyim/i18n"
+	"github.com/coyim/coyim/xmpp/jid"
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+type roomViewConversation struct {
+	view     gtki.Box        `gtk-widget:"roomConversation"`
+	messages gtki.TextBuffer `gtk-widget:"messages"`
+}
+
+func newRoomViewConversation() *roomViewConversation {
+	c := &roomViewConversation{}
+
+	builder := newBuilder("MUCRoomConversation")
+	panicOnDevError(builder.bindObjects(c))
+
+	builder.ConnectSignals(map[string]interface{}{})
+
+	return c
+}
+
+func (v *roomViewConversation) showOccupantLeftRoom(nickname jid.Resource) {
+	v.addNewMessage(i18n.Localf("%s left the room", nickname))
+}
+
+func (v *roomViewConversation) addNewMessage(text string) {
+	i := v.messages.GetEndIter()
+	v.messages.Insert(i, fmt.Sprintf("%s\n", text))
+}

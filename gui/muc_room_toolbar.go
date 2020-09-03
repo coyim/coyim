0424ea89e00d677a677c6aba01fb69c0ff4f18df
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-09-03 09:40:56 -0500

    Moves the MUC-window toolbar to it's own structure to make easier to maintain it

---
+++ gui/muc_room_toolbar.go
@@ -0,0 +1,22 @@
+package gui
+
+import "github.com/coyim/gotk3adapter/gtki"
+
+type roomViewToolbar struct {
+	view                    gtki.Box    `gtk-widget:"roomToolbar"`
+	roomNameLabel           gtki.Label  `gtk-widget:"roomNameLabel"`
+	roomDescriptionLabel    gtki.Label  `gtk-widget:"roomDescriptionLabel"`
+	togglePanelButton       gtki.Button `gtk-widget:"togglePanelButton"`
+	closeConversationButton gtki.Button `gtk-widget:"closeConversationButton"`
+}
+
+func newRoomViewToolbar() *roomViewToolbar {
+	t := &roomViewToolbar{}
+
+	builder := newBuilder("MUCRoomToolbar")
+	panicOnDevError(builder.bindObjects(t))
+
+	builder.ConnectSignals(map[string]interface{}{})
+
+	return t
+}

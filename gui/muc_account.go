6f393ed33e52b0d1717d3fb531b421cabfacd4bb
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-09-04 12:42:13 -0500

    Since getting a room had lot of steps, this commit improves that by removing some steps and removes turning errors to bools and bool to errors

--- gui/muc_account.go
+++ gui/muc_account.go
@@ -6,53 +6,56 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func (a *account) getRoomByIdentity(ident jid.Bare) (*muc.Room, error) {
-	room, exists := a.roomManager.GetRoom(ident)
-	if !exists {
+func (a *account) getRoom(ident jid.Bare) (*muc.Room, error) {
+	room, ok := a.roomManager.GetRoom(ident)
+	if !ok {
+		a.log.WithField("room", ident).Error("Trying to get a room that is not in the manager")
 		return nil, newRoomNotExistsError(ident)
 	}
 	return room, nil
 }
 
-// getRoom will return a specific room based on it's bare.
-// If the room doesn't exists this method will return false
-func (a *account) getRoom(ident jid.Bare) (*muc.Room, bool) {
-	room, err := a.getRoomByIdentity(ident)
+func (a *account) getRoomView(ident jid.Bare) (*roomView, error) {
+	room, err := a.getRoom(ident)
 	if err != nil {
-		a.log.WithField("room", ident).WithError(err).Error("Trying to get a room that is not in the manager")
-		return nil, false
+		a.log.WithField("room", ident).WithError(err).Error("An error occurred while trying to get a room view")
+		return nil, err
 	}
-	return room, true
-}
-
-// getRoomView will return a specific room view based on it's bare.
-// If the view doesn't exists this method will return false
-func (a *account) getRoomView(ident jid.Bare) (*roomView, bool) {
-	room, ok := a.getRoom(ident)
-	if !ok {
-		a.log.WithField("room", ident).Error("Trying to get a room that is not in the manager")
-		return nil, false
-	}
-	return getViewFromRoom(room), true
+	return getViewFromRoom(room), nil
 }
 
 func (a *account) onRoomNicknameConflict(from jid.Full) {
-	view, ok := a.getRoomView(from.Bare())
-	if ok {
-		view.onNicknameConflictReceived(from)
+	view, err := a.getRoomView(from.Bare())
+	if err != nil {
+		a.log.WithField("from", from).WithError(err).Error("An error occurred when a room nickname conflict event was received")
+		return
 	}
+
+	view.onNicknameConflictReceived(from)
 }
 
 func (a *account) onRoomRegistrationRequired(from jid.Full) {
-	view, ok := a.getRoomView(from.Bare())
-	if ok {
-		view.onRegistrationRequiredReceived(from)
+	view, err := a.getRoomView(from.Bare())
+	if err != nil {
+		a.log.WithField("from", from).WithError(err).Error("An error occurred when a room registration required event was received")
+		return
 	}
+
+	view.onRegistrationRequiredReceived(from)
 }
 
 func (a *account) onRoomOccupantJoined(from jid.Full, ident jid.Full, affiliation, role, status string) {
-	room, ok := a.getRoom(from.Bare())
-	if !ok {
+	l := a.log.WithFields(log.Fields{
+		"from":        from,
+		"occupant":    ident,
+		"affiliation": affiliation,
+		"role":        role,
+		"status":      status,
+	})
+
+	room, err := a.getRoom(from.Bare())
+	if err != nil {
+		l.WithError(err).Error("An error occurred when a room occupant joined event was received")
 		return
 	}
 
@@ -61,39 +64,37 @@ func (a *account) onRoomOccupantJoined(from jid.Full, ident jid.Full, affiliatio
 	roster := room.Roster()
 	joined, _, err := roster.UpdatePresence(from, "", affiliation, role, "", status, "Occupant joined", ident)
 	if err != nil {
-		a.log.WithFields(log.Fields{
-			"from":        from,
-			"occupant":    ident,
-			"affiliation": affiliation,
-			"role":        role,
-			"status":      status,
-		}).WithError(err).Error("An error occurred trying to add the occupant to the roster")
-
+		l.WithError(err).Error("An error occurred trying to add the occupant to the roster")
 		view.onRoomOccupantErrorReceived(from)
 		return
 	}
 
-	// TODO: we are receiving a `join the room` event, so,
-	// if !joined we should log it
-	if joined {
-		view.onRoomOccupantJoinedReceived(ident.Resource(), roster.AllOccupants())
+	if !joined {
+		l.Error("The occupant can't join the room roster")
+		return
 	}
+
+	view.onRoomOccupantJoinedReceived(ident.Resource(), roster.AllOccupants())
 }
 
 func (a *account) onRoomOccupantUpdated(from jid.Full, occupant jid.Full, affiliation, role string) {
-	room, ok := a.getRoom(from.Bare())
-	if !ok {
+	l := a.log.WithFields(log.Fields{
+		"from":        from,
+		"occupant":    occupant,
+		"affiliation": affiliation,
+		"role":        role,
+	})
+
+	room, err := a.getRoom(from.Bare())
+	if err != nil {
+		l.WithError(err).Error("An error occurred when a room occupant updated event was received")
 		return
 	}
 
 	roster := room.Roster()
-	_, _, err := roster.UpdatePresence(from, "", affiliation, role, "", "", "Occupant updated", occupant)
+	_, _, err = roster.UpdatePresence(from, "", affiliation, role, "", "", "Occupant updated", occupant)
 	if err != nil {
-		a.log.WithFields(log.Fields{
-			"from":        from,
-			"occupant":    occupant,
-			"affiliation": affiliation,
-		}).WithError(err).Error("Error on trying to update the occupant status in the roster")
+		l.WithError(err).Error("Error on trying to update the occupant status in the roster")
 		return
 	}
 
@@ -102,27 +103,31 @@ func (a *account) onRoomOccupantUpdated(from jid.Full, occupant jid.Full, affili
 }
 
 func (a *account) onRoomOccupantLeftTheRoom(from jid.Full, ident jid.Full, affiliation, role string) {
-	room, ok := a.getRoom(from.Bare())
-	if !ok {
+	l := a.log.WithFields(log.Fields{
+		"from":        from,
+		"occupant":    ident,
+		"affiliation": affiliation,
+		"role":        role,
+	})
+
+	room, err := a.getRoom(from.Bare())
+	if err != nil {
+		l.WithError(err).Error("An error occurred when an occupant left the room event was received")
 		return
 	}
 
 	roster := room.Roster()
 	_, left, err := roster.UpdatePresence(from, "unavailable", affiliation, role, "", "unavailable", "Occupant left the room", ident)
 	if err != nil {
-		a.log.WithFields(log.Fields{
-			"from":        from,
-			"occupant":    ident,
-			"affiliation": affiliation,
-			"role":        role,
-		}).WithError(err).Error("An error occurred trying to remove the occupant from the roster")
+		l.WithError(err).Error("An error occurred trying to remove the occupant from the roster")
 		return
 	}
 
-	// TODO: we are receiving a `left the room` event, so,
-	// if !left we should log it
-	if left {
-		view := getViewFromRoom(room)
-		view.onRoomOccupantLeftTheRoomReceived(ident.Resource(), roster.AllOccupants())
+	if !left {
+		l.Error("The occupant can't left the room roster")
+		return
 	}
+
+	view := getViewFromRoom(room)
+	view.onRoomOccupantLeftTheRoomReceived(ident.Resource(), roster.AllOccupants())
 }

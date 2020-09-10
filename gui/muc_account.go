42086f5864da38bba432a65afb176d281e0c54f5
Mauro Velasco <mauro.velasco@gmail.com>
2020-09-10 11:06:52 -0500

    Fix, change roomName by room in field to log

--- gui/muc_account.go
+++ gui/muc_account.go
@@ -148,7 +148,7 @@ func (a *account) onRoomOccupantLeftTheRoom(roomName jid.Bare, nickname string, 
 
 func (a *account) onRoomMessageReceived(roomName jid.Bare, nickname jid.Resource, message string) {
 	l := a.log.WithFields(log.Fields{
-		"roomName": roomName,
+		"room":     roomName,
 		"nickname": nickname,
 		"message":  message,
 	})

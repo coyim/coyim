981365921a36ae41b3f1e7ae32f13ed302ed3c97
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-10-23 09:20:45 -0500

    Adds some room's config constant "keys" that represents configuration values

--- session/muc/data/room_config.go
+++ session/muc/data/room_config.go
@@ -1,5 +1,40 @@
 package data
 
+// RoomConfigType is used to represent a room configuration field
+type RoomConfigType int
+
+const (
+	// RoomConfigSupportsVoiceRequests represents room's "SupportsVoiceRequests" config
+	RoomConfigSupportsVoiceRequests RoomConfigType = iota
+	// RoomConfigAllowsRegistration represents room's "AllowsRegistration" config
+	RoomConfigAllowsRegistration
+	// RoomConfigPersistent represents room's "Persistent" config
+	RoomConfigPersistent
+	// RoomConfigModerated represents room's "Moderated" config
+	RoomConfigModerated
+	// RoomConfigOpen represents room's "Open" config
+	RoomConfigOpen
+	// RoomConfigPasswordProtected represents room's "PasswordProtected" config
+	RoomConfigPasswordProtected
+	// RoomConfigPublic represents room's "Public" config
+	RoomConfigPublic
+	// RoomConfigLanguage represents room's "Language" config
+	RoomConfigLanguage
+	// RoomConfigOccupantsCanChangeSubject represents room's "OccupantsCanChangeSubject" config
+	RoomConfigOccupantsCanChangeSubject
+	// RoomConfigTitle represents room's "Title" config
+	RoomConfigTitle
+	// RoomConfigDescription represents room's "Description" config
+	RoomConfigDescription
+	// RoomConfigOccupants represents room's "Occupants" config
+	RoomConfigOccupants
+	// RoomConfigOccupantsCanInvite represents room's "OccupantsCanInvite" config
+	RoomConfigOccupantsCanInvite
+	// RoomConfigAllowPrivateMessages represents room's "AllowPrivateMessages" config
+	RoomConfigAllowPrivateMessages
+	// RoomConfigLogged represents room's "Logged" config
+	RoomConfigLogged
+)
 
 // RoomConfig represents the room's configuration values
 type RoomConfig struct {

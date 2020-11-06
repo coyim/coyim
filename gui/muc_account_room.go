94790dc3816879605a497df2d286a77173104f52
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-11-06 15:33:59 -0500

    [muc-account] Uses the new room-operations functionality, in this case for leaving a room This code was added to this file to have a more clear vision of what the code is doing instead of mixing everything with existing code

---
+++ gui/muc_account_room.go
@@ -0,0 +1,23 @@
+package gui
+
+import "github.com/coyim/coyim/xmpp/jid"
+
+// leaveRoom should return the context so the caller can cancel the context early if required
+func (a *account) leaveRoom(roomID jid.Bare, nickname string, onSuccess func(), onError func(error)) {
+	leaveRoomCb := func() (<-chan bool, <-chan error, func()) {
+		ok, err := a.session.LeaveRoom(roomID, nickname)
+		return ok, err, nil
+	}
+
+	leaveRoomSuccess := func() {
+		a.removeRoomView(roomID)
+		if onSuccess != nil {
+			onSuccess()
+		}
+	}
+
+	controller := a.newRoomOpController("leave-room", leaveRoomCb, leaveRoomSuccess, onError)
+	ctx := a.newAccountRoomOpContext("leave-room", roomID, controller)
+
+	go ctx.doOperation()
+}

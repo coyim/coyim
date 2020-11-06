ea0da932da500282432ce19ae623341513fb486d
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-11-06 15:31:47 -0500

    [muc-account] Implements a proper way do operations over the room, like leave and destroy the room These two functionalities for example, are very similar in how they were implemented, so this commit adds some special structures and methods to make easier the call of those functionalities

--- gui/muc_account.go
+++ gui/muc_account.go
@@ -3,6 +3,9 @@ package gui
 import (
 	"fmt"
 
+	"github.com/coyim/coyim/coylog"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/coyim/coyim/session/muc"
 	"github.com/coyim/coyim/xmpp/jid"
 )
@@ -42,33 +45,136 @@ func (a *account) newRoomModel(roomID jid.Bare) *muc.Room {
 	return a.session.NewRoom(roomID)
 }
 
-func (a *account) newErrAccountInvalidRoom(roomID jid.Bare) error {
-	return fmt.Errorf("trying to leave a not available room \"%s\" for the account \"%s\"", roomID.String(), a.Account())
+type roomOpCallbackType func() (<-chan bool, <-chan error, func())
+
+type roomOpController struct {
+	callback  roomOpCallbackType
+	onSuccess func()
+	onError   func(error)
+	log       coylog.Logger
 }
 
-func (a *account) leaveRoom(roomID jid.Bare, nickname string, onSuccess func(), onError func(error)) {
-	// We need the room view because if something bad occurr while
-	// leaving the room, we might want to log the error using the room's logger
-	room, exists := a.getRoomView(roomID)
-	if !exists {
-		onError(a.newErrAccountInvalidRoom(roomID))
+func (a *account) newRoomOpController(op string, cb roomOpCallbackType, onSuccess func(), onError func(error)) *roomOpController {
+	return &roomOpController{
+		callback:  cb,
+		onSuccess: onSuccess,
+		onError:   onError,
+		log:       a.log.WithField("operation", op),
+	}
+}
+
+func (c *roomOpController) request(sch chan bool, ech chan error) {
+	ok, anyError, _ := c.callback()
+	select {
+	case <-ok:
+		sch <- true
+	case err := <-anyError:
+		ech <- err
+	}
+}
+
+func (c *roomOpController) success() {
+	if c.onSuccess != nil {
+		c.onSuccess()
+	}
+	c.log.Warn("Room operation executed but no success callback was given")
+}
+
+func (c *roomOpController) error(err error) {
+	log := c.log.WithError(err)
+	if c.onError == nil {
+		log.Error("Room operation executed but no error callback was given")
 		return
 	}
 
-	ok, anyError := a.session.LeaveRoom(roomID, nickname)
+	log.Error("A error occurred while trying to do a room's operation")
+	c.onError(err)
+}
 
-	go func() {
-		select {
-		case <-ok:
-			a.removeRoomView(roomID)
-			if onSuccess != nil {
-				onSuccess()
-			}
-		case err := <-anyError:
-			room.log.WithError(err).Error("An error occurred while trying to leave the room.")
-			if onError != nil {
-				onError(err)
-			}
-		}
-	}()
+type accountRoomOpContext struct {
+	op         string
+	roomID     jid.Bare
+	account    *account
+	controller *roomOpController
+
+	successChannel chan bool
+	errorChannel   chan error
+	cancelChannel  chan bool
+
+	log coylog.Logger
+}
+
+func (a *account) newAccountRoomOpContext(op string, roomID jid.Bare, c *roomOpController) *accountRoomOpContext {
+	ctx := &accountRoomOpContext{
+		op:         op,
+		roomID:     roomID,
+		account:    a,
+		controller: c,
+	}
+
+	ctx.initLog()
+
+	return ctx
+}
+
+func (ctx *accountRoomOpContext) initLog() {
+	fields := ctx.getLogCommonFields()
+	ctx.log = ctx.account.log.WithFields(fields)
+
+	// We need the room view because if some error happens
+	// during this operation we might want to log it using the room's logger
+	room, exists := ctx.account.getRoomView(ctx.roomID)
+	if !exists {
+		// We don't call "stopWithError" here because we haven't even
+		// started this context's operation
+		ctx.controller.error(ctx.newInvalidRoomError())
+		return
+	}
+
+	ctx.log = room.log.WithFields(fields)
+}
+
+func (ctx *accountRoomOpContext) getLogCommonFields() log.Fields {
+	return log.Fields{
+		"room":      ctx.roomID,
+		"operation": ctx.op,
+		"who":       "accountRoomOpContext",
+	}
+}
+
+// doOperation will block until the controller finishes
+func (ctx *accountRoomOpContext) doOperation() {
+	ctx.successChannel = make(chan bool, 1)
+	ctx.errorChannel = make(chan error, 1)
+	ctx.cancelChannel = make(chan bool, 1)
+
+	go ctx.waitUntilItFinish()
+	ctx.controller.request(ctx.successChannel, ctx.errorChannel)
+}
+
+func (ctx *accountRoomOpContext) waitUntilItFinish() {
+	select {
+	case <-ctx.successChannel:
+		ctx.controller.success()
+	case err := <-ctx.errorChannel:
+		ctx.stopWithError(err)
+	case <-ctx.cancelChannel:
+	}
+}
+
+func (ctx *accountRoomOpContext) stopWithError(err error) {
+	ctx.controller.error(err)
+}
+
+func (ctx *accountRoomOpContext) cancelOperation() {
+	if ctx.cancelChannel == nil {
+		return
+	}
+
+	ctx.log.Warn("A room operation was canceled, but can still occur")
+	ctx.cancelChannel <- true
+}
+
+func (ctx *accountRoomOpContext) newInvalidRoomError() error {
+	return fmt.Errorf("trying to %s a not available room \"%s\" for the account \"%s\"", ctx.op, ctx.roomID.String(), ctx.account.Account())
 }

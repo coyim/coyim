11155896600433cc55b8cc6921c9800dbc4fab7c
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-11-25 08:18:32 -0500

    [ps] #639 muc-room - Updates config changes callbacks to use the room disco info

--- gui/muc_room_config.go
+++ gui/muc_room_config.go
@@ -18,10 +18,10 @@ func (c roomConfigChangedTypes) contains(k data.RoomConfigType) bool {
 	return false
 }
 
-var roomConfigFriendlyMessages map[data.RoomConfigType]func(data.RoomConfig) string
+var roomConfigFriendlyMessages map[data.RoomConfigType]func(data.RoomDiscoInfo) string
 
 func initMUCConfigUpdateMessages() {
-	roomConfigFriendlyMessages = map[data.RoomConfigType]func(data.RoomConfig) string{
+	roomConfigFriendlyMessages = map[data.RoomConfigType]func(data.RoomDiscoInfo) string{
 		data.RoomConfigSupportsVoiceRequests:     roomConfigSupportsVoiceRequests,
 		data.RoomConfigAllowsRegistration:        roomConfigAllowsRegistration,
 		data.RoomConfigPersistent:                roomConfigPersistent,
@@ -38,20 +38,20 @@ func initMUCConfigUpdateMessages() {
 	}
 }
 
-func getRoomConfigUpdatedFriendlyMessages(changes roomConfigChangedTypes, config data.RoomConfig) []string {
+func getRoomConfigUpdatedFriendlyMessages(changes roomConfigChangedTypes, discoInfo data.RoomDiscoInfo) []string {
 	messages := []string{}
 
 	for k, f := range roomConfigFriendlyMessages {
 		if changes.contains(k) {
-			messages = append(messages, f(config))
+			messages = append(messages, f(discoInfo))
 		}
 	}
 
 	return messages
 }
 
-func roomConfigSupportsVoiceRequests(config data.RoomConfig) string {
-	if config.SupportsVoiceRequests {
+func roomConfigSupportsVoiceRequests(di data.RoomDiscoInfo) string {
+	if di.SupportsVoiceRequests {
 		return i18n.Local("The room's occupants with no \"voice\" can now " +
 			"request permission to speak.")
 	}
@@ -59,43 +59,43 @@ func roomConfigSupportsVoiceRequests(config data.RoomConfig) string {
 		"now, which means that visitors can't ask for permission to speak.")
 }
 
-func roomConfigAllowsRegistration(config data.RoomConfig) string {
-	if config.AllowsRegistration {
+func roomConfigAllowsRegistration(di data.RoomDiscoInfo) string {
+	if di.AllowsRegistration {
 		return i18n.Local("This room supports user registration.")
 	}
 	return i18n.Local("This room doesn't support user registration.")
 }
 
-func roomConfigPersistent(config data.RoomConfig) string {
-	if config.Persistent {
+func roomConfigPersistent(di data.RoomDiscoInfo) string {
+	if di.Persistent {
 		return i18n.Local("This room is now persistent.")
 	}
 	return i18n.Local("This room is not persistent anymore.")
 }
 
-func roomConfigModerated(config data.RoomConfig) string {
-	if config.Moderated {
+func roomConfigModerated(di data.RoomDiscoInfo) string {
+	if di.Moderated {
 		return i18n.Local("Only participants and moderators can now send messages in this room.")
 	}
 	return i18n.Local("Everyone can now send messages in this room.")
 }
 
-func roomConfigPasswordProtected(config data.RoomConfig) string {
-	if config.PasswordProtected {
+func roomConfigPasswordProtected(di data.RoomDiscoInfo) string {
+	if di.PasswordProtected {
 		return i18n.Local("This room is now protected by a password.")
 	}
 	return i18n.Local("This room is not protected by a password.")
 }
 
-func roomConfigPublic(config data.RoomConfig) string {
-	if config.Public {
+func roomConfigPublic(di data.RoomDiscoInfo) string {
+	if di.Public {
 		return i18n.Local("This room is publicly listed.")
 	}
 	return i18n.Local("This room is not publicly listed anymore.")
 }
 
-func roomConfigLanguage(config data.RoomConfig) string {
-	return i18n.Localf("The language of this room was changed to %s", getLanguage(config.Language))
+func roomConfigLanguage(di data.RoomDiscoInfo) string {
+	return i18n.Localf("The language of this room was changed to %s", getLanguage(di.Language))
 }
 
 func getLanguage(languageCode string) string {
@@ -107,22 +107,22 @@ func getLanguage(languageCode string) string {
 	return l
 }
 
-func roomConfigOccupantsCanChangeSubject(config data.RoomConfig) string {
+func roomConfigOccupantsCanChangeSubject(config data.RoomDiscoInfo) string {
 	if config.OccupantsCanChangeSubject {
 		return i18n.Local("Occupants can now change the subject of this room.")
 	}
 	return i18n.Local("Occupants cannot change the subject of this room.")
 }
 
-func roomConfigTitle(config data.RoomConfig) string {
+func roomConfigTitle(config data.RoomDiscoInfo) string {
 	return i18n.Localf("Title was changed to \"%s\".", config.Title)
 }
 
-func roomConfigDescription(config data.RoomConfig) string {
+func roomConfigDescription(config data.RoomDiscoInfo) string {
 	return i18n.Localf("Description was changed to \"%s\".", config.Description)
 }
 
-func roomConfigAllowPrivateMessages(config data.RoomConfig) string {
+func roomConfigAllowPrivateMessages(config data.RoomDiscoInfo) string {
 	if config.AllowPrivateMessages == data.RoleNone {
 		return i18n.Local("No one in this room can send private messages now.")
 	}
@@ -130,13 +130,13 @@ func roomConfigAllowPrivateMessages(config data.RoomConfig) string {
 		"to the room's occupants.", rolePluralName(config.AllowPrivateMessages))
 }
 
-func roomConfigMembersCanInvite(config data.RoomConfig) string {
+func roomConfigMembersCanInvite(config data.RoomDiscoInfo) string {
 	if config.MembersCanInvite {
 		return i18n.Local("Members can now invite other users to join.")
 	}
 	return i18n.Local("Members cannot invite other users to join anymore.")
 }
 
-func roomConfigMaxHistoryFetch(config data.RoomConfig) string {
+func roomConfigMaxHistoryFetch(config data.RoomDiscoInfo) string {
 	return i18n.Localf("The room's max history length was changed to %d", config.MaxHistoryFetch)
 }

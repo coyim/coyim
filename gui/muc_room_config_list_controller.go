f758175db8760197a6c3b1b51673f0bbce767738
Sandy <sandy.acurio@gmail.com>
2020-11-30 08:44:59 -0500

    [ps] #639 - Creates a "controller" to handle the relation between the configuration list and the form for adding occupants to each list

---
+++ gui/muc_room_config_list_controller.go
@@ -0,0 +1,73 @@
+package gui
+
+import (
+	"github.com/coyim/gotk3adapter/glibi"
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+type mucRoomConfigListControllerData struct {
+	addOccupantButton        gtki.Button
+	removeOccupantButton     gtki.Button
+	occupantsTreeView        gtki.TreeView
+	occupantsTreeViewColumns []glibi.Type
+
+	addOccupantDialogTitle string
+	addOccupantDescription string
+	addOccupantForm        func(onAnyValueChanges func()) mucRoomConfigListForm
+}
+
+type mucRoomConfigListController struct {
+	u *gtkUI
+
+	addOccupantButton        gtki.Button
+	removeOccupantButton     gtki.Button
+	ocuppantsTreeView        gtki.TreeView
+	occupantsTreeViewColumns []glibi.Type
+
+	listComponent    *mucRoomConfigListComponent
+	addComponentForm mucRoomConfigListForm
+	addComponent     *mucRoomConfigListAddComponent
+}
+
+func (u *gtkUI) newMUCRoomConfigListController(d *mucRoomConfigListControllerData) *mucRoomConfigListController {
+	c := &mucRoomConfigListController{
+		u:                        u,
+		addOccupantButton:        d.addOccupantButton,
+		removeOccupantButton:     d.removeOccupantButton,
+		ocuppantsTreeView:        d.occupantsTreeView,
+		occupantsTreeViewColumns: d.occupantsTreeViewColumns,
+	}
+
+	c.initListComponent(d)
+	c.initListAddFormComponent(d)
+	c.initListAddComponent(d)
+
+	return c
+}
+
+func (c *mucRoomConfigListController) initListComponent(d *mucRoomConfigListControllerData) {
+	c.listComponent = c.u.newMUCRoomConfigListComponent(
+		c.ocuppantsTreeView,
+		c.occupantsTreeViewColumns,
+		c.addOccupantButton,
+		c.removeOccupantButton,
+		c.onAddOccupantsToList,
+	)
+}
+
+func (c *mucRoomConfigListController) initListAddFormComponent(d *mucRoomConfigListControllerData) {
+	c.addComponentForm = d.addOccupantForm(nil)
+}
+
+func (c *mucRoomConfigListController) initListAddComponent(d *mucRoomConfigListControllerData) {
+	c.addComponent = c.u.newMUCRoomConfigListAddComponent(
+		d.addOccupantDialogTitle,
+		d.addOccupantDescription,
+		c.addComponentForm,
+		c.listComponent.addListItem,
+	)
+}
+
+func (c *mucRoomConfigListController) onAddOccupantsToList() {
+	c.addComponent.show()
+}

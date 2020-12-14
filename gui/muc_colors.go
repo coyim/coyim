8827594531315d1d71adc1f60361a6cd181e1101
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-12-14 10:58:01 -0500

    [cp] #680 - Uses abstract names for color sets in order to be more flexible in case we want to update the colors

--- gui/muc_colors.go
+++ gui/muc_colors.go
@@ -6,26 +6,28 @@ import (
 )
 
 type mucColorSet struct {
-	warningForeground       string
-	someoneJoinedForeground string
-	someoneLeftForeground   string
-	timestampForeground     string
-	nicknameForeground      string
-	subjectForeground       string
-	infoMessageForeground   string
-	messageForeground       string
-	errorForeground         string
-	configurationForeground string
-	white                   string
-	black                   string
-	light                   string
-	dark                    string
-	gray300                 string
-	gray500                 string
-	brown500                string
-	yellow200               string
-	yellow600               string
-	red200                  string
+	warningForeground            string
+	warningBackground            string
+	someoneJoinedForeground      string
+	someoneLeftForeground        string
+	timestampForeground          string
+	nicknameForeground           string
+	subjectForeground            string
+	infoMessageForeground        string
+	messageForeground            string
+	errorForeground              string
+	configurationForeground      string
+	roomMessagesBackground       string
+	roomOverlaySolidBackground   string
+	roomOverlayContentBackground string
+	roomOverlayBackground        string
+	roomOverlayContentForeground string
+	roomNameDisabledForeground   string
+	roomRosterStatusForeground   string
+	roomSubjectForeground        string
+	roomWarningForeground        string
+	roomWarningBackground        string
+	roomWarningBorder            string
 }
 
 func (u *gtkUI) currentMUCColorSet() mucColorSet {
@@ -39,26 +41,28 @@ func (u *gtkUI) defaultMUCLightColorSet() mucColorSet {
 	cs := u.defaultLightColorSet()
 
 	return mucColorSet{
-		warningForeground:       cs.warningForeground,
-		errorForeground:         cs.errorForeground,
-		someoneJoinedForeground: "#297316",
-		someoneLeftForeground:   "#731629",
-		timestampForeground:     "#AAB7B8",
-		nicknameForeground:      "#395BA3",
-		subjectForeground:       "#000080",
-		infoMessageForeground:   "#395BA3",
-		messageForeground:       "#000000",
-		configurationForeground: "#9a04bf",
-		white:                   "#FFFFFF",
-		black:                   "#000000",
-		light:                   "#FFFFFF",
-		dark:                    "#333333",
-		gray300:                 "#A9A9A9",
-		gray500:                 "#666666",
-		brown500:                "#744210",
-		yellow200:               "#FEFCBF",
-		yellow600:               "#D69E2E",
-		red200:                  "#FEE2E2",
+		warningForeground:            cs.warningForeground,
+		warningBackground:            cs.warningBackground,
+		errorForeground:              cs.errorForeground,
+		someoneJoinedForeground:      "#297316",
+		someoneLeftForeground:        "#731629",
+		timestampForeground:          "#AAB7B8",
+		nicknameForeground:           "#395BA3",
+		subjectForeground:            "#000080",
+		infoMessageForeground:        "#395BA3",
+		messageForeground:            "#000000",
+		configurationForeground:      "#9a04bf",
+		roomMessagesBackground:       "#FFFFFF",
+		roomOverlaySolidBackground:   "#FFFFFF",
+		roomOverlayContentBackground: "#FFFFFF",
+		roomOverlayBackground:        "#000000",
+		roomOverlayContentForeground: "#333333",
+		roomNameDisabledForeground:   "#A9A9A9",
+		roomRosterStatusForeground:   "#666666",
+		roomSubjectForeground:        "#666666",
+		roomWarningForeground:        "#744210",
+		roomWarningBackground:        "#FEFCBF",
+		roomWarningBorder:            "#D69E2E",
 	}
 }
 
@@ -66,26 +70,28 @@ func (u *gtkUI) defaultMUCDarkColorSet() mucColorSet {
 	cs := u.defaultDarkColorSet()
 
 	return mucColorSet{
-		warningForeground:       cs.warningForeground,
-		errorForeground:         cs.errorForeground,
-		someoneJoinedForeground: "#297316",
-		someoneLeftForeground:   "#731629",
-		timestampForeground:     "#AAB7B8",
-		nicknameForeground:      "#395BA3",
-		subjectForeground:       "#000080",
-		infoMessageForeground:   "#E34267",
-		messageForeground:       "#000000",
-		configurationForeground: "#9a04bf",
-		white:                   "#FFFFFF",
-		black:                   "#000000",
-		light:                   "#111111",
-		dark:                    "#000000",
-		gray300:                 "#A9A9A9",
-		gray500:                 "#666666",
-		brown500:                "#744210",
-		yellow200:               "#FEFCBF",
-		yellow600:               "#D69E2E",
-		red200:                  "#FEE2E2",
+		warningForeground:            cs.warningForeground,
+		warningBackground:            cs.warningBackground,
+		errorForeground:              cs.errorForeground,
+		someoneJoinedForeground:      "#297316",
+		someoneLeftForeground:        "#731629",
+		timestampForeground:          "#AAB7B8",
+		nicknameForeground:           "#395BA3",
+		subjectForeground:            "#000080",
+		infoMessageForeground:        "#E34267",
+		messageForeground:            "#000000",
+		configurationForeground:      "#9a04bf",
+		roomMessagesBackground:       "#FFFFFF",
+		roomOverlaySolidBackground:   "#FFFFFF",
+		roomOverlayContentBackground: "#FFFFFF",
+		roomOverlayBackground:        "#000000",
+		roomOverlayContentForeground: "#333333",
+		roomNameDisabledForeground:   "#A9A9A9",
+		roomRosterStatusForeground:   "#666666",
+		roomSubjectForeground:        "#666666",
+		roomWarningForeground:        "#744210",
+		roomWarningBackground:        "#FEFCBF",
+		roomWarningBorder:            "#D69E2E",
 	}
 }
 

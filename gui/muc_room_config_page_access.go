3ea6907c7c67f5e3e246241ba1446732d406fcbb
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-12-17 11:42:36 -0500

    [ps] #639 - Creates the "Access" configuration page

---
+++ gui/muc_room_config_page_access.go
@@ -0,0 +1,37 @@
+package gui
+
+import "github.com/coyim/gotk3adapter/gtki"
+
+type roomConfigAccessPage struct {
+	*roomConfigPageBase
+
+	configAccessBox  gtki.Box    `gtk-widget:"room-config-access-page"`
+	roomPassword     gtki.Entry  `gtk-widget:"room-password"`
+	roomMembersOnly  gtki.Switch `gtk-widget:"room-membersonly"`
+	roomAllowInvites gtki.Switch `gtk-widget:"room-allowinvites"`
+}
+
+func (c *mucRoomConfigComponent) newRoomConfigAccessPage() mucRoomConfigPage {
+	p := &roomConfigAccessPage{}
+
+	builder := newBuilder("MUCRoomConfigPageAccess")
+	panicOnDevError(builder.bindObjects(p))
+
+	p.roomConfigPageBase = c.newConfigPage(p.configAccessBox)
+
+	p.initDefaultValues()
+
+	return p
+}
+
+func (p *roomConfigAccessPage) initDefaultValues() {
+	setEntryText(p.roomPassword, p.form.Password)
+	setSwitchActive(p.roomMembersOnly, p.form.MembersOnly)
+	setSwitchActive(p.roomAllowInvites, p.form.OccupantsCanInvite)
+}
+
+func (p *roomConfigAccessPage) collectData() {
+	p.form.Password = getEntryText(p.roomPassword)
+	p.form.MembersOnly = getSwitchActive(p.roomMembersOnly)
+	p.form.OccupantsCanInvite = getSwitchActive(p.roomAllowInvites)
+}

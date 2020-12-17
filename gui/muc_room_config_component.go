78b257973cb83ad9f35615d0d2cb3bf01d0f0697
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-12-17 11:44:34 -0500

    [ps] #639 - Updates the room configuration component in order to simplifying it by using only what it needs and now it uses custom configuration pages that have the logic for each one

--- gui/muc_room_config_component.go
+++ gui/muc_room_config_component.go
@@ -1,259 +1,113 @@
 package gui
 
 import (
+	"github.com/coyim/coyim/coylog"
 	"github.com/coyim/coyim/i18n"
 	"github.com/coyim/coyim/session/muc"
-	"github.com/coyim/gotk3adapter/glibi"
-	"github.com/coyim/gotk3adapter/gtki"
+	"github.com/coyim/coyim/xmpp/jid"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
-	roomConfigInformationPage = iota
-	roomConfigAccessPage
-	roomConfigPermissionsPage
-	roomConfigOccupantsPage
-	roomConfigOthersPage
-	roomConfigSummaryPage
+	roomConfigInformationPageIndex = iota
+	roomConfigAccessPageIndex
+	roomConfigPermissionsPageIndex
+	roomConfigOccupantsPageIndex
+	roomConfigOthersPageIndex
+	roomConfigSummaryPageIndex
 )
 
 type mucRoomConfigComponent struct {
-	u    *gtkUI
-	form *muc.RoomConfigForm
+	u      *gtkUI
+	form   *muc.RoomConfigForm
+	roomID jid.Bare
 
-	// Information cofiguration page
-	configInformationPage *mucRoomConfigPage
-	configInformationBox  gtki.Box      `gtk-widget:"room-config-info-page"`
-	roomTitle             gtki.Entry    `gtk-widget:"room-title"`
-	roomDescription       gtki.TextView `gtk-widget:"room-description"`
-	roomLanguage          gtki.Entry    `gtk-widget:"room-language"`
-	roomPersistent        gtki.Switch   `gtk-widget:"room-persistent"`
-	roomPublic            gtki.Switch   `gtk-widget:"room-public"`
+	infoPage        mucRoomConfigPage
+	accessPage      mucRoomConfigPage
+	permissionsPage mucRoomConfigPage
+	occupantsPage   mucRoomConfigPage
+	othersPage      mucRoomConfigPage
+	summaryPage     mucRoomConfigPage
 
-	// Access cofiguration page
-	configAccessPage *mucRoomConfigPage
-	configAccessBox  gtki.Box    `gtk-widget:"room-config-access-page"`
-	roomPassword     gtki.Entry  `gtk-widget:"room-password"`
-	roomMembersOnly  gtki.Switch `gtk-widget:"room-membersonly"`
-	roomAllowInvites gtki.Switch `gtk-widget:"room-allowinvites"`
-
-	// Permissions cofiguration page
-	configPermissionsPage *mucRoomConfigPage
-	configPermissionsBox  gtki.Box      `gtk-widget:"room-config-permissions-page"`
-	roomChangeSubject     gtki.Switch   `gtk-widget:"room-changesubject"`
-	roomModerated         gtki.Switch   `gtk-widget:"room-moderated"`
-	roomWhois             gtki.ComboBox `gtk-widget:"room-whois"`
-
-	// Members cofiguration page
-	configOccupantsPage *mucRoomConfigPage
-	configOccupantsBox  gtki.Box `gtk-widget:"room-config-occupants-page"`
-
-	// Ban list-related UI elements
-	banList           gtki.TreeView `gtk-widget:"room-config-ban-list"`
-	banAddButton      gtki.Button   `gtk-widget:"room-ban-add"`
-	banRemoveButton   gtki.Button   `gtk-widget:"room-ban-remove"`
-	banListController *mucRoomConfigListController
-
-	// Members list-related UI elements
-	membersList           gtki.TreeView `gtk-widget:"room-config-members-list"`
-	membersAddButton      gtki.Button   `gtk-widget:"room-member-add"`
-	membersRemoveButton   gtki.Button   `gtk-widget:"room-member-remove"`
-	membersListController *mucRoomConfigListController
-
-	// Owners list-related UI elements
-	ownersList           gtki.TreeView `gtk-widget:"room-config-owners-list"`
-	ownersAddButton      gtki.Button   `gtk-widget:"room-owner-add"`
-	ownersRemoveButton   gtki.Button   `gtk-widget:"room-owner-remove"`
-	ownersListController *mucRoomConfigListController
-
-	// Admin list-related UI elements
-	adminList            gtki.TreeView `gtk-widget:"room-config-admin-list"`
-	adminAddButton       gtki.Button   `gtk-widget:"room-admin-add"`
-	adminRemoveButton    gtki.Button   `gtk-widget:"room-admin-remove"`
-	adminsListController *mucRoomConfigListController
-
-	// Others cofiguration page
-	configOthersPage    *mucRoomConfigPage
-	configOthersBox     gtki.Box        `gtk-widget:"room-config-others-page"`
-	roomMaxHistoryFetch gtki.SpinButton `gtk-widget:"room-maxhistoryfetch"`
-	roomMaxOccupants    gtki.SpinButton `gtk-widget:"room-maxoccupants"`
-	roomEnableLoggin    gtki.Switch     `gtk-widget:"room-enablelogging"`
-
-	// Summary cofiguration page
-	configSummaryPage *mucRoomConfigPage
-	configSummaryBox  gtki.Box `gtk-widget:"room-config-summary-page"`
+	log coylog.Logger
 }
 
-func (u *gtkUI) newMUCRoomConfigComponent(f *muc.RoomConfigForm) *mucRoomConfigComponent {
-	c := &mucRoomConfigComponent{u: u, form: f}
+func (u *gtkUI) newMUCRoomConfigComponent(roomID jid.Bare, f *muc.RoomConfigForm) *mucRoomConfigComponent {
+	c := &mucRoomConfigComponent{
+		u:      u,
+		form:   f,
+		roomID: roomID,
+		log: u.log.WithFields(log.Fields{
+			"room":  roomID,
+			"where": "roomConfigComponent",
+		}),
+	}
 
-	c.initBuilder()
 	c.initConfigPages()
 
 	return c
 }
 
-func (c *mucRoomConfigComponent) initBuilder() {
-	b := newBuilder("MUCRoomConfig")
-	panicOnDevError(b.bindObjects(c))
-}
-
 func (c *mucRoomConfigComponent) initConfigPages() {
-	c.initConfigInformationPage()
-	c.initConfigAccessPage()
-	c.initConfigPermissionsPage()
-	c.initConfigOccupantsPage()
-	c.initConfigOthersPage()
-	c.initConfigSummaryPage()
+	c.infoPage = c.newRoomConfigInfoPage()
+	c.accessPage = c.newRoomConfigAccessPage()
+	c.permissionsPage = c.newRoomConfigPermissionsPage()
+	c.occupantsPage = c.newRoomConfigOccupantsPage()
+	c.othersPage = c.newRoomConfigOthersPage()
+	c.summaryPage = c.newRoomConfigSummaryPage()
 }
 
-func (c *mucRoomConfigComponent) initInformationPageDefaultValues() {
-	c.roomTitle.SetText(c.form.Title)
-	c.setDescriptionText()
-	c.roomLanguage.SetText(c.form.Language)
-	c.roomPersistent.SetActive(c.form.Persistent)
-	c.roomPublic.SetActive(c.form.Public)
+func (c *mucRoomConfigComponent) submitForm() error {
+	// TODO: implements the behavior of this method
+	return c.form.Submit()
 }
 
-func (c *mucRoomConfigComponent) initConfigInformationPage() {
-	c.configInformationPage = newMUCRoomConfigPage(c.configInformationBox)
-	c.initInformationPageDefaultValues()
-}
-
-func (c *mucRoomConfigComponent) initConfigAccessPage() {
-	c.configAccessPage = newMUCRoomConfigPage(c.configAccessBox)
-}
-
-func (c *mucRoomConfigComponent) initConfigPermissionsPage() {
-	c.configPermissionsPage = newMUCRoomConfigPage(c.configPermissionsBox)
-}
-
-func (c *mucRoomConfigComponent) initConfigOccupantsPage() {
-	c.configOccupantsPage = newMUCRoomConfigPage(c.configOccupantsBox)
-	c.initConfigOccupantsLists()
-}
-
-func (c *mucRoomConfigComponent) initConfigOthersPage() {
-	c.configOthersPage = newMUCRoomConfigPage(c.configOthersBox)
-}
-
-func (c *mucRoomConfigComponent) initConfigSummaryPage() {
-	c.configSummaryPage = newMUCRoomConfigPage(c.configSummaryBox)
-}
-
-func (c *mucRoomConfigComponent) initConfigOccupantsLists() {
-	c.initBanListController()
-	c.initMembersListController()
-	c.initOwnersListController()
-	c.initAdminsListController()
-}
-
-func (c *mucRoomConfigComponent) initBanListController() {
-	banListColumns := []glibi.Type{
-		// jid
-		glibi.TYPE_STRING,
-		// reason
-		glibi.TYPE_STRING,
-	}
-
-	c.banListController = c.u.newMUCRoomConfigListController(&mucRoomConfigListControllerData{
-		addOccupantButton:        c.banAddButton,
-		removeOccupantButton:     c.banRemoveButton,
-		occupantsTreeView:        c.banList,
-		occupantsTreeViewColumns: banListColumns,
-		addOccupantDialogTitle:   i18n.Local("Adding banned member..."),
-		addOccupantDescription:   i18n.Local("Whom do you want to ban a member?"),
-		addOccupantForm:          newMUCRoomConfigListKickedForm,
-	})
-}
-
-func (c *mucRoomConfigComponent) initMembersListController() {
-	membersListColumns := []glibi.Type{
-		// jid
-		glibi.TYPE_STRING,
-		// nickname
-		glibi.TYPE_STRING,
-		// role
-		glibi.TYPE_STRING,
-	}
-
-	c.membersListController = c.u.newMUCRoomConfigListController(&mucRoomConfigListControllerData{
-		addOccupantButton:        c.membersAddButton,
-		removeOccupantButton:     c.membersRemoveButton,
-		occupantsTreeView:        c.membersList,
-		occupantsTreeViewColumns: membersListColumns,
-		addOccupantDialogTitle:   i18n.Local("Adding member..."),
-		addOccupantDescription:   i18n.Local("Whom do you want to make a member?"),
-		addOccupantForm:          newMUCRoomConfigListMembersForm,
-	})
-}
-
-func (c *mucRoomConfigComponent) initOwnersListController() {
-	ownersListColumns := []glibi.Type{
-		// jid
-		glibi.TYPE_STRING,
-	}
-
-	c.ownersListController = c.u.newMUCRoomConfigListController(&mucRoomConfigListControllerData{
-		addOccupantButton:        c.ownersAddButton,
-		removeOccupantButton:     c.ownersRemoveButton,
-		occupantsTreeView:        c.ownersList,
-		occupantsTreeViewColumns: ownersListColumns,
-		addOccupantDialogTitle:   i18n.Local("Adding owner member..."),
-		addOccupantDescription:   i18n.Local("Whom do you want to make an owner member?"),
-		addOccupantForm:          newMUCRoomConfigListOwnersForm,
-	})
-}
-
-func (c *mucRoomConfigComponent) initAdminsListController() {
-	adminsListColumns := []glibi.Type{
-		// jid
-		glibi.TYPE_STRING,
-	}
-
-	c.adminsListController = c.u.newMUCRoomConfigListController(&mucRoomConfigListControllerData{
-		addOccupantButton:        c.adminAddButton,
-		removeOccupantButton:     c.adminRemoveButton,
-		occupantsTreeView:        c.adminList,
-		occupantsTreeViewColumns: adminsListColumns,
-		addOccupantDialogTitle:   i18n.Local("Adding admin member..."),
-		addOccupantDescription:   i18n.Local("Whom do you want to make an admin member?"),
-		addOccupantForm:          newMUCRoomConfigListAdminsForm,
-	})
-}
-
-func (c *mucRoomConfigComponent) getConfigPage(p int) *mucRoomConfigPage {
+func (c *mucRoomConfigComponent) getConfigPage(p int) mucRoomConfigPage {
 	switch p {
-	case roomConfigInformationPage:
-		return c.configInformationPage
-	case roomConfigAccessPage:
-		return c.configAccessPage
-	case roomConfigPermissionsPage:
-		return c.configPermissionsPage
-	case roomConfigOccupantsPage:
-		return c.configOccupantsPage
-	case roomConfigOthersPage:
-		return c.configOthersPage
-	case roomConfigSummaryPage:
-		return c.configSummaryPage
+	case roomConfigInformationPageIndex:
+		return c.infoPage
+	case roomConfigAccessPageIndex:
+		return c.accessPage
+	case roomConfigPermissionsPageIndex:
+		return c.permissionsPage
+	case roomConfigOccupantsPageIndex:
+		return c.occupantsPage
+	case roomConfigOthersPageIndex:
+		return c.othersPage
+	case roomConfigSummaryPageIndex:
+		return c.summaryPage
 	default:
 		return nil
 	}
 }
 
-type mucRoomConfigPage struct {
-	box gtki.Box
-}
-
-func newMUCRoomConfigPage(b gtki.Box) *mucRoomConfigPage {
-	return &mucRoomConfigPage{b}
-}
-
-func (p *mucRoomConfigPage) getPageView() gtki.Box {
-	return p.box
-}
-
-func (c *mucRoomConfigComponent) setDescriptionText() {
-	cb, _ := g.gtk.TextBufferNew(nil)
-	cb.SetText(c.form.Description)
-	c.roomDescription.SetBuffer(cb)
+func configOptionToFriendlyMessage(o string) string {
+	switch o {
+	case muc.RoomConfigOptionParticipants:
+		return i18n.Local("Participants")
+	case muc.RoomConfigOptionParticipant:
+		return i18n.Local("Participant")
+	case muc.RoomConfigOptionModerators:
+		return i18n.Local("Moderators")
+	case muc.RoomConfigOptionModerator:
+		return i18n.Local("Moderator")
+	case muc.RoomConfigOptionVisitor:
+		return i18n.Local("Visitor")
+	case muc.RoomConfigOptionAnyone:
+		return i18n.Local("Anyone")
+	case muc.RoomConfigOptionNone:
+		return i18n.Local("None")
+	case muc.RoomConfigOption10:
+		return i18n.Local("10")
+	case muc.RoomConfigOption20:
+		return i18n.Local("20")
+	case muc.RoomConfigOption30:
+		return i18n.Local("30")
+	case muc.RoomConfigOption50:
+		return i18n.Local("50")
+	case muc.RoomConfigOption100:
+		return i18n.Local("100")
+	default:
+		return i18n.Localf("Unsupported \"%s\" option", o)
+	}
 }

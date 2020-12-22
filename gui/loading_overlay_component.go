327bd2de3e7d22a650fc16efd1e16d874c607b97
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-12-22 14:25:22 -0500

    [cp] #639 - Creates a pretty "loading overlay" component that can be use in different UI to show some "block while loading" status to the user

---
+++ gui/loading_overlay_component.go
@@ -0,0 +1,65 @@
+package gui
+
+import "github.com/coyim/gotk3adapter/gtki"
+
+type loadingOverlayComponent struct {
+	overlay     gtki.Overlay `gtk-widget:"loading-overlay"`
+	title       gtki.Label   `gtk-widget:"loading-overlay-title"`
+	description gtki.Label   `gtk-widget:"loading-overlay-description"`
+	box         gtki.Box     `gtk-widget:"loading-overlay-box"`
+}
+
+func (u *gtkUI) newLoadingOverlayComponent() *loadingOverlayComponent {
+	lo := &loadingOverlayComponent{}
+
+	builder := newBuilder("LoadingOverlay")
+	panicOnDevError(builder.bindObjects(lo))
+
+	mucStyles.setLabelBoldStyle(lo.title)
+
+	return lo
+}
+
+// setTransparent MUST be called from the UI thread
+func (lo *loadingOverlayComponent) setTransparent() {
+	mucStyles.setRoomLoadingViewOverlayTransparentStyle(lo.box)
+}
+
+// setSolid MUST be called from the UI thread
+func (lo *loadingOverlayComponent) setSolid() {
+	mucStyles.setRoomLoadingViewOverlaySolidStyle(lo.box)
+}
+
+// show MUST be called from the UI thread
+func (lo *loadingOverlayComponent) show() {
+	lo.overlay.Show()
+}
+
+// showWithMessage MUST be called from the UI thread
+func (lo *loadingOverlayComponent) showWithMessage(m string) {
+	lo.setTitle(m)
+	lo.show()
+}
+
+// setTitle MUST be called from the UI thread
+func (lo *loadingOverlayComponent) setTitle(t string) {
+	lo.title.SetLabel(t)
+	lo.title.Show()
+}
+
+// setDescription MUST be called from the UI thread
+func (lo *loadingOverlayComponent) setDescription(d string) {
+	lo.description.SetLabel(d)
+	lo.description.Show()
+}
+
+// hide MUST be called from the UI thread
+func (lo *loadingOverlayComponent) hide() {
+	lo.setTitle("")
+	lo.setDescription("")
+
+	lo.title.Hide()
+	lo.description.Hide()
+
+	lo.overlay.Hide()
+}

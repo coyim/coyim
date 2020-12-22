63930cf75623ed38bf20be34b7fe8369051f1596
Sandy <sandy.acurio@gmail.com>
2020-12-22 11:47:15 -0500

    [ms] #656 - Updates an error message when a room doesn't exists

--- gui/muc_room_destroy_error.go
+++ gui/muc_room_destroy_error.go
@@ -76,7 +76,7 @@ func (rd *roomDestroyErrorView) friendlyMessageForDestroyError(err error) string
 		return i18n.Local("You don't have the permission to destroy this room. " +
 			"Please contact one of the room owners.")
 	case session.ErrDestroyRoomDoesntExist:
-		return i18n.Local("We couldn't find the room in the server.")
+		return i18n.Local("We couldn't find the room. Please try again in a moment.")
 	default:
 		return i18n.Local("An unknown error ocurred during the process. Please try again later.")
 	}

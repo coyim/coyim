67e97ae589c26872c1e0b6cd0b06fa0bce8d24c9
Pedro Enrique Palau <pepalauisaac@gmail.com>
2020-12-22 13:30:33 -0500

    [cp] #639 - Removes the "config list forms" file and simplifies the creationg of a list form by using a "base" structure from where other forms can "inherit"

---
+++ gui/muc_room_config_list_form.go
@@ -0,0 +1,65 @@
+package gui
+
+import (
+	"errors"
+
+	"github.com/coyim/coyim/i18n"
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+type mucRoomConfigListForm interface {
+	jid() string
+	isValid() (bool, error)
+	getValues() []string
+	friendlyErrorMessage(error) string
+	getFormView() gtki.Widget
+}
+
+var (
+	errRoomConfigListFormInvalidJid = errors.New("invalid jid")
+)
+
+type roomConfigListForm struct {
+	form     gtki.Box   `gtk-widget:"room-config-list-form"`
+	jidEntry gtki.Entry `gtk-widget:"room-config-list-jid"`
+}
+
+func newRoomConfigListForm(ui string, ef interface{}, onFieldChanged func()) *roomConfigListForm {
+	f := &roomConfigListForm{}
+
+	builder := newBuilder(ui)
+	panicOnDevError(builder.bindObjects(f))
+	if ef != nil {
+		panicOnDevError(builder.bindObjects(ef))
+	}
+
+	builder.ConnectSignals(map[string]interface{}{
+		"on_field_changed": func() {
+			if onFieldChanged != nil {
+				onFieldChanged()
+			}
+		},
+	})
+
+	return f
+}
+
+func (f *roomConfigListForm) jid() string {
+	return getEntryText(f.jidEntry)
+}
+
+func (*roomConfigListForm) isValid() (bool, error) {
+	return true, nil
+}
+
+func (f *roomConfigListForm) getValues() []string {
+	return []string{f.jid()}
+}
+
+func (f *roomConfigListForm) friendlyErrorMessage(err error) string {
+	return i18n.Local("Invalid form values")
+}
+
+func (f *roomConfigListForm) getFormView() gtki.Widget {
+	return f.form
+}

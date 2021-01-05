73eca7b385c9a6f99a71306fb579fb1e41d8dd73
Sandy <sandy.acurio@gmail.com>
2021-01-05 17:17:57 -0500

    [ps] muc-room-config-assistant - Set parent window for room configuration assistant window

--- gui/muc_room_config_assistant.go
+++ gui/muc_room_config_assistant.go
@@ -115,6 +115,7 @@ func (rc *roomConfigAssistant) initDefaults() {
 	rc.summaryPageBox.SetHExpand(true)
 
 	rc.assistant.SetTitle(i18n.Localf("Configuration for room [%s]", rc.roomID.String()))
+	rc.assistant.SetTransientFor(rc.u.window)
 }
 
 func (rc *roomConfigAssistant) onPageChanged(_ gtki.Assistant, p gtki.Widget) {

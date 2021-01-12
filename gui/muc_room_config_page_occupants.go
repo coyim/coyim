baf7eddb85921bd01c3fb294094454c87a764f42
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-01-12 10:28:59 -0500

    [mp] #639 - Allows the user to update in place the occupant list information

--- gui/muc_room_config_page_occupants.go
+++ gui/muc_room_config_page_occupants.go
@@ -5,17 +5,25 @@ import (
 	"github.com/coyim/coyim/xmpp/jid"
 	"github.com/coyim/gotk3adapter/glibi"
 	"github.com/coyim/gotk3adapter/gtki"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	ownersListColumnJidIndex = 0
+	adminsListColumnJidIndex = 0
 )
 
 type roomConfigOccupantsPage struct {
 	*roomConfigPageBase
 
-	ownersList         gtki.TreeView `gtk-widget:"room-config-owners-list"`
-	ownersAddButton    gtki.Button   `gtk-widget:"room-owner-add"`
-	ownersRemoveButton gtki.Button   `gtk-widget:"room-owner-remove"`
-	adminList          gtki.TreeView `gtk-widget:"room-config-admin-list"`
-	adminAddButton     gtki.Button   `gtk-widget:"room-admin-add"`
-	adminRemoveButton  gtki.Button   `gtk-widget:"room-admin-remove"`
+	ownersList            gtki.TreeView         `gtk-widget:"room-config-owners-list"`
+	ownersAddButton       gtki.Button           `gtk-widget:"room-owner-add"`
+	ownersRemoveButton    gtki.Button           `gtk-widget:"room-owner-remove"`
+	ownersListJidRenderer gtki.CellRendererText `gtk-widget:"owner-jid-text-renderer"`
+	adminList             gtki.TreeView         `gtk-widget:"room-config-admin-list"`
+	adminAddButton        gtki.Button           `gtk-widget:"room-admin-add"`
+	adminRemoveButton     gtki.Button           `gtk-widget:"room-admin-remove"`
+	adminListJidRenderer  gtki.CellRendererText `gtk-widget:"admin-jid-text-renderer"`
 
 	ownersListController *mucRoomConfigListController
 	adminsListController *mucRoomConfigListController
@@ -23,12 +31,40 @@ type roomConfigOccupantsPage struct {
 
 func (c *mucRoomConfigComponent) newRoomConfigOccupantsPage(parent gtki.Window) mucRoomConfigPage {
 	p := &roomConfigOccupantsPage{}
-	p.roomConfigPageBase = c.newConfigPage("occupants", "MUCRoomConfigPageOccupants", p, nil)
+	p.roomConfigPageBase = c.newConfigPage("occupants", "MUCRoomConfigPageOccupants", p, map[string]interface{}{
+		"on_owner_jid_edited": p.onOwnerJidEdited,
+		"on_admin_jid_edited": p.onAdminJidEdited,
+	})
 
+	p.initOccupantsEditableCells()
 	p.initOccupantsLists(parent)
 	p.initDefaultValues()
 
 	return p
+}
+
+func (p *roomConfigOccupantsPage) onOwnerJidEdited(_ gtki.CellRendererText, path string, newValue string) {
+	p.updateOccupantListCellForString("owners", p.ownersListController, ownersListColumnJidIndex, path, newValue)
+}
+
+func (p *roomConfigOccupantsPage) onAdminJidEdited(_ gtki.CellRendererText, path string, newValue string) {
+	p.updateOccupantListCellForString("admins", p.adminsListController, adminsListColumnJidIndex, path, newValue)
+}
+
+func (p *roomConfigOccupantsPage) updateOccupantListCellForString(list string, controller *mucRoomConfigListController, column int, path string, newValue string) {
+	err := controller.updateCellForString(column, path, newValue)
+	if err != nil {
+		p.log.WithError(err).WithFields(log.Fields{
+			"path":    path,
+			"newText": newValue,
+			"list":    list,
+		}).Error("The occupant's jid can't be updated")
+	}
+}
+
+func (p *roomConfigOccupantsPage) initOccupantsEditableCells() {
+	p.ownersListJidRenderer.SetProperty("editable", true)
+	p.adminListJidRenderer.SetProperty("editable", true)
 }
 
 func (p *roomConfigOccupantsPage) initOccupantsLists(parent gtki.Window) {

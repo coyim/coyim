134d2f2bac5862c26901826536a9014ec667a588
cnaranjo <crispool@hotmail.com>
2021-02-01 15:58:29 -0500

    [cm] #634 - Adds functionality that enable or disable the apply button if the occupant role change

--- gui/muc_room_occupant_role.go
+++ gui/muc_room_occupant_role.go
@@ -19,12 +19,13 @@ type occupantRoleUpdateView struct {
 	occupant       *muc.Occupant
 	rosterInfoView *roomViewRosterInfo
 
-	dialog         gtki.Dialog      `gtk-widget:"role-dialog"`
-	roleLabel      gtki.Label       `gtk-widget:"role-type-label"`
-	moderatorRadio gtki.RadioButton `gtk-widget:"role-moderator"`
-	reasonLabel    gtki.Label       `gtk-widget:"role-reason-label"`
-	reasonEntry    gtki.TextView    `gtk-widget:"role-reason-entry"`
-	applyButton    gtki.Button      `gtk-widget:"role-apply-button"`
+	dialog           gtki.Dialog      `gtk-widget:"role-dialog"`
+	roleLabel        gtki.Label       `gtk-widget:"role-type-label"`
+	moderatorRadio   gtki.RadioButton `gtk-widget:"role-moderator"`
+	participantRadio gtki.RadioButton `gtk-widget:"role-participant"`
+	reasonLabel      gtki.Label       `gtk-widget:"role-reason-label"`
+	reasonEntry      gtki.TextView    `gtk-widget:"role-reason-entry"`
+	applyButton      gtki.Button      `gtk-widget:"role-apply-button"`
 }
 
 func (r *roomViewRosterInfo) newOccupantRoleUpdateView(a *account, roomID jid.Bare, o *muc.Occupant) *occupantRoleUpdateView {
@@ -49,7 +50,13 @@ func (rv *occupantRoleUpdateView) initBuilder() {
 		"on_cancel":    rv.closeDialog,
 		"on_apply":     rv.onApply,
 		"on_key_press": rv.onKeyPress,
+		"on_toggled":   rv.onRadioButtonToggled,
 	})
+}
+
+// onRadioButtonToggled MUST be called from the UI thread
+func (rv *occupantRoleUpdateView) onRadioButtonToggled() {
+	rv.applyButton.SetSensitive(rv.occupant.Role.Name() != rv.getRoleBasedOnRadioSelected().Name())
 }
 
 func (rv *occupantRoleUpdateView) onKeyPress(_ gtki.Widget, ev gdki.Event) {
@@ -61,8 +68,6 @@ func (rv *occupantRoleUpdateView) onKeyPress(_ gtki.Widget, ev gdki.Event) {
 func (rv *occupantRoleUpdateView) initDefaults() {
 	rv.dialog.SetTransientFor(rv.rosterInfoView.parentWindow())
 	mucStyles.setFormSectionLabelStyle(rv.roleLabel)
-	// TODO: This button is enabled until another radio option will be added
-	rv.applyButton.SetSensitive(true)
 
 	switch rv.occupant.Role.(type) {
 	case *data.ModeratorRole:
@@ -80,6 +85,8 @@ func (rv *occupantRoleUpdateView) getRoleBasedOnRadioSelected() data.Role {
 	switch {
 	case rv.moderatorRadio.GetActive():
 		return &data.ModeratorRole{}
+	case rv.participantRadio.GetActive():
+		return &data.ParticipantRole{}
 	default:
 		return &data.NoneRole{}
 	}

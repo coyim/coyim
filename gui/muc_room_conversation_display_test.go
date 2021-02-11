62f6e406b31b2ed5cbe85eaf853b6351c353beec
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-02-11 10:56:43 -0500

    [mp] muc-room-conversation - Removes affiliations and roles functionalities from the main file and implements some tests for common display functions In some way related to #632

--- gui/muc_room_conversation_display_test.go
+++ gui/muc_room_conversation_display_test.go
@@ -2,18 +2,16 @@ package gui
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/coyim/coyim/i18n"
 	"github.com/coyim/gotk3adapter/glib_mock"
 
-	"github.com/coyim/coyim/session/muc/data"
 	. "gopkg.in/check.v1"
 )
 
 type MUCRoomConversationDisplaySuite struct{}
 
-var _ = Suite(&JoinMUCRoomSuite{})
+var _ = Suite(&MUCRoomConversationDisplaySuite{})
 
 type mucRoomConversationDisplayMockGlib struct {
 	glib_mock.Mock
@@ -27,198 +25,25 @@ func (*mucRoomConversationDisplayMockGlib) Localf(vx string, args ...interface{}
 	return fmt.Sprintf(vx, args...)
 }
 
-func (*SignalsSuite) Test_mucRoomConversationDisplay_displayForAffiliationUpdate(c *C) {
+func initMUCRoomConversationDisplayI18n() {
 	i18n.InitLocalization(&mucRoomConversationDisplayMockGlib{})
-
-	none := newAffiliationFromString(data.AffiliationNone)
-	outcast := newAffiliationFromString(data.AffiliationOutcast)
-	member := newAffiliationFromString(data.AffiliationMember)
-
-	d := newAffiliationUpdateDisplayData(data.AffiliationUpdate{
-		New:      member,
-		Previous: none,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "nick",
-			Actor: &data.OccupantUpdateActor{
-				Nickname: "alex",
-			},
-		},
-	})
-
-	c.Assert(displayAffiliationUpdateMessage(d), Equals,
-		"alex changed the position of nick to member")
-
-	c.Assert(getDisplayForOccupantAffiliationUpdate(data.AffiliationUpdate{
-		New:      none,
-		Previous: member,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "robin",
-			Reason:   "I'm batman",
-			Actor: &data.OccupantUpdateActor{
-				Nickname:    "batman",
-				Affiliation: member,
-			},
-		},
-	}), Equals, "The member batman removed the member position from robin because: I'm batman")
-
-	c.Assert(getDisplayForOccupantAffiliationUpdate(data.AffiliationUpdate{
-		New:      outcast,
-		Previous: member,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "bob",
-			Reason:   "he was rude",
-			Actor: &data.OccupantUpdateActor{
-				Nickname:    "alice",
-				Affiliation: member,
-			},
-		},
-	}), Equals, "The member alice banned bob from the room because: he was rude")
-
-	c.Assert(getDisplayForOccupantAffiliationUpdate(data.AffiliationUpdate{
-		New:      none,
-		Previous: outcast,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "nick",
-			Actor: &data.OccupantUpdateActor{
-				Nickname:    "jonathan",
-				Affiliation: outcast,
-			},
-		},
-	}), Equals, "The outcast jonathan removed the outcast position from nick")
 }
 
-func (*SignalsSuite) Test_mucRoomConversationDisplay_displayForAffiliationRemoved(c *C) {
-	i18n.InitLocalization(&mucRoomConversationDisplayMockGlib{})
+func (*MUCRoomConversationDisplaySuite) Test_mucRoomConversationDisplay_getDisplayRoomSubjectForNickname(c *C) {
+	c.Assert(getDisplayRoomSubjectForNickname("", "room subject"), Equals,
+		"Someone has updated the room subject to: \"room subject\"")
 
-	member := newAffiliationFromString(data.AffiliationMember)
-	admin := newAffiliationFromString(data.AffiliationAdmin)
-	owner := newAffiliationFromString(data.AffiliationOwner)
-	none := newAffiliationFromString(data.AffiliationNone)
+	c.Assert(getDisplayRoomSubjectForNickname("nick", "new subject"), Equals,
+		"nick updated the room subject to: \"new subject\"")
 
-	d := newAffiliationUpdateDisplayData(data.AffiliationUpdate{
-		New:      none,
-		Previous: admin,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "jonathan",
-		},
-	})
-
-	c.Assert(strings.Contains(d.displayForAffiliationRemoved(), "nick"), Equals, false)
-
-	d.nickname = "alice"
-	d.actor = ""
-	c.Assert(strings.Contains(d.displayForAffiliationRemoved(), "administrator"), Equals, true)
-
-	d.nickname = "alberto"
-	d.actor = "me"
-	c.Assert(strings.Contains(d.displayForAffiliationRemoved(), "me"), Equals, true)
-
-	d.nickname = "nick"
-	d.previousAffiliation = member
-	d.actor = ""
-	c.Assert(d.displayForAffiliationRemoved(), Equals,
-		"The member position of nick was removed")
-
-	d.nickname = "007"
-	d.previousAffiliation = owner
-	d.actor = "maria"
-	d.actorAffiliation = owner
-	c.Assert(d.displayForAffiliationRemoved(), Equals,
-		"The owner maria removed the owner position from 007")
+	c.Assert(getDisplayRoomSubjectForNickname("batman", "the batman cave"), Equals,
+		"batman updated the room subject to: \"the batman cave\"")
 }
 
-func (*SignalsSuite) Test_mucRoomConversationDisplay_displayForAffiliationOutcast(c *C) {
-	i18n.InitLocalization(&mucRoomConversationDisplayMockGlib{})
+func (*MUCRoomConversationDisplaySuite) Test_mucRoomConversationDisplay_getDisplayRoomSubject(c *C) {
+	c.Assert(getDisplayRoomSubject(""), Equals,
+		"The room does not have a subject")
 
-	d := newAffiliationUpdateDisplayData(data.AffiliationUpdate{
-		New: newAffiliationFromString(data.AffiliationOutcast),
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "nick",
-		},
-	})
-
-	c.Assert(d.displayForAffiliationOutcast(), Equals,
-		"nick was banned from the room")
-
-	d.nickname = "jonathan"
-	d.actor = "maria"
-	c.Assert(d.displayForAffiliationOutcast(), Equals,
-		"maria banned jonathan from the room")
-}
-
-func (*SignalsSuite) Test_mucRoomConversationDisplay_displayForAffiliationAdded(c *C) {
-	i18n.InitLocalization(&mucRoomConversationDisplayMockGlib{})
-
-	member := newAffiliationFromString(data.AffiliationMember)
-	admin := newAffiliationFromString(data.AffiliationAdmin)
-	owner := newAffiliationFromString(data.AffiliationOwner)
-	none := newAffiliationFromString(data.AffiliationNone)
-
-	d := newAffiliationUpdateDisplayData(data.AffiliationUpdate{
-		New:      member,
-		Previous: none,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "nick",
-		},
-	})
-
-	c.Assert(d.displayForAffiliationAdded(), Equals,
-		"nick is now a member")
-
-	d.nickname = "maria"
-	d.newAffiliation = admin
-	d.actor = "alberto"
-	d.actorAffiliation = admin
-	c.Assert(d.displayForAffiliationAdded(), Equals,
-		"The administrator alberto changed the position of maria to administrator")
-
-	d.nickname = "alice"
-	d.newAffiliation = owner
-	d.actor = "bob"
-	d.actorAffiliation = owner
-	c.Assert(d.displayForAffiliationAdded(), Equals,
-		"The owner bob changed the position of alice to owner")
-}
-
-func (*SignalsSuite) Test_mucRoomConversationDisplay_displayForAffiliationChanged(c *C) {
-	i18n.InitLocalization(&mucRoomConversationDisplayMockGlib{})
-
-	member := newAffiliationFromString(data.AffiliationMember)
-	admin := newAffiliationFromString(data.AffiliationAdmin)
-	owner := newAffiliationFromString(data.AffiliationOwner)
-
-	d := newAffiliationUpdateDisplayData(data.AffiliationUpdate{
-		New:      member,
-		Previous: admin,
-		OccupantUpdateAffiliationRole: data.OccupantUpdateAffiliationRole{
-			Nickname: "nick",
-		},
-	})
-
-	c.Assert(d.displayForAffiliationChanged(), Equals,
-		"The position of nick was changed from administrator to member")
-
-	d.nickname = "maria"
-	d.newAffiliation = admin
-	d.previousAffiliation = member
-	d.actor = "juan"
-	d.actorAffiliation = member
-	c.Assert(d.displayForAffiliationChanged(), Equals,
-		"The member juan changed the position of maria from member to administrator")
-
-	d.nickname = "alice"
-	d.newAffiliation = owner
-	d.previousAffiliation = member
-	d.actor = "bob"
-	d.actorAffiliation = member
-	c.Assert(d.displayForAffiliationChanged(), Equals,
-		"The member bob changed the position of alice from member to owner")
-}
-
-func newAffiliationFromString(s string) data.Affiliation {
-	a, err := data.AffiliationFromString(s)
-	if err != nil {
-		return nil
-	}
-	return a
+	c.Assert(getDisplayRoomSubject("the batman cave"), Equals,
+		"The room subject is \"the batman cave\"")
 }

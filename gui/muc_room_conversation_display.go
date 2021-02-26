df37e971fea33578a63d2c6944fd99811c073410
Sandy <sandy.acurio@gmail.com>
2021-02-26 15:46:18 -0500

    [ps] #624, #628, #632, #633, #637, #644, #648, #651 - Refactor display helper functions for affiliations and roles

--- gui/muc_room_conversation_display.go
+++ gui/muc_room_conversation_display.go
@@ -2,6 +2,7 @@ package gui
 
 import (
 	"github.com/coyim/coyim/i18n"
+	"github.com/coyim/coyim/session/muc/data"
 )
 
 func getDisplayRoomSubjectForNickname(nickname, subject string) string {
@@ -18,4 +19,56 @@ func getDisplayRoomSubject(subject string) string {
 	}
 
 	return i18n.Localf("The room subject is \"%s\"", subject)
+}
+
+func displayActorWithAffiliation(actor string, affiliation data.Affiliation) string {
+	if affiliation != nil {
+		return i18n.Localf("The %s %s", displayNameForAffiliation(affiliation), actor)
+	}
+	return actor
+}
+
+func displayNameForAffiliation(affiliation data.Affiliation) string {
+	switch {
+	case affiliation.IsAdmin():
+		return i18n.Local("administrator")
+	case affiliation.IsOwner():
+		return i18n.Local("owner")
+	case affiliation.IsBanned():
+		return i18n.Local("outcast")
+	case affiliation.IsMember():
+		return i18n.Local("member")
+	default: // Other values get the default treatment
+		return ""
+	}
+}
+
+func displayNameForAffiliationWithPreposition(affiliation data.Affiliation) string {
+	switch {
+	case affiliation.IsAdmin():
+		return i18n.Local("an administrator")
+	case affiliation.IsOwner():
+		return i18n.Local("an owner")
+	case affiliation.IsMember():
+		return i18n.Local("a member")
+	case affiliation.IsBanned():
+		return i18n.Local("a banned")
+	default: // Other values get the default treatment
+		return ""
+	}
+}
+
+func displayNameForRole(role data.Role) string {
+	switch {
+	case role.IsModerator():
+		return i18n.Local("moderator")
+	case role.IsParticipant():
+		return i18n.Local("participant")
+	case role.IsVisitor():
+		return i18n.Local("visitor")
+	case role.IsNone():
+		return i18n.Local("removed")
+	default: // Other values get the default treatment
+		return ""
+	}
 }

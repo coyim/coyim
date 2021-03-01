419d476d695c9520595de6eb23826ea2421b8c06
Ola Bini <ola@olabini.se>
2021-03-01 15:14:10 +0000

    Test rest of servers package

--- servers/known_test.go
+++ servers/known_test.go
@@ -18,7 +18,52 @@ func (s *KnownSuite) Test_Get_returnsTheServerIfItExists(c *C) {
 }
 
 func (s *KnownSuite) Test_register_willAddANewServer(c *C) {
+	origKnown := known
+	defer func() {
+		known = origKnown
+	}()
+
+	known = map[string]Server{}
+
 	Server{"something.de", "123123123.onion", false, false, false}.register()
 	serv, _ := Get("something.de")
 	c.Assert(serv.Onion, Equals, "123123123.onion")
+}
+
+func (s *KnownSuite) Test_register_willPanicIfDoubleRegistering(c *C) {
+	origKnown := known
+	defer func() {
+		known = origKnown
+	}()
+
+	known = map[string]Server{}
+
+	srv := Server{"something.de", "123123123.onion", false, false, false}
+
+	srv.register()
+
+	c.Assert(srv.register, PanicMatches, "double registration of something.de")
+}
+
+func (s *KnownSuite) Test_GetOnion_failsWhenServerNotKnown(c *C) {
+	res, ok := GetOnion("somewhere.com")
+	c.Assert(res, Equals, "")
+	c.Assert(ok, Equals, false)
+}
+
+func (s *KnownSuite) Test_GetOnion_failsWhenServerDoesntHaveOnion(c *C) {
+	res, ok := GetOnion("jabber.at")
+	c.Assert(res, Equals, "")
+	c.Assert(ok, Equals, false)
+}
+
+func (s *KnownSuite) Test_GetOnion_returnsOnion(c *C) {
+	res, ok := GetOnion("jabber.ccc.de")
+	c.Assert(res, Equals, "okj7xc6j2szr2y75.onion")
+	c.Assert(ok, Equals, true)
+}
+
+func (s *KnownSuite) Test_GetServersForRegistration(c *C) {
+	res := GetServersForRegistration()
+	c.Assert(res, HasLen, 58)
 }

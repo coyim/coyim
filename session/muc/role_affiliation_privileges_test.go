aa6a382e6eb7ab59832f9aeedcf5cbaf2bf0ce86
cnaranjo <crispool@hotmail.com>
2021-03-02 12:44:31 -0500

    [cp] #570 - Improves test structure using tables

--- session/muc/role_affiliation_privileges_test.go
+++ session/muc/role_affiliation_privileges_test.go
@@ -3,6 +3,7 @@ package muc
 import (
 	"io/ioutil"
 
+	"github.com/coyim/coyim/session/muc/data"
 	log "github.com/sirupsen/logrus"
 	. "gopkg.in/check.v1"
 )
@@ -15,54 +16,110 @@ type MucOccupantRoleAffiliationPrivilegesSuite struct{}
 
 var _ = Suite(&MucOccupantRoleAffiliationPrivilegesSuite{})
 
+type canKickOccupantTest struct {
+	actor    *Occupant
+	occupant *Occupant
+	expected bool
+}
+
+func newTestOccupant(affiliation data.Affiliation, role data.Role) *Occupant {
+	return &Occupant{
+		Affiliation: affiliation,
+		Role:        role,
+	}
+}
+
 func (*MucOccupantRoleAffiliationPrivilegesSuite) Test_RoleModeratorAffiliationNone_CanKickAnOccupant(c *C) {
-	actor := &Occupant{}
-	actor.ChangeRoleToModerator()
-	actor.ChangeAffiliationToNone()
+	testCases := []canKickOccupantTest{
+		// Actor: Moderator - None
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.MemberAffiliation{}, &data.ModeratorRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.AdminAffiliation{}, &data.ModeratorRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.OwnerAffiliation{}, &data.ModeratorRole{}),
+			expected: false,
+		},
 
-	oc := &Occupant{}
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.NoneAffiliation{}, &data.ParticipantRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.MemberAffiliation{}, &data.ParticipantRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.AdminAffiliation{}, &data.ParticipantRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.OwnerAffiliation{}, &data.ParticipantRole{}),
+			expected: false,
+		},
 
-	// Testing role moderator
-	oc.ChangeRoleToModerator()
-	oc.ChangeAffiliationToNone()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.NoneAffiliation{}, &data.VisitorRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.MemberAffiliation{}, &data.VisitorRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.AdminAffiliation{}, &data.VisitorRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.OwnerAffiliation{}, &data.VisitorRole{}),
+			expected: false,
+		},
 
-	oc.ChangeAffiliationToMember()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.NoneAffiliation{}, &data.NoneRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.MemberAffiliation{}, &data.NoneRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.AdminAffiliation{}, &data.NoneRole{}),
+			expected: false,
+		},
+		{
+			actor:    newTestOccupant(&data.NoneAffiliation{}, &data.ModeratorRole{}),
+			occupant: newTestOccupant(&data.OwnerAffiliation{}, &data.NoneRole{}),
+			expected: false,
+		},
+	}
 
-	oc.ChangeAffiliationToAdmin()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToOwner()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	// Testing role participant
-	oc.ChangeRoleToParticipant()
-	oc.ChangeAffiliationToNone()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToMember()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToAdmin()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToOwner()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	// Testing role visitor
-	oc.ChangeRoleToVisitor()
-	oc.ChangeAffiliationToNone()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToMember()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToAdmin()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
-
-	oc.ChangeAffiliationToOwner()
-	c.Assert(actor.CanKickOccupant(oc), Equals, false)
+	for _, scenario := range testCases {
+		c.Assert(scenario.actor.CanKickOccupant(scenario.occupant), Equals, scenario.expected)
+	}
 }
 
 func (*MucOccupantRoleAffiliationPrivilegesSuite) Test_RoleModeratorAffiliationMember_CanKickAnOccupant(c *C) {

c0cd10faf0f2bfb59ba5b523891773aed1e6b44d
Ola Bini <ola@olabini.se>
2021-03-09 16:12:00 +0000

    Test XMPP cookie functionality

--- xmpp/cookie_test.go
+++ xmpp/cookie_test.go
@@ -3,6 +3,7 @@ package xmpp
 import (
 	"errors"
 
+	"github.com/coyim/coyim/xmpp/data"
 	. "gopkg.in/check.v1"
 )
 
@@ -20,4 +21,18 @@ func (s *CookieXMPPSuite) Test_getCookie_panicsOnReadFailure(c *C) {
 	c.Assert(func() {
 		conn.getCookie()
 	}, PanicMatches, "Failed to read random bytes: EOF")
+}
+
+func (s *CookieXMPPSuite) Test_conn_cancelInFlights_works(c *C) {
+	conn := conn{
+		log: testLogger(),
+	}
+
+	conn.inflights = map[data.Cookie]inflight{
+		data.Cookie(42): inflight{replyChan: make(chan data.Stanza)},
+	}
+
+	conn.cancelInflights()
+
+	c.Assert(conn.inflights, HasLen, 0)
 }

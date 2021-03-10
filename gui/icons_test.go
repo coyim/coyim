c6a9cecccd679a134c51ee6cddd5742fb5255b1f
Ola Bini <ola@olabini.se>
2021-03-10 16:52:37 +0000

    #725 - use case insensitive regexp to check for result

--- gui/icons_test.go
+++ gui/icons_test.go
@@ -7,5 +7,5 @@ type IconsSuite struct{}
 var _ = Suite(&IconsSuite{})
 
 func (*IconsSuite) Test_icons_getPath(c *C) {
-	c.Assert(coyimIcon.getPath(), Matches, ".*?icon_256x256\\.png$")
+	c.Assert(coyimIcon.getPath(), Matches, "(?i).*?icon_256x256\\.png$")
 }

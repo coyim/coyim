0de01d2ff81dba68ed3b1c5728302aca00132cf3
cnaranjo <crispool@hotmail.com>
2021-03-16 15:16:14 -0500

    [cm] #639 - Adds functionality that allows show/hide the password and confirm password entries

--- gui/password_confirmation_component.go
+++ gui/password_confirmation_component.go
@@ -1,22 +1,37 @@
 package gui
 
 import (
+	"github.com/coyim/coyim/i18n"
 	"github.com/coyim/gotk3adapter/gtki"
 )
 
 type passwordConfirmationComponent struct {
-	box          gtki.Box   `gtk-widget:"password-confirmation-box"`
-	entry        gtki.Entry `gtk-widget:"password-entry"`
-	confirmEntry gtki.Entry `gtk-widget:"password-confirmation-entry"`
+	box                gtki.Box    `gtk-widget:"password-confirmation-box"`
+	entry              gtki.Entry  `gtk-widget:"password-entry"`
+	confirmEntry       gtki.Entry  `gtk-widget:"password-confirmation-entry"`
+	showPasswordButton gtki.Button `gtk-widget:"password-show-button"`
 }
 
 func (u *gtkUI) createPasswordConfirmationComponent() *passwordConfirmationComponent {
 	pc := &passwordConfirmationComponent{}
 
-	builder := newBuilder("PasswordConfirmation")
-	panicOnDevError(builder.bindObjects(pc))
+	pc.initBuilder()
+	pc.initDefaults()
 
 	return pc
+}
+
+func (pc *passwordConfirmationComponent) initBuilder() {
+	builder := newBuilder("PasswordConfirmation")
+	panicOnDevError(builder.bindObjects(pc))
+	builder.ConnectSignals(map[string]interface{}{
+		"on_show_password_clicked": pc.onShowPasswordClicked,
+		"on_password_change":       pc.onPasswordChange,
+	})
+}
+
+func (pc *passwordConfirmationComponent) initDefaults() {
+	pc.confirmEntry.SetSensitive(!pc.entry.GetVisibility())
 }
 
 func (pc *passwordConfirmationComponent) setPassword(p string) {
@@ -41,4 +56,38 @@ func (pc *passwordConfirmationComponent) focusConfirm() {
 
 func (pc *passwordConfirmationComponent) contentBox() gtki.Widget {
 	return pc.box
+}
+
+// onPasswordChange MUST be called from the UI thread
+func (pc *passwordConfirmationComponent) onPasswordChange() {
+	if pc.entry.GetVisibility() {
+		pc.updateConfirmPasswordEntry()
+	}
+}
+
+// onShowPasswordClicked MUST be called from the UI thread
+func (pc *passwordConfirmationComponent) onShowPasswordClicked() {
+	visible := pc.entry.GetVisibility()
+	if !visible {
+		pc.updateConfirmPasswordEntry()
+	}
+	pc.confirmEntry.SetVisibility(!visible)
+	pc.confirmEntry.SetSensitive(visible)
+	pc.entry.SetVisibility(!visible)
+	pc.updateShowPasswordLabel(!visible)
+}
+
+// updateShowPasswordLabel MUST be called from the UI thread
+func (pc *passwordConfirmationComponent) updateShowPasswordLabel(v bool) {
+	if v {
+		pc.showPasswordButton.SetLabel(i18n.Local("Hide"))
+		return
+	}
+	pc.showPasswordButton.SetLabel(i18n.Local("Show"))
+}
+
+// updateConfirmPasswordEntry MUST be called from the UI thread
+func (pc *passwordConfirmationComponent) updateConfirmPasswordEntry() {
+	pe, _ := pc.entry.GetText()
+	pc.confirmEntry.SetText(pe)
 }

fab922c7c4c80beb7649241684ac1e14fdaff3ac
Mauro Velasco <mauro.velasco@gmail.com>
2021-03-30 10:32:33 -0500

    [mp] #639 - Moves muc room list form item functionalities to his own file.

---
+++ gui/muc_room_config_list_add_item.go
@@ -0,0 +1,68 @@
+package gui
+
+import "github.com/coyim/gotk3adapter/gtki"
+
+type mucRoomConfigListFormItem struct {
+	form *roomConfigListForm
+
+	box          gtki.Box    `gtk-widget:"room-config-list-add-item-box"`
+	formBox      gtki.Box    `gtk-widget:"room-config-list-add-item-form-box"`
+	addButton    gtki.Button `gtk-widget:"room-config-list-add-item-button"`
+	removeButton gtki.Button `gtk-widget:"room-config-list-remove-item-button"`
+}
+
+func newMUCRoomConfigListFormItem(form *roomConfigListForm, onAdd func(jid string), onRemove func()) *mucRoomConfigListFormItem {
+	lfi := &mucRoomConfigListFormItem{
+		form: form,
+	}
+
+	lfi.initBuilder()
+	lfi.initListAdd(onAdd)
+	lfi.initListRemove(onRemove)
+	lfi.initDefaults()
+
+	return lfi
+}
+
+func (lfi *mucRoomConfigListFormItem) initBuilder() {
+	builder := newBuilder("MUCRoomConfigListAddFormItem")
+	panicOnDevError(builder.bindObjects(lfi))
+}
+
+func (lfi *mucRoomConfigListFormItem) initListAdd(onAdd func(jid string)) {
+	lfi.addButton.SetSensitive(false)
+	lfi.addButton.SetVisible(false)
+
+	if onAdd != nil {
+		lfi.addButton.Connect("clicked", func() {
+			onAdd(lfi.form.jid())
+			lfi.form.reset()
+			lfi.form.focus()
+		})
+
+		lfi.form.onFieldChanged(func() {
+			lfi.addButton.SetSensitive(lfi.form.isFilled())
+		})
+
+		lfi.addButton.SetVisible(true)
+	}
+}
+
+func (lfi *mucRoomConfigListFormItem) initListRemove(onRemove func()) {
+	lfi.removeButton.SetSensitive(false)
+	lfi.removeButton.SetVisible(false)
+
+	if onRemove != nil {
+		lfi.removeButton.Connect("clicked", onRemove)
+		lfi.removeButton.SetSensitive(true)
+		lfi.removeButton.SetVisible(true)
+	}
+}
+
+func (lfi *mucRoomConfigListFormItem) initDefaults() {
+	lfi.formBox.Add(lfi.form.formView)
+}
+
+func (lfi *mucRoomConfigListFormItem) contentBox() gtki.Box {
+	return lfi.box
+}

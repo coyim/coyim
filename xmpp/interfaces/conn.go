337c9380fdd8ddc3944722cf2cb7a415f484e095
Ola Bini <ola@olabini.se>
2021-04-09 22:02:16 +0000

    Divide XMPP Conn interface into many smaller sub-interfaces

--- xmpp/interfaces/conn.go
+++ xmpp/interfaces/conn.go
@@ -15,58 +15,113 @@ type Has interface {
 	Conn() Conn
 }
 
-// Conn represents a connection to an XMPP server.
-type Conn interface {
+// Authentication contains authentication related functionality for the XMPP connection
+type Authentication interface {
 	Authenticate(string, string) error
 	AuthenticationFailure() error
-	BindResource() error
-	Cancel(data.Cookie) bool
-	Cache() cache.WithExpiry
+}
+
+// Registration contains registration related functionality for the XMPP connection
+type Registration interface {
 	ChangePassword(string, string, string) error
-	Close() error
-	Config() *data.Config
-	CustomStorage() map[xml.Name]reflect.Type
+	RegisterAccount(string, string) (bool, error)
+}
+
+// Resource contains resource related functionality for the XMPP connection
+type Resource interface {
+	GetJIDResource() string
+	SetJIDResource(string)
+	BindResource() error
+	OriginDomain() string
+}
+
+// ConnectionData contains connection information for the XMPP connection
+type ConnectionData interface {
+	ServerAddress() string
+	SetInOut(*xml.Decoder, io.Writer)
+	SetKeepaliveOut(io.Writer)
+	SetRawOut(io.WriteCloser)
+	SetServerAddress(string)
+}
+
+// Presence contains functionality related to signaling presence for the XMPP connection
+type Presence interface {
+	SendPresence(string, string, string, string) error
+	SendMUCPresence(string, *data.MUC) error
+	SignalPresence(string) error
+}
+
+// Features contains functionality for managing discovery of features and identities for the XMPP connection
+type Features interface {
 	DiscoveryFeatures(string) ([]string, bool)
 	DiscoveryFeaturesAndIdentities(string) ([]data.DiscoveryIdentity, []string, bool)
 	Features() data.StreamFeatures
-	GetRosterDelimiter() (string, error)
+	HasSupportTo(entity string, features ...string) bool
+	QueryServiceInformation(entity string) (*data.DiscoveryInfoQuery, error)
+	QueryServiceItems(entity string) (*data.DiscoveryItemsQuery, error)
+	EntityExists(entity string) (bool, error)
+	ServerHasFeature(ns string) bool
+}
+
+// Transport contains functionality for managing the transport of information over the XMPP connection
+type Transport interface {
+	SetChannelBinding([]byte)
+	GetChannelBinding() []byte
+	Close() error
+}
+
+// InputOutput manages the low level input and output functionality of the XMPP connection
+type InputOutput interface {
 	In() *xml.Decoder
-	Lock() *sync.Mutex
 	Next() (data.Stanza, error)
-	OriginDomain() string
 	Out() io.Writer
-	Rand() io.Reader
 	RawOut() io.WriteCloser
-	ReadStanzas(chan<- data.Stanza) error
-	RegisterAccount(string, string) (bool, error)
+}
+
+// Configuration contains functionality related to the configuration of the XMPP connection
+type Configuration interface {
+	Cache() cache.WithExpiry
+	Config() *data.Config
+	CustomStorage() map[xml.Name]reflect.Type
+	Lock() *sync.Mutex
+	Rand() io.Reader
+}
+
+// Requests manages the different request functionalities for the XMPP connection
+type Requests interface {
+	GetRosterDelimiter() (string, error)
 	RequestRoster() (<-chan data.Stanza, data.Cookie, error)
 	RequestVCard() (<-chan data.Stanza, data.Cookie, error)
+}
+
+// Reading gives access to reading of stanzas over the XMPP connection
+type Reading interface {
+	ReadStanzas(chan<- data.Stanza) error
+}
+
+// Sending allows for sending of different kinds of data over the XMPP connection
+type Sending interface {
 	Send(string, string, bool) error
 	SendMessage(*data.Message) error
 	SendIQ(string, string, interface{}) (<-chan data.Stanza, data.Cookie, error)
 	SendIQReply(string, string, string, interface{}) error
 	SendInitialStreamHeader() error
 	SendPing() (reply <-chan data.Stanza, cookie data.Cookie, err error)
-	SendPresence(string, string, string, string) error
-	SendMUCPresence(string, *data.MUC) error
-	ServerAddress() string
-	SetInOut(*xml.Decoder, io.Writer)
-	SetKeepaliveOut(io.Writer)
-	SetRawOut(io.WriteCloser)
-	SetServerAddress(string)
-	SignalPresence(string) error
+	Cancel(data.Cookie) bool
+}
 
-	SetChannelBinding([]byte)
-	GetChannelBinding() []byte
-
-	GetJIDResource() string
-	SetJIDResource(string)
-
-	HasSupportTo(entity string, features ...string) bool
-
-	QueryServiceInformation(entity string) (*data.DiscoveryInfoQuery, error)
-	QueryServiceItems(entity string) (*data.DiscoveryItemsQuery, error)
-	EntityExists(entity string) (bool, error)
-
-	ServerHasFeature(ns string) bool
+// Conn represents a connection to an XMPP server.
+type Conn interface {
+	ConnectionData
+	Authentication
+	Registration
+	Resource
+	Presence
+	Features
+	Transport
+	InputOutput
+	Configuration
+	Requests
+	Reading
+	Sending
 }

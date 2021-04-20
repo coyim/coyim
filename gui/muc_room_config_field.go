c92e6839e6d25733af221627d35f281434b11967
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-20 16:14:12 -0500

    [cps] #639 muc-room-config-field - Implements a basic interface and structure that contains information for any configuration field.

---
+++ gui/muc_room_config_field.go
@@ -0,0 +1,61 @@
+package gui
+
+import (
+	"errors"
+
+	"github.com/coyim/coyim/session/muc"
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+type hasRoomConfigFormField interface {
+	fieldWidget() gtki.Widget
+	fieldName() string
+	fieldLabel() string
+	fieldValue() interface{}
+}
+
+type roomConfigFormField struct {
+	field *muc.RoomConfigFormField
+
+	widget      gtki.Box   `gtk-widget:"room-config-field-box"`
+	label       gtki.Label `gtk-widget:"room-config-field-label"`
+	description gtki.Label `gtk-widget:"room-config-field-description"`
+
+	builder *builder
+}
+
+func newRoomConfigFormField(field *muc.RoomConfigFormField, template string) *roomConfigFormField {
+	f := &roomConfigFormField{
+		field: field,
+	}
+
+	f.initBuilder(template)
+	f.initDefaults()
+
+	return f
+}
+
+func (f *roomConfigFormField) initBuilder(template string) {
+	f.builder = newBuilder(template)
+	panicOnDevError(f.builder.bindObjects(f))
+}
+
+func (f *roomConfigFormField) initDefaults() {
+	f.label.SetText(f.field.Label)
+}
+
+func (f *roomConfigFormField) fieldWidget() gtki.Widget {
+	return f.widget
+}
+
+func (f *roomConfigFormField) fieldName() string {
+	return f.field.Name
+}
+
+func (f *roomConfigFormField) fieldLabel() string {
+	return f.field.Label
+}
+
+func (f *roomConfigFormField) fieldValue() interface{} {
+	return f.field.Value
+}

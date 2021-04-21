bfb5aa43c20496af94fca7d9d215b26b1734568f
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-21 14:28:21 -0500

    #571 - Does multiple refactoring in order to allow `occupantActionView` to be responsible for the logic of functionality.

--- gui/muc_room_occupant_action.go
+++ gui/muc_room_occupant_action.go
@@ -15,6 +15,8 @@ type occupantActionViewData struct {
 	headerText  string
 	messageText string
 	reasonText  string
+
+	confirmationAction func(reason string) // confirmationAction will not be called from the UI thread
 }
 
 type occupantActionView struct {
@@ -27,13 +29,14 @@ type occupantActionView struct {
 	reason      gtki.TextView `gtk-widget:"occupant-action-reason-entry"`
 	reasonLabel gtki.Label    `gtk-widget:"occupant-action-reason-label"`
 
-	confirmationAction func() // confirmationAction will not be called from the UI thread
+	confirmationAction func(reason string) // confirmationAction will not be called from the UI thread
 }
 
 func newOccupantActionView(d *occupantActionViewData) *occupantActionView {
 	oa := &occupantActionView{
-		occupant:   d.occupant,
-		rosterView: d.rosterView,
+		occupant:           d.occupant,
+		rosterView:         d.rosterView,
+		confirmationAction: d.confirmationAction,
 	}
 
 	oa.initBuilder()
@@ -65,55 +68,10 @@ func (oa *occupantActionView) initDialogTitleAndTexts(d *occupantActionViewData)
 	oa.reasonLabel.SetText(d.reasonText)
 }
 
-func (r *roomViewRoster) newKickOccupantView(o *muc.Occupant) *occupantActionView {
-	k := newOccupantActionView(&occupantActionViewData{
-		occupant:   o,
-		rosterView: r,
-
-		dialogTitle: i18n.Localf("Expel %s from the room", o.Nickname),
-		headerText:  i18n.Localf("You are about to temporarily remove %s from the room.", o.Nickname),
-		messageText: i18n.Local("They will be able to join the room again. Are you sure you want to continue?"),
-		reasonText:  i18n.Local("Here you can provide an optional reason for removing the person. Everyone in the room will see this reason."),
-	})
-
-	k.initKickOccupantDefaults()
-
-	return k
-}
-
-// initKickOccupantDefaults MUST be called from the UI thread
-func (oa *occupantActionView) initKickOccupantDefaults() {
-	oa.confirmationAction = func() {
-		oa.rosterView.updateOccupantRole(oa.occupant, &data.NoneRole{}, getTextViewText(oa.reason))
-	}
-}
-
-func (r *roomViewRoster) newBanOccupantView(o *muc.Occupant) *occupantActionView {
-	k := newOccupantActionView(&occupantActionViewData{
-		occupant:   o,
-		rosterView: r,
-
-		dialogTitle: i18n.Localf("Ban %s from the room", o.Nickname),
-		headerText:  i18n.Localf("You are about to ban %s from the room", o.Nickname),
-		messageText: i18n.Local("They won't be able to join the room again. Are you sure you want to continue?"),
-		reasonText:  i18n.Local("Here you can provide an optional reason for banning the person. Everyone in the room will see this reason."),
-	})
-
-	k.initBanOccupantDefaults()
-
-	return k
-}
-
-// initBanOccupantDefaults MUST be called from the UI thread
-func (oa *occupantActionView) initBanOccupantDefaults() {
-	oa.confirmationAction = func() {
-		oa.rosterView.updateOccupantAffiliation(oa.occupant, &data.OutcastAffiliation{}, getTextViewText(oa.reason))
-	}
-}
-
 // onConfirmClicked MUST be called from the UI thread
 func (oa *occupantActionView) onConfirmClicked() {
-	go oa.confirmationAction()
+	reason := getTextViewText(oa.reason)
+	go oa.confirmationAction(reason)
 	oa.close()
 }
 
@@ -130,4 +88,40 @@ func (oa *occupantActionView) show() {
 // close MUST be called from the UI thread
 func (oa *occupantActionView) close() {
 	oa.dialog.Destroy()
+}
+
+func (r *roomViewRoster) newKickOccupantView(o *muc.Occupant) *occupantActionView {
+	k := newOccupantActionView(&occupantActionViewData{
+		occupant:   o,
+		rosterView: r,
+
+		dialogTitle: i18n.Localf("Expel %s from the room", o.Nickname),
+		headerText:  i18n.Localf("You are about to temporarily remove %s from the room.", o.Nickname),
+		messageText: i18n.Local("They will be able to join the room again. Are you sure you want to continue?"),
+		reasonText:  i18n.Local("Here you can provide an optional reason for removing the person. Everyone in the room will see this reason."),
+
+		confirmationAction: func(reason string) {
+			r.updateOccupantRole(o, &data.NoneRole{}, reason)
+		},
+	})
+
+	return k
+}
+
+func (r *roomViewRoster) newBanOccupantView(o *muc.Occupant) *occupantActionView {
+	k := newOccupantActionView(&occupantActionViewData{
+		occupant:   o,
+		rosterView: r,
+
+		dialogTitle: i18n.Localf("Ban %s from the room", o.Nickname),
+		headerText:  i18n.Localf("You are about to ban %s from the room", o.Nickname),
+		messageText: i18n.Local("They won't be able to join the room again. Are you sure you want to continue?"),
+		reasonText:  i18n.Local("Here you can provide an optional reason for banning the person. Everyone in the room will see this reason."),
+
+		confirmationAction: func(reason string) {
+			r.updateOccupantAffiliation(o, &data.OutcastAffiliation{}, reason)
+		},
+	})
+
+	return k
 }

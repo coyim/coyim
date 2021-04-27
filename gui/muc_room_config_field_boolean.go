05e9a289fadb3693c99b382784598ee494a4b957
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-27 16:08:14 -0500

    #639 muc-room-config-field-boolean - Refactor the implementation of the room config boolean field.

--- gui/muc_room_config_field_boolean.go
+++ gui/muc_room_config_field_boolean.go
@@ -6,55 +6,28 @@ import (
 )
 
 type roomConfigFormFieldBoolean struct {
-	field *muc.RoomConfigFormField
+	*roomConfigFormField
 
-	widget      gtki.Box    `gtk-widget:"room-config-field-box"`
-	contentGrid gtki.Grid   `gtk-widget:"room-config-field-boolean-grid"`
-	label       gtki.Label  `gtk-widget:"room-config-field-label"`
-	entry       gtki.Switch `gtk-widget:"room-config-field-boolean"`
-	description gtki.Label  `gtk-widget:"room-config-field-description"`
+	grid   gtki.Grid   `gtk-widget:"room-config-field-boolean-grid"`
+	toggle gtki.Switch `gtk-widget:"room-config-field-boolean"`
 }
 
-func newRoomConfigFormFieldBoolean(field *muc.RoomConfigFormField) *roomConfigFormFieldBoolean {
-	f := &roomConfigFormFieldBoolean{field: field}
+func newRoomConfigFormFieldBoolean(f muc.HasRoomConfigFormField) hasRoomConfigFormField {
+	field := &roomConfigFormFieldBoolean{}
+	field.roomConfigFormField = newRoomConfigFormField(f, "MUCRoomConfigFormFieldBoolean")
 
-	f.initBuilder()
-	f.initDefaults()
-	return f
-}
+	panicOnDevError(field.builder.bindObjects(field))
 
-func (f *roomConfigFormFieldBoolean) initBuilder() {
-	builder := newBuilder("MUCRoomConfigFormFieldBoolean")
-	panicOnDevError(builder.bindObjects(f))
-}
-
-func (f *roomConfigFormFieldBoolean) initDefaults() {
-	f.label.SetText(f.field.Label)
-	f.entry.SetActive(f.field.Value.(bool))
-	if f.field.Description != "" {
-		f.description.SetText(f.field.Description)
+	if active, ok := field.fieldValue().(bool); ok {
+		field.toggle.SetActive(active)
 	}
+
+	return field
 }
 
-func (f *roomConfigFormFieldBoolean) fieldWidget() gtki.Widget {
-	return f.widget
-}
-
-func (f *roomConfigFormFieldBoolean) fieldName() string {
-	return f.field.Name
-}
-
-func (f *roomConfigFormFieldBoolean) fieldLabel() string {
-	return f.field.Label
-}
-
-func (f *roomConfigFormFieldBoolean) fieldValue() interface{} {
-	return f.entry.GetActive()
-}
-
+// refreshContent MUST NOT be called from the UI thread
 func (f *roomConfigFormFieldBoolean) refreshContent() {
 	doInUIThread(func() {
-		f.contentGrid.SetVAlign(gtki.ALIGN_CENTER)
-		f.description.SetVisible(f.field.Description != "")
+		f.grid.SetVAlign(gtki.ALIGN_CENTER)
 	})
 }

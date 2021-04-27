75ede29c7eba61074c2dd48ffd89c3be1acf0d69
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-27 16:07:05 -0500

    #639 muc-room-config-field - Adds a basic implementation of the UI for the base configuration form field.

--- gui/muc_room_config_field.go
+++ gui/muc_room_config_field.go
@@ -8,19 +8,76 @@ import (
 )
 
 type hasRoomConfigFormField interface {
-	fieldWidget() gtki.Widget
 	fieldName() string
 	fieldLabel() string
+	fieldDescription() string
 	fieldValue() interface{}
+	fieldWidget() gtki.Widget
 	refreshContent()
 }
+
+type roomConfigFormField struct {
+	field   muc.HasRoomConfigFormField
+	builder *builder
+
+	widget      gtki.Box   `gtk-widget:"room-config-field-box"`
+	label       gtki.Label `gtk-widget:"room-config-field-label"`
+	description gtki.Label `gtk-widget:"room-config-field-description"`
+}
+
+func newRoomConfigFormField(f muc.HasRoomConfigFormField, template string) *roomConfigFormField {
+	field := &roomConfigFormField{
+		field: f,
+	}
+
+	field.builder = newBuilder(template)
+	panicOnDevError(field.builder.bindObjects(field))
+
+	field.label.SetText(field.fieldLabel())
+
+	description := field.fieldDescription()
+	field.description.SetText(description)
+	if description == "" {
+		field.description.Hide()
+	}
+
+	return field
+}
+
+// fieldName implements the hasRoomConfigFormField interface
+func (f *roomConfigFormField) fieldName() string {
+	return f.field.Name()
+}
+
+// fieldLabel implements the hasRoomConfigFormField interface
+func (f *roomConfigFormField) fieldLabel() string {
+	return f.field.Label()
+}
+
+// fieldDescription implements the hasRoomConfigFormField interface
+func (f *roomConfigFormField) fieldDescription() string {
+	return f.field.Description()
+}
+
+// fieldValue implements the hasRoomConfigFormField interface
+func (f *roomConfigFormField) fieldValue() interface{} {
+	return f.field.Value()
+}
+
+// fieldWidget implements the hasRoomConfigFormField interface
+func (f *roomConfigFormField) fieldWidget() gtki.Widget {
+	return f.widget
+}
+
+// refreshContent implements the hasRoomConfigFormField interface
+func (f *roomConfigFormField) refreshContent() {}
 
 var (
 	errRoomConfigFieldNotSupported = errors.New("room configuration form field not supported")
 )
 
-func roomConfigFormFieldFactory(field *muc.RoomConfigFormField) (hasRoomConfigFormField, error) {
-	switch field.Type {
+func roomConfigFormFieldFactory(field muc.HasRoomConfigFormField) (hasRoomConfigFormField, error) {
+	switch field.Type() {
 	case muc.RoomConfigFieldText:
 		return newRoomConfigFormTextField(field), nil
 	case muc.RoomConfigFieldBoolean:

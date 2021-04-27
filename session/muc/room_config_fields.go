bb8ae0d1bf05509a5cbbe7179edbf7a3bdf23e97
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-27 14:56:08 -0500

    #639 - Moves to another file, all the configuration fields constants

--- session/muc/room_config_fields.go
+++ session/muc/room_config_fields.go
@@ -2,27 +2,6 @@ package muc
 
 import "strconv"
 
-const (
-	// RoomConfigFieldText represents a "text-single" config field type
-	RoomConfigFieldText = "text-single"
-	// RoomConfigFieldTextPrivate represents a "text-private" config field type
-	RoomConfigFieldTextPrivate = "text-private"
-	// RoomConfigFieldTextMulti represents a "text-multi" config field type
-	RoomConfigFieldTextMulti = "text-multi"
-	// RoomConfigFieldBoolean represents a "boolean" config field type
-	RoomConfigFieldBoolean = "boolean"
-	// RoomConfigFieldList represents a "list-single" config field type
-	RoomConfigFieldList = "list-single"
-	// RoomConfigFieldListMulti represents a "list-multi" config field type
-	RoomConfigFieldListMulti = "list-multi"
-	// RoomConfigFieldJidMulti represents a "jid-multi" config field type
-	RoomConfigFieldJidMulti = "jid-multi"
-	// RoomConfigFieldFixed represents a "fixed" config field type
-	RoomConfigFieldFixed = "fixed"
-	// RoomConfigFieldHidden represents a "hidden" config field type
-	RoomConfigFieldHidden = "hidden"
-)
-
 // RoomConfigFormField contains information about a room config form field
 type RoomConfigFormField struct {
 	Name, Type, Label, Description string
@@ -45,33 +24,6 @@ func (f *RoomConfigFormField) GetValue() (values []string) {
 	}
 	return
 }
-
-const (
-	// RoomConfigOptionModerators represents the field option for "moderators"
-	RoomConfigOptionModerators = "moderators"
-	// RoomConfigOptionParticipants represents the field option for "participants"
-	RoomConfigOptionParticipants = "participants"
-	// RoomConfigOptionAnyone represents the field opion for "anyone"
-	RoomConfigOptionAnyone = "anyone"
-	// RoomConfigOptionModerator represents the field option for "moderator"
-	RoomConfigOptionModerator = "moderator"
-	// RoomConfigOptionParticipant represents the field option for "participant"
-	RoomConfigOptionParticipant = "participant"
-	// RoomConfigOptionVisitor represents the field option for "visitor"
-	RoomConfigOptionVisitor = "visitor"
-	// RoomConfigOptionNone represents the field option for "none"
-	RoomConfigOptionNone = "0"
-	// RoomConfigOption10 represents the field option for "10"
-	RoomConfigOption10 = "10"
-	// RoomConfigOption20 represents the field option for "20"
-	RoomConfigOption20 = "20"
-	// RoomConfigOption30 represents the field option for "30"
-	RoomConfigOption30 = "30"
-	// RoomConfigOption50 represents the field option for "50"
-	RoomConfigOption50 = "50"
-	// RoomConfigOption100 represents the field option for "100"
-	RoomConfigOption100 = "100"
-)
 
 // ConfigListSingleField description
 type ConfigListSingleField interface {

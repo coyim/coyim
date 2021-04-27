5afce051759758f4087aded5d27338e842e97b19
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-04-27 16:07:53 -0500

    #639 muc-room-config-field-text - Refactor the implementation of the room config text field.

--- gui/muc_room_config_field_text.go
+++ gui/muc_room_config_field_text.go
@@ -6,49 +6,20 @@ import (
 )
 
 type roomConfigFormFieldText struct {
-	field *muc.RoomConfigFormField
+	*roomConfigFormField
 
-	widget      gtki.Box   `gtk-widget:"room-config-field-box"`
-	label       gtki.Label `gtk-widget:"room-config-field-label"`
-	entry       gtki.Entry `gtk-widget:"room-config-text-field-entry"`
-	description gtki.Label `gtk-widget:"room-config-field-description"`
+	entry gtki.Entry `gtk-widget:"room-config-text-field-entry"`
 }
 
-func newRoomConfigFormTextField(field *muc.RoomConfigFormField) *roomConfigFormFieldText {
-	f := &roomConfigFormFieldText{field: field}
+func newRoomConfigFormTextField(f muc.HasRoomConfigFormField) hasRoomConfigFormField {
+	field := &roomConfigFormFieldText{}
+	field.roomConfigFormField = newRoomConfigFormField(f, "MUCRoomConfigFormFieldText")
 
-	f.initBuilder()
-	f.initDefaults()
-	return f
-}
+	panicOnDevError(field.builder.bindObjects(field))
 
-func (f *roomConfigFormFieldText) initBuilder() {
-	builder := newBuilder("MUCRoomConfigFormFieldText")
-	panicOnDevError(builder.bindObjects(f))
-}
-
-func (f *roomConfigFormFieldText) initDefaults() {
-	f.label.SetText(f.field.Label)
-	f.entry.SetText(f.field.Value.(string))
-	if f.field.Description != "" {
-		f.description.SetText(f.field.Description)
+	if text, ok := field.fieldValue().(string); ok {
+		field.entry.SetText(text)
 	}
-}
 
-func (f *roomConfigFormFieldText) fieldWidget() gtki.Widget {
-	return f.widget
+	return field
 }
-
-func (f *roomConfigFormFieldText) fieldName() string {
-	return f.field.Name
-}
-
-func (f *roomConfigFormFieldText) fieldLabel() string {
-	return f.field.Label
-}
-
-func (f *roomConfigFormFieldText) fieldValue() interface{} {
-	return getEntryText(f.entry)
-}
-
-func (f *roomConfigFormFieldText) refreshContent() {}

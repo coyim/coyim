617d57f2f3bfc9740c3e3988e721380bd22247be
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-05-03 16:15:12 -0500

    #639 muc-room-config-assistant - Adds a basic implementation that allows us to add dividers per each item in the navigation and updates the logic to work well with this change

--- gui/muc_room_config_assistant_navigation.go
+++ gui/muc_room_config_assistant_navigation.go
@@ -22,6 +22,7 @@ func (rc *roomConfigAssistant) newRoomConfigAssistantNavigation() *roomConfigAss
 
 	rcn.initBuilder()
 	rcn.initNavigationItems()
+	rcn.initNavigationDividers()
 
 	return rcn
 }
@@ -31,32 +32,60 @@ func (rcn *roomConfigAssistantNavigation) initBuilder() {
 	panicOnDevError(b.bindObjects(rcn))
 
 	b.ConnectSignals(map[string]interface{}{
-		"row_selected": rcn.onRowSelected,
+		"on_row_selected": rcn.onRowSelected,
 	})
 }
 
 func (rcn *roomConfigAssistantNavigation) initNavigationItems() {
 	for _, p := range rcn.assistant.allPages() {
-		itm := newRoomConfigAssistantNavigationItem(p.pageTitle())
+		itm := rcn.newRoomConfigAssistantNavigationItem(p.pageTitle())
 		rcn.items = append(rcn.items, itm)
-		rcn.navigation.Add(itm.boxRow)
+		rcn.navigation.Add(itm.row)
+		rcn.navigation.Add(itm.divider)
+	}
+}
+
+func (rcn *roomConfigAssistantNavigation) initNavigationDividers() {
+	for idx := range rcn.dividerIndexes() {
+		divider := rcn.navigation.GetRowAtIndex(idx)
+		if divider != nil {
+			divider.SetProperty("activatable", false)
+			divider.SetProperty("selectable", false)
+		}
 	}
 }
 
 // onRowSelected MUST be called from the UI thread
 func (rcn *roomConfigAssistantNavigation) onRowSelected(_ gtki.ListBox, r gtki.ListBoxRow) {
-	rcn.assistant.updateAssistantPage(r.GetIndex())
+	// Every time a row is selected, we check if it's not a divider or a normal item.
+	// Normal items will always be in even positions, and rows dividers will be in odd positions.
+	if idx := getListBoxRowIndex(r); idx%2 == 0 {
+		rcn.assistant.updateAssistantPage(idx - (idx / 2))
+	}
 }
 
-// selectOptionByIndex MUST be called from the UI thread
-func (rcn *roomConfigAssistantNavigation) selectOptionByIndex(idx int) {
-	row := rcn.navigation.GetRowAtIndex(idx)
+// selectPageByIndex MUST be called from the UI thread
+func (rcn *roomConfigAssistantNavigation) selectPageByIndex(idx int) {
+	// Since we have a divider for each navigation item, every time we want to select
+	// a page by its index, we should calculate the right index to avoid a wrong behavior
+	row := rcn.navigation.GetRowAtIndex(idx * 2)
 	rowIndex := getListBoxRowIndex(row)
 	currentRowIndex := getListBoxRowIndex(rcn.navigation.GetSelectedRow())
 
 	if rowIndex != noRowIndex && rowIndex != currentRowIndex {
 		rcn.navigation.SelectRow(row)
 	}
+}
+
+func (rcn *roomConfigAssistantNavigation) dividerIndexes() map[int]bool {
+	indexes := map[int]bool{}
+
+	// Each row has a divider near to it. We want the divider and not the row itself.
+	for idx := 1; idx < len(rcn.assistant.allPages())*2; idx += 2 {
+		indexes[idx] = true
+	}
+
+	return indexes
 }
 
 func getListBoxRowIndex(r gtki.ListBoxRow) int {

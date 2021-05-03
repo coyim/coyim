cafc27f519fda09a4ef32b3acc4296e0d843b3ac
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-05-03 16:13:36 -0500

    #639 muc-room-config-assistant - Adds navigation item implementation as member of room config assistant navigation and updates the gtk bindings to member of the struct.

--- gui/muc_room_config_assistant_navigation_item.go
+++ gui/muc_room_config_assistant_navigation_item.go
@@ -3,11 +3,12 @@ package gui
 import "github.com/coyim/gotk3adapter/gtki"
 
 type roomConfigAssistantNavigationItem struct {
-	label  gtki.Label      `gtk-widget:"room-config-assistant-navigation-item-label"`
-	boxRow gtki.ListBoxRow `gtk-widget:"room-config-assistant-navigation-item-row"`
+	label   gtki.Label      `gtk-widget:"room-config-assistant-navigation-item-label"`
+	row     gtki.ListBoxRow `gtk-widget:"room-config-assistant-navigation-item-row"`
+	divider gtki.Separator  `gtk-widget:"room-config-assistant-navigation-item-separator"`
 }
 
-func newRoomConfigAssistantNavigationItem(lbl string) *roomConfigAssistantNavigationItem {
+func (rcn *roomConfigAssistantNavigation) newRoomConfigAssistantNavigationItem(lbl string) *roomConfigAssistantNavigationItem {
 	itm := &roomConfigAssistantNavigationItem{}
 
 	b := newBuilder("MUCRoomConfigAssistantNavigationItem")

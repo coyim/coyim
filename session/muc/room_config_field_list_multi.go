401776d1e4719d35a0018f235ab3870fe9dce7b4
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-05-06 08:36:34 -0500

    [cp] #639 - Removes the old implementation for list multi field

--- session/muc/room_config_field_list_multi.go
+++ session/muc/room_config_field_list_multi.go
@@ -1,32 +1,5 @@
 package muc
 
-// ConfigListMultiField represents a "list-multi" configuration form field
-type ConfigListMultiField interface {
-	// UpdateField will update the field with the given "values" and "options"
-	UpdateField(values, options []string)
-	Values() []string
-}
-
-type configListMultiField struct {
-	values  []string
-	options []string
-}
-
-func newConfigListMultiField(o []string) ConfigListMultiField {
-	return &configListMultiField{
-		options: o,
-	}
-}
-
-func (cf *configListMultiField) UpdateField(v, o []string) {
-	cf.values = v
-	cf.options = o
-}
-
-func (cf *configListMultiField) Values() []string {
-	return cf.values
-}
-
 // RoomConfigFieldListMultiValue contains information of the value of the text field
 type RoomConfigFieldListMultiValue struct {
 	value []string

89c35cf3405d137e875a7f98b0888f6c820ffff2
cnaranjo <crispool@hotmail.com>
2021-05-17 13:08:19 -0500

    #639 - Removes old functionality related with password-component

--- gui/muc_room_config_page_access.go
+++ gui/muc_room_config_page_access.go
@@ -6,7 +6,6 @@ import (
 
 type roomConfigAccessPage struct {
 	*roomConfigPageBase
-	roomPassword *passwordConfirmationComponent
 
 	roomPasswordBox  gtki.Box    `gtk-widget:"room-password-box"`
 	roomMembersOnly  gtki.Switch `gtk-widget:"room-membersonly"`
@@ -17,26 +16,17 @@ func (c *mucRoomConfigComponent) newRoomConfigAccessPage() mucRoomConfigPage {
 	p := &roomConfigAccessPage{}
 	p.roomConfigPageBase = c.newConfigPage(pageConfigAccess, "MUCRoomConfigPageAccess", p, nil)
 
-	p.initPasswordComponent()
 	p.initDefaultValues()
 
 	return p
 }
 
-func (p *roomConfigAccessPage) initPasswordComponent() {
-	p.roomPassword = p.u.createPasswordConfirmationComponent()
-	p.roomPasswordBox.Add(p.roomPassword.contentBox())
-	p.doAfterRefresh.add(p.roomPassword.onShowConfirmPasswordBasedOnMatchError)
-}
-
 func (p *roomConfigAccessPage) initDefaultValues() {
-	p.roomPassword.setPassword(p.form.Password)
 	setSwitchActive(p.roomMembersOnly, p.form.MembersOnly)
 	setSwitchActive(p.roomAllowInvites, p.form.OccupantsCanInvite)
 }
 
 func (p *roomConfigAccessPage) collectData() {
-	p.form.Password = p.roomPassword.currentPassword()
 	p.form.PasswordProtected = p.form.Password != ""
 	p.form.MembersOnly = getSwitchActive(p.roomMembersOnly)
 	p.form.OccupantsCanInvite = getSwitchActive(p.roomAllowInvites)
@@ -44,10 +34,4 @@ func (p *roomConfigAccessPage) collectData() {
 
 func (p *roomConfigAccessPage) isInvalid() bool {
 	return p.roomConfigPageBase.isInvalid()
-}
-
-func (p *roomConfigAccessPage) showValidationErrors() {
-	p.roomPassword.changeConfirmPasswordEntryStyle()
-	p.roomPassword.onShowConfirmPasswordBasedOnMatchError()
-	p.roomPassword.focusConfirm()
 }

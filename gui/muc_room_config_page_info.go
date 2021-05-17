7092967c003ca2dd09a86d34c86e6d6b782f4e9d
cnaranjo <crispool@hotmail.com>
2021-05-17 11:01:24 -0500

    #639 - Removes unused fields from info-page

--- gui/muc_room_config_page_info.go
+++ gui/muc_room_config_page_info.go
@@ -7,33 +7,10 @@ import (
 type roomConfigInfoPage struct {
 	*roomConfigPageBase
 	roomDescriptionBuffer gtki.TextBuffer
-
-	roomDescription            gtki.TextView `gtk-widget:"room-description"`
-	roomLanguageFieldContainer gtki.Box      `gtk-widget:"room-config-language-field"`
-	roomPersistent             gtki.Switch   `gtk-widget:"room-persistent"`
-	roomPublic                 gtki.Switch   `gtk-widget:"room-public"`
 }
 
 func (c *mucRoomConfigComponent) newRoomConfigInfoPage() mucRoomConfigPage {
 	p := &roomConfigInfoPage{}
 	p.roomConfigPageBase = c.newConfigPage(pageConfigInfo, "MUCRoomConfigPageInfo", p, nil)
-
-	p.roomDescriptionBuffer, _ = g.gtk.TextBufferNew(nil)
-	p.roomDescription.SetBuffer(p.roomDescriptionBuffer)
-
-	p.initDefaultValues()
-
 	return p
-}
-
-func (p *roomConfigInfoPage) initDefaultValues() {
-	setTextViewText(p.roomDescription, p.form.Description)
-	setSwitchActive(p.roomPersistent, p.form.Persistent)
-	setSwitchActive(p.roomPublic, p.form.Public)
-}
-
-func (p *roomConfigInfoPage) collectData() {
-	p.form.Description = getTextViewText(p.roomDescription)
-	p.form.Persistent = getSwitchActive(p.roomPersistent)
-	p.form.Public = getSwitchActive(p.roomPublic)
 }

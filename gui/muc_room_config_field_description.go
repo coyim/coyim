8d228b4b22f6a04204464a354efccb5cb8ffbc5f
Sandy <sandy.acurio@gmail.com>
2021-05-27 10:57:05 -0500

    [cs] #639 - Adds `roomConfigFieldDescription` in order to handle description field in the UI

---
+++ gui/muc_room_config_field_description.go
@@ -0,0 +1,32 @@
+package gui
+
+import (
+	"github.com/coyim/coyim/session/muc"
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+type roomConfigFieldDescription struct {
+	*roomConfigFormField
+	value *muc.RoomConfigFieldTextValue
+
+	textView gtki.TextView `gtk-widget:"room-config-text-multi-field-textview"`
+}
+
+func newRoomConfigFieldDescription(fieldInfo roomConfigFieldTextInfo, value *muc.RoomConfigFieldTextValue) hasRoomConfigFormField {
+	field := &roomConfigFieldDescription{value: value}
+	field.roomConfigFormField = newRoomConfigFormField(fieldInfo, "MUCRoomConfigFormFieldTextMulti")
+
+	panicOnDevError(field.builder.bindObjects(field))
+
+	tb, _ := g.gtk.TextBufferNew(nil)
+	field.textView.SetBuffer(tb)
+
+	tb.SetText(value.Text())
+
+	return field
+}
+
+// collectFieldValue MUST be called from the UI thread
+func (f *roomConfigFieldDescription) collectFieldValue() {
+	f.value.SetText(getTextViewText(f.textView))
+}

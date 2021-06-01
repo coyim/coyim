9bedbfc05e571b6de94c5a1595a9ba105c38c41b
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-06-01 09:31:26 -0500

    [ps] - Adds a helper to create temporary files during tests and that uses the testing framework, meaning that those files will be removed automatically when tests finishes

--- config/importer/helpers_for_test.go
+++ config/importer/helpers_for_test.go
@@ -1,11 +1,20 @@
 package importer
 
 import (
+	"io/ioutil"
+	"os"
 	"path"
 	"runtime"
+
+	. "gopkg.in/check.v1"
 )
 
 func testResourceFilename(s string) string {
 	_, filename, _, _ := runtime.Caller(0)
 	return path.Join(path.Dir(filename), s)
+}
+
+func tempFile(c *C) *os.File {
+	tmpfile, _ := ioutil.TempFile(c.MkDir(), "")
+	return tmpfile
 }

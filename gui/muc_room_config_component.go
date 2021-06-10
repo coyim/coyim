63eeb91d294bea13f07293bc1e7b067d1244d3e7
ivanjijon <ivanjijon@autonomia.digital>
2021-06-10 16:50:46 -0500

    [ci] #639 - Destroy the room when user cancels its reservation

--- gui/muc_room_config_component.go
+++ gui/muc_room_config_component.go
@@ -71,10 +71,13 @@ func (c *mucRoomConfigComponent) updateAutoJoin(v bool) {
 
 // cancelConfiguration IS SAFE to be called from the UI thread
 func (c *mucRoomConfigComponent) cancelConfiguration(onError func(error)) {
-	ec := c.account.session.CancelRoomConfiguration(c.roomID)
+	sc, ec := c.account.session.DestroyRoom(c.roomID, "", nil, "")
 
 	go func() {
-		if err := <-ec; err != nil {
+		select {
+		case <-sc:
+			// do nothing
+		case err := <-ec:
 			c.log.WithError(err).Error("An error occurred when trying to cancel the room configuration")
 			if onError != nil {
 				doInUIThread(func() {

b6319eb4d3f50588f3e4dc7338fb77a07af2fd02
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-06-21 14:47:54 -0500

    #745 - Updates padding style

--- gui/muc_styles_infobar.go
+++ gui/muc_styles_infobar.go
@@ -85,7 +85,7 @@ var (
 	infoBarCommonStyle = style{
 		"text-shadow":   "none",
 		"font-weight":   "500",
-		"padding":       "0 4px 0 0",
+		"padding":       "4px 4px 4px 0",
 		"border-radius": "0",
 	}
 

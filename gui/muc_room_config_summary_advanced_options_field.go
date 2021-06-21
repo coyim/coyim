c972a06c99d778b231497290b873d46ec61a87da
cnaranjo <crispool@hotmail.com>
2021-06-21 11:46:33 -0500

    [cp] #639 - Added component in order to handle advanced options as a field inside of summary fields

---
+++ gui/muc_room_config_summary_advanced_options_field.go
@@ -0,0 +1,73 @@
+package gui
+
+import (
+	"github.com/coyim/gotk3adapter/gtki"
+)
+
+type roomConfigSummaryAdvancedOptionsField struct {
+	advancedOptions []hasRoomConfigFormField
+
+	widget                    gtki.Box     `gtk-widget:"room-config-field-box"`
+	fieldListValueButtonImage gtki.Image   `gtk-widget:"room-config-field-list-button-image"`
+	advancedOptionsContent    gtki.Box     `gtk-widget:"room-config-field-advanced-options-box"`
+	advancedOptionsList       gtki.ListBox `gtk-widget:"room-config-field-advanced-options-content-box"`
+}
+
+func newAdvancedOptionSummaryField(advancedOptions []hasRoomConfigFormField) hasRoomConfigFormField {
+	field := &roomConfigSummaryAdvancedOptionsField{
+		advancedOptions: advancedOptions,
+	}
+
+	field.initBuilder()
+	field.initDefaults()
+
+	return field
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) initBuilder() {
+	builder := newBuilder("MUCRoomConfigSummaryAdvancedOptionsField")
+	panicOnDevError(builder.bindObjects(f))
+	builder.ConnectSignals(map[string]interface{}{
+		"on_show_list": f.showAdvancedOptions,
+	})
+}
+
+func (fc *roomConfigSummaryAdvancedOptionsField) fieldWidgets() (widgets []gtki.Widget) {
+	for _, f := range fc.advancedOptions {
+		widgets = append(widgets, f.fieldWidget())
+	}
+	return
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) initDefaults() {
+	fields := addSeparatorsBetweenWidgets(f.fieldWidgets())
+	for _, ff := range fields {
+		f.advancedOptionsList.Add(ff)
+	}
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) showAdvancedOptions() {
+	if f.advancedOptionsContent.IsVisible() {
+		f.fieldListValueButtonImage.SetFromIconName("pan-down-symbolic", gtki.ICON_SIZE_MENU)
+		f.advancedOptionsContent.Hide()
+	} else {
+		f.fieldListValueButtonImage.SetFromIconName("pan-up-symbolic", gtki.ICON_SIZE_MENU)
+		f.advancedOptionsContent.Show()
+	}
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) fieldWidget() gtki.Widget {
+	return f.widget
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) refreshContent() {
+	f.advancedOptionsContent.Hide()
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) updateFieldValue() {}
+
+func (f *roomConfigSummaryAdvancedOptionsField) isValid() bool {
+	return true
+}
+
+func (f *roomConfigSummaryAdvancedOptionsField) showValidationErrors() {}

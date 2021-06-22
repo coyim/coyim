a6b0df07e8dd5224e74a7ce8d26b341a8e4f060c
Ola Bini <ola@olabini.se>
2021-06-22 15:43:01 +0000

    Add basic testing for the session filetransfer packaging functionality

---
+++ session/filetransfer/packaging_test.go
@@ -0,0 +1,84 @@
+package filetransfer
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "gopkg.in/check.v1"
+)
+
+type PackagingSuite struct{}
+
+var _ = Suite(&PackagingSuite{})
+
+func createTemporaryDirectoryStructure(intoDir string) {
+	_ = os.MkdirAll(filepath.Join(intoDir, "foo", "bar", "quux"), 0755)
+	_ = os.MkdirAll(filepath.Join(intoDir, "foo", "somewhere"), 0755)
+	_ = os.MkdirAll(filepath.Join(intoDir, "another place"), 0755)
+
+	_ = ioutil.WriteFile(filepath.Join(intoDir, "a file"), []byte("some content"), 0755)
+	_ = ioutil.WriteFile(filepath.Join(intoDir, "another file"), []byte("some more content"), 0755)
+	_ = ioutil.WriteFile(filepath.Join(intoDir, "foo", "wut"), []byte("even more content"), 0755)
+	_ = ioutil.WriteFile(filepath.Join(intoDir, "another place", "oh well"), []byte("final content"), 0755)
+}
+
+func (s *PackagingSuite) Test_pack_and_unpack(c *C) {
+	dd := c.MkDir()
+	createTemporaryDirectoryStructure(dd)
+
+	f, _ := ioutil.TempFile("", "")
+	defer os.Remove(f.Name())
+
+	e := pack(dd, f)
+	_ = f.Close()
+
+	c.Assert(e, IsNil)
+
+	dd2 := c.MkDir()
+	e2 := unpack(f.Name(), dd2)
+	c.Assert(e2, IsNil)
+
+	actualDir := filepath.Join(dd2, filepath.Base(dd))
+
+	st, ee := os.Stat(filepath.Join(actualDir, "foo"))
+	c.Assert(ee, IsNil)
+	c.Assert(st, Not(IsNil))
+	c.Assert(st.IsDir(), Equals, true)
+
+	st, ee = os.Stat(filepath.Join(actualDir, "foo", "bar"))
+	c.Assert(ee, IsNil)
+	c.Assert(st, Not(IsNil))
+	c.Assert(st.IsDir(), Equals, true)
+
+	st, ee = os.Stat(filepath.Join(actualDir, "foo", "bar", "quux"))
+	c.Assert(ee, IsNil)
+	c.Assert(st, Not(IsNil))
+	c.Assert(st.IsDir(), Equals, true)
+
+	st, ee = os.Stat(filepath.Join(actualDir, "foo", "somewhere"))
+	c.Assert(ee, IsNil)
+	c.Assert(st, Not(IsNil))
+	c.Assert(st.IsDir(), Equals, true)
+
+	st, ee = os.Stat(filepath.Join(actualDir, "another place"))
+	c.Assert(ee, IsNil)
+	c.Assert(st, Not(IsNil))
+	c.Assert(st.IsDir(), Equals, true)
+
+	ct, ee2 := ioutil.ReadFile(filepath.Join(actualDir, "a file"))
+	c.Assert(ee2, IsNil)
+	c.Assert(string(ct), Equals, "some content")
+
+	ct, ee2 = ioutil.ReadFile(filepath.Join(actualDir, "another file"))
+	c.Assert(ee2, IsNil)
+	c.Assert(string(ct), Equals, "some more content")
+
+	ct, ee2 = ioutil.ReadFile(filepath.Join(actualDir, "foo", "wut"))
+	c.Assert(ee2, IsNil)
+	c.Assert(string(ct), Equals, "even more content")
+
+	ct, ee2 = ioutil.ReadFile(filepath.Join(actualDir, "another place", "oh well"))
+	c.Assert(ee2, IsNil)
+	c.Assert(string(ct), Equals, "final content")
+}

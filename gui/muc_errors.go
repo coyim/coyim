591e63d19135a2f6dfb3098e28e3f2fa1f677b47
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-06-29 15:58:37 -0500

    [ip] #640 muc-errors - Updates `handleMUCErrorEvent` to avoid loging errors when no view is available

--- gui/muc_errors.go
+++ gui/muc_errors.go
@@ -5,35 +5,31 @@ import (
 )
 
 func (a *account) handleMUCErrorEvent(ev events.MUCError) {
-	view, ok := a.getRoomView(ev.Room)
-	if !ok {
-		a.log.WithField("room", ev.Room).Error("Not possible to get room view when handling multi user chat event")
-		return
-	}
-
-	switch ev.ErrorType {
-	case events.MUCNotAuthorized:
-		view.notAuthorized()
-	case events.MUCForbidden:
-		view.occupantForbidden()
-	case events.MUCMessageForbidden:
-		view.messageForbidden()
-	case events.MUCItemNotFound:
-		a.log.Debug("MUC Error MUCItemNotFound received")
-	case events.MUCNotAllowed:
-		a.log.Debug("MUC Error MUCNotAllowed received")
-	case events.MUCNotAcceptable:
-		a.log.Debug("MUC Error MUCNotAcceptable received")
-	case events.MUCMessageNotAcceptable:
-		view.messageNotAccepted()
-	case events.MUCRegistrationRequired:
-		view.registrationRequired(ev.Nickname)
-	case events.MUCConflict:
-		view.nicknameConflict(ev.Nickname)
-	case events.MUCServiceUnavailable:
-		view.serviceUnavailable()
-	default:
-		a.log.WithField("event", ev).Warn("unsupported muc error event")
-		view.unknownError()
+	if view, ok := a.getRoomView(ev.Room); ok {
+		switch ev.ErrorType {
+		case events.MUCNotAuthorized:
+			view.notAuthorized()
+		case events.MUCForbidden:
+			view.occupantForbidden()
+		case events.MUCMessageForbidden:
+			view.messageForbidden()
+		case events.MUCItemNotFound:
+			a.log.Debug("MUC Error MUCItemNotFound received")
+		case events.MUCNotAllowed:
+			a.log.Debug("MUC Error MUCNotAllowed received")
+		case events.MUCNotAcceptable:
+			a.log.Debug("MUC Error MUCNotAcceptable received")
+		case events.MUCMessageNotAcceptable:
+			view.messageNotAccepted()
+		case events.MUCRegistrationRequired:
+			view.registrationRequired(ev.Nickname)
+		case events.MUCConflict:
+			view.nicknameConflict(ev.Nickname)
+		case events.MUCServiceUnavailable:
+			view.serviceUnavailable()
+		default:
+			a.log.WithField("event", ev).Warn("Unsupported MUC error event")
+			view.unknownError()
+		}
 	}
 }

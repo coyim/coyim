f7b6782afef5b0ee2e00f595e29272236d01428f
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-07-13 17:01:55 -0500

    [ip] #704 - Adds more specific highlight styles attributes

--- gui/infobar_highlight_component.go
+++ gui/infobar_highlight_component.go
@@ -9,11 +9,15 @@ import (
 type infoBarHighlightType int
 
 const (
-	infoBarHighlightFontWeight infoBarHighlightType = iota
+	infoBarHighlightNickname infoBarHighlightType = iota
+	infoBarHighlightAffiliation
+	infoBarHighlightRole
 )
 
 type infoBarHighlightAttributes struct {
-	labelWithFontWeight gtki.Label `gtk-widget:"labelWithFontWeight"`
+	labelNickname    gtki.Label `gtk-widget:"labelNickname"`
+	labelAffiliation gtki.Label `gtk-widget:"labelAffiliation"`
+	labelRole        gtki.Label `gtk-widget:"labelRole"`
 }
 
 func newInfoBarHighlightAttributes(tp infoBarHighlightType) *gotk3extra.PangoAttrList {
@@ -24,8 +28,12 @@ func newInfoBarHighlightAttributes(tp infoBarHighlightType) *gotk3extra.PangoAtt
 
 	var highlightLabel gtki.Label
 	switch tp {
-	case infoBarHighlightFontWeight:
-		highlightLabel = ibh.labelWithFontWeight
+	case infoBarHighlightNickname:
+		highlightLabel = ibh.labelNickname
+	case infoBarHighlightAffiliation:
+		highlightLabel = ibh.labelAffiliation
+	case infoBarHighlightRole:
+		highlightLabel = ibh.labelRole
 	}
 
 	if highlightLabel != nil {

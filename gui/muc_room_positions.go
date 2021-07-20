896ca756dc8bdb5d97fb4d9d0c3d78af833b4beb
cnaranjo <crispool@hotmail.com>
2021-07-20 17:35:41 -0500

    #571 - Creates `roomPositions` structure in order to handle all information related with different affiliation lists

--- gui/muc_room_positions.go
+++ gui/muc_room_positions.go
@@ -1,9 +1,65 @@
 package gui
 
 import (
+	"sync"
+
 	"github.com/coyim/coyim/coylog"
+	"github.com/coyim/coyim/session/muc"
 	"github.com/coyim/gotk3adapter/gtki"
 )
+
+type roomPositions struct {
+	owners muc.RoomOccupantItemList
+	admins muc.RoomOccupantItemList
+	banned muc.RoomOccupantItemList
+	none   muc.RoomOccupantItemList
+
+	sync.Mutex
+}
+
+func newRoomPositions() *roomPositions {
+	return &roomPositions{}
+}
+
+func (rp *roomPositions) ownersList() muc.RoomOccupantItemList {
+	return rp.owners
+}
+
+func (rp *roomPositions) setOwnerList(owners muc.RoomOccupantItemList) {
+	rp.Lock()
+	defer rp.Unlock()
+
+	rp.owners = owners
+}
+
+func (rp *roomPositions) adminsList() muc.RoomOccupantItemList {
+	return rp.admins
+}
+
+func (rp *roomPositions) setAdminList(admins muc.RoomOccupantItemList) {
+	rp.Lock()
+	defer rp.Unlock()
+
+	rp.admins = admins
+}
+
+func (rp *roomPositions) bannedList() muc.RoomOccupantItemList {
+	return rp.banned
+}
+
+func (rp *roomPositions) setBanList(banned muc.RoomOccupantItemList) {
+	rp.Lock()
+	defer rp.Unlock()
+
+	rp.banned = banned
+}
+
+func (rp *roomPositions) updateRemovedOccupantList(occupantsRemoved muc.RoomOccupantItemList) {
+	rp.Lock()
+	defer rp.Unlock()
+
+	rp.none = append(rp.none, occupantsRemoved...)
+}
 
 func (v *roomView) onRoomPositionsView() {
 	rp := v.newRoomPositionsView()
@@ -18,7 +74,8 @@ const (
 )
 
 type roomPositionsView struct {
-	roomView *roomView
+	roomView      *roomView
+	roomPositions *roomPositions
 
 	dialog  gtki.Window `gtk-widget:"positions-window"`
 	content gtki.Box    `gtk-widget:"content"`
@@ -28,8 +85,9 @@ type roomPositionsView struct {
 
 func (v *roomView) newRoomPositionsView() *roomPositionsView {
 	rp := &roomPositionsView{
-		roomView: v,
-		log:      v.log.WithField("where", "roomPositionsView"),
+		roomView:      v,
+		roomPositions: newRoomPositions(),
+		log:           v.log.WithField("where", "roomPositionsView"),
 	}
 
 	rp.initBuilder()

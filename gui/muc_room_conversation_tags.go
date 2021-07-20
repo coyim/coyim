e4ffab571a49ec74304f6df76281ce160f9ddc19
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-07-20 12:40:33 -0500

    #704 muc-room-conversation-tags - Now that each tag is defined in the registry, we don't need all these functions.

--- gui/muc_room_conversation_tags.go
+++ gui/muc_room_conversation_tags.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/coyim/gotk3adapter/gtki"
-	"github.com/coyim/gotk3adapter/pangoi"
 )
 
 type conversationTag string
@@ -81,100 +80,6 @@ func (c *roomViewConversation) createConversationTag(name conversationTag, prope
 		_ = tag.SetProperty(attribute, value)
 	}
 	return tag
-}
-
-func (c *roomViewConversation) createWarningTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagWarning, pangoAttributes{
-		"foreground": cs.warningForeground,
-	})
-}
-
-func (c *roomViewConversation) createInfoMessageTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagInfo, pangoAttributes{
-		"foreground": cs.infoMessageForeground,
-		"style":      pangoi.STYLE_ITALIC,
-	})
-}
-
-func (c *roomViewConversation) createLeftRoomTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagSomeoneLeftRoom, pangoAttributes{
-		"foreground": cs.someoneLeftForeground,
-		"style":      pangoi.STYLE_ITALIC,
-	})
-}
-
-func (c *roomViewConversation) createJoinedRoomTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagSomeoneJoinedRoom, pangoAttributes{
-		"foreground": cs.someoneJoinedForeground,
-		"style":      pangoi.STYLE_ITALIC,
-	})
-}
-
-func (c *roomViewConversation) createTimestampTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagTimestamp, pangoAttributes{
-		"foreground": cs.timestampForeground,
-		"style":      pangoi.STYLE_NORMAL,
-	})
-}
-
-func (c *roomViewConversation) createNicknameTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagNickname, pangoAttributes{
-		"foreground": cs.nicknameForeground,
-		"style":      pangoi.STYLE_NORMAL,
-		"weight":     pangoi.WEIGHT_BOLD,
-	})
-}
-
-func (c *roomViewConversation) createSubjectTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagRoomSubject, pangoAttributes{
-		"foreground": cs.subjectForeground,
-		"style":      pangoi.STYLE_ITALIC,
-	})
-}
-
-func (c *roomViewConversation) createMessageTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagMessage, pangoAttributes{
-		"foreground": cs.messageForeground,
-		"style":      pangoi.STYLE_NORMAL,
-	})
-}
-
-func (c *roomViewConversation) createGroupDateTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagDateGroup, pangoAttributes{
-		"justification":      pangoi.JUSTIFY_CENTER,
-		"pixels-above-lines": 12,
-		"pixels-below-lines": 12,
-		"foreground":         cs.infoMessageForeground,
-		"style":              pangoi.STYLE_ITALIC,
-	})
-}
-
-func (c *roomViewConversation) createDividerTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagDivider, pangoAttributes{
-		"justification":      pangoi.JUSTIFY_CENTER,
-		"pixels-above-lines": 12,
-		"pixels-below-lines": 12,
-	})
-}
-
-func (c *roomViewConversation) createErrorTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagError, pangoAttributes{
-		"foreground": cs.errorForeground,
-	})
-}
-
-func (c *roomViewConversation) createConfigurationChangeTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagRoomConfigChange, pangoAttributes{
-		"foreground": cs.configurationForeground,
-		"style":      pangoi.STYLE_ITALIC,
-	})
-}
-
-func (c *roomViewConversation) createPasswordTag(cs mucColorSet) gtki.TextTag {
-	return c.createConversationTag(conversationTagPassword, pangoAttributes{
-		"foreground": cs.warningForeground,
-		"background": cs.warningBackground,
-	})
 }
 
 var uniqueHighlightFormatName = func(format string) conversationTag {

4c2bb35c1d2cb1c1c033abe87f4fd5783f0e4508
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-07-23 09:43:25 -0500

    #571 muc-room-positions - Updates notification texts shown when updating room positions
    Related to https://github.com/coyim/coyim/commit/796536e833ee4145fd1ca090d3b381974a0ac27d#r53883329

--- gui/muc_room_positions.go
+++ gui/muc_room_positions.go
@@ -140,7 +140,7 @@ func (rp *roomPositionsView) onApply() {
 		case <-rc:
 			doInUIThread(func() {
 				rp.roomView.notifications.info(roomNotificationOptions{
-					message:   i18n.Local("The positions list has been updated."),
+					message:   i18n.Local("The room positions were updated."),
 					closeable: true,
 				})
 				rp.roomView.loadingViewOverlay.hide()
@@ -148,7 +148,7 @@ func (rp *roomPositionsView) onApply() {
 		case <-ec:
 			doInUIThread(func() {
 				rp.roomView.notifications.error(roomNotificationOptions{
-					message:   i18n.Local("The positions list couldn't be updated."),
+					message:   i18n.Local("The room positions could not be updated."),
 					closeable: true,
 				})
 				rp.roomView.loadingViewOverlay.hide()

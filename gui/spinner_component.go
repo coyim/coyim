6b6eb9988b1f16d9f0f84a78fc3abaf8fb8ff3a8
ivanjijon <ivanjijon@autonomia.digital>
2021-08-30 13:40:31 -0500

    [ip] #761 - updates spinner definition to support different sizes

--- gui/spinner_component.go
+++ gui/spinner_component.go
@@ -2,17 +2,34 @@ package gui
 
 import "github.com/coyim/gotk3adapter/gtki"
 
+type spinnerSize int
+
+const (
+	spinnerSizeNormal spinnerSize = 16
+	spinnerSizeMedium spinnerSize = 24
+
+	spinnerSizeDefault = spinnerSizeNormal
+)
+
 type spinner struct {
 	s gtki.Spinner
 }
 
 func (u *gtkUI) newSpinnerComponent() *spinner {
 	s, _ := g.gtk.SpinnerNew()
-	return &spinner{s}
+	sp := &spinner{s}
+	sp.setSize(spinnerSizeDefault)
+	return sp
 }
 
 func (sp *spinner) spinner() gtki.Spinner {
 	return sp.s
+}
+
+// setSize MUST be called from the UI thread
+func (sp *spinner) setSize(s spinnerSize) {
+	sp.s.SetProperty("width_request", int(s))
+	sp.s.SetProperty("height_request", int(s))
 }
 
 // show MUST be called from the ui thread

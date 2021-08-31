86eb688542658717a7e7d868d43e51955bbf05d9
Sandy <sandy.acurio@gmail.com>
2021-08-31 11:21:51 -0500

    [ps] #761 - Updates  the `GetRoom` documentation.

--- session/muc.go
+++ session/muc.go
@@ -41,7 +41,7 @@ func newMUCManager(log coylog.Logger, conn func() xi.Conn, publishEvent func(ev 
 	return m
 }
 
-// GetRoom will look up the room with the given ID in the room manager and returns it
+// GetRoom will return from the room manager the room with the given ID
 func (s *session) GetRoom(roomID jid.Bare) (*muc.Room, bool) {
 	return s.muc.getRoom(roomID)
 }

93d8f8c22055d4e23baf9f9c876bb183cf50952c
Pedro Enrique Palau <pepalauisaac@gmail.com>
2021-09-03 13:51:41 -0500

    [ps] #571 - Deletes the `muc_room_config_field_positions_tmp`. The code is now more simple and easy to understand.
    Related to https://github.com/coyim/coyim/commit/f6784a18854ce8be1315d61df033f1d259dbd1d9#commitcomment-55845265

--- gui/muc_room_config_page.go
+++ gui/muc_room_config_page.go
@@ -284,7 +284,7 @@ func (p *roomConfigPage) initOccupantsSummaryFields() {
 }
 
 func (p *roomConfigPage) initOccupants(parent gtki.Window) {
-	p.addField(newRoomConfigPositionsField(roomConfigPositionsOptions{
+	p.addField(newRoomConfigFieldPositions(roomConfigPositionsOptions{
 		affiliation:            ownerAffiliation,
 		occupantList:           p.form.OwnersList(),
 		setOccupantList:        p.form.SetOwnerList,
@@ -294,7 +294,7 @@ func (p *roomConfigPage) initOccupants(parent gtki.Window) {
 	}))
 	p.content.Add(createSeparator(gtki.HorizontalOrientation))
 
-	p.addField(newRoomConfigPositionsField(roomConfigPositionsOptions{
+	p.addField(newRoomConfigFieldPositions(roomConfigPositionsOptions{
 		affiliation:            adminAffiliation,
 		occupantList:           p.form.AdminsList(),
 		setOccupantList:        p.form.SetAdminList,
@@ -303,7 +303,7 @@ func (p *roomConfigPage) initOccupants(parent gtki.Window) {
 	}))
 	p.content.Add(createSeparator(gtki.HorizontalOrientation))
 
-	p.addField(newRoomConfigPositionsField(roomConfigPositionsOptions{
+	p.addField(newRoomConfigFieldPositions(roomConfigPositionsOptions{
 		affiliation:            outcastAffiliation,
 		occupantList:           p.form.BanList(),
 		setOccupantList:        p.form.SetBanList,

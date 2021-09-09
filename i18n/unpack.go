78cca1aecfe6d701b97ba1444398ebb0b27b4b2f
Ola Bini <ola@olabini.se>
2021-09-09 17:15:37 +0000

    Update translations, and add initial Norwegian Bokmål translation, contributed by Allan Nordhøy

--- i18n/unpack.go
+++ i18n/unpack.go
@@ -1,4 +1,4 @@
-//go:generate ../.build-tools/esc -o translations.go -modtime 1489449600 -pkg i18n -private ar en es pt sv zh
+//go:generate ../.build-tools/esc -o translations.go -modtime 1489449600 -pkg i18n -private ar en es pt sv zh nb_NO
 
 package i18n
 

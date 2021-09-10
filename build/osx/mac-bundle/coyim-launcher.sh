676280fac9b3b7ae9ecceda7cb638aaf4fe3bc49
Ola Bini <ola@olabini.se>
2021-09-10 16:11:41 +0000

    Make coyim launcher on OS X take a possible command line argument

--- build/osx/mac-bundle/coyim-launcher.sh
+++ build/osx/mac-bundle/coyim-launcher.sh
@@ -83,7 +83,7 @@ if test "$APPLELANGUAGES"; then
     #  export LANG=$L
     #  break
     #fi;
-  done  
+  done
 fi
 unset APPLELANGUAGES L
 
@@ -104,7 +104,7 @@ unset APPLECOLLATION
 APPLELOCALE=`defaults read .GlobalPreferences AppleLocale`
 
 if test -f "$I18NDIR/${APPLELOCALE:0:5}/LC_MESSAGES/$APP.mo"; then
-  if test -z $LANG; then 
+  if test -z $LANG; then
     export LANG="${APPLELOCALE:0:5}"
   fi
 
@@ -116,7 +116,7 @@ fi
 #5-character locale to avoid the "Locale not supported by C library"
 #warning from Gtk -- even though Gtk will translate with a
 #two-character code.
-if test -n "$LANG"; then 
+if test -n "$LANG"; then
   #If the language code matches the applelocale, then that's the message
   #locale; otherwise, if it's longer than two characters, then it's
   #probably a good message locale and we'll go with it.
@@ -133,7 +133,7 @@ if test -n "$LANG"; then
     export LC_MESSAGES="en_US"
   else
     LOC=`find $PREFIX/share/locale -name $LANG???`
-    for L in $LOC; do 
+    for L in $LOC; do
       export LC_MESSAGES=$L
     done
   fi
@@ -144,16 +144,16 @@ else
 fi
 
 CURRENCY=`echo $APPLELOCALE |  sed -En 's/.*currency=([[:alpha:]]+).*/\1/p'`
-if test "x$CURRENCY" != "x"; then 
+if test "x$CURRENCY" != "x"; then
   #The user has set a special currency. Gtk doesn't install LC_MONETARY files, but Apple does in /usr/share/locale, so we're going to look there for a locale to set LC_CURRENCY to.
   if test -f /usr/local/share/$LC_MESSAGES/LC_MONETARY; then
     if test -a `cat /usr/local/share/$LC_MESSAGES/LC_MONETARY` == $CURRENCY; then
       export LC_MONETARY=$LC_MESSAGES
     fi
   fi
-  if test -z "$LC_MONETARY"; then 
+  if test -z "$LC_MONETARY"; then
     FILES=`find /usr/share/locale -name LC_MONETARY -exec grep -H $CURRENCY {} \;`
-    if test -n "$FILES"; then 
+    if test -n "$FILES"; then
       export LC_MONETARY=`echo $FILES | sed -En 's%/usr/share/locale/([[:alpha:]_]+)/LC_MONETARY.*%\1%p'`
     fi
   fi
@@ -184,4 +184,4 @@ fi
 
 # TODO: fixme
 cd $bundle_contents/MacOS # we need to cd to this directory for the locale to work
-$EXEC "$bundle_contents/MacOS/$name-bin" "$@" $EXTRA_ARGS $COY_DEBUG_EXTRA
+$EXEC "$bundle_contents/MacOS/$name-bin" "$@" $EXTRA_ARGS $COY_DEBUG_EXTRA $COYIM_ARGUMENTS

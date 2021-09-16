65091925498f544a1286dbbe376a426b3021a396
Ola Bini <ola@olabini.se>
2021-09-16 21:36:56 +0000

    #817 - It seems that this variable was added to fix a problem with GTK on Mac in 2015 and 2016. However, this bug has now been long fixed, and setting the variable actually causes another bug instead. So, to fix this issue, we simply do not set the variable

--- build/osx/mac-bundle/environment.sh
+++ build/osx/mac-bundle/environment.sh
@@ -2,5 +2,5 @@
 
 # See: https://bugzilla.gnome.org/show_bug.cgi?id=758338
 # export GTK_DEBUG=no-pixel-cache # requires building with debug
-export GDK_RENDERING=image
+# export GDK_RENDERING=image
 

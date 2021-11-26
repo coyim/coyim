eff53a38cd6b70c91152abb8bae19f550faf09b9
cnaranjo <crispool@hotmail.com>
2021-11-26 09:59:08 -0500

    #815 - Removes functionality that verifies if the navigation item was pair or not in order to handle the navigation

--- gui/muc_room_config_assistant_navigation.go
+++ gui/muc_room_config_assistant_navigation.go
@@ -68,18 +68,12 @@ func (rcn *roomConfigAssistantNavigation) forEachItem(fn func(*roomConfigAssista
 
 // onRowSelected MUST be called from the UI thread
 func (rcn *roomConfigAssistantNavigation) onRowSelected(_ gtki.ListBox, r gtki.ListBoxRow) {
-	// Every time a row is selected, we check if it's not a divider or a normal item.
-	// Normal items will always be in even positions, and rows dividers will be in odd positions.
-	if idx := getListBoxRowIndex(r); idx%2 == 0 {
-		rcn.assistant.updateAssistantPage(mucRoomConfigPageID(idx - (idx / 2)))
-	}
+	rcn.assistant.updateAssistantPage(mucRoomConfigPageID(getListBoxRowIndex(r)))
 }
 
 // selectPageByIndex MUST be called from the UI thread
 func (rcn *roomConfigAssistantNavigation) selectPageByIndex(pageID mucRoomConfigPageID) {
-	// Since we have a divider for each navigation item, every time we want to select
-	// a page by its index, we should calculate the right index to avoid a wrong behavior
-	row := rcn.navigation.GetRowAtIndex(int(pageID) * 2)
+	row := rcn.navigation.GetRowAtIndex(int(pageID))
 	rowIndex := getListBoxRowIndex(row)
 	currentRowIndex := getListBoxRowIndex(rcn.navigation.GetSelectedRow())
 

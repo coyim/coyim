3d422d6f11289a54d2ec0bf9866afb7d9776c0db
Fausto <fausto@autonomia.digital>
2021-12-16 18:51:42 +0000

    [cif] #833 - Define icons for dark themes in room configuration assitant items

--- gui/muc_room_config_assistant_navigation_item.go
+++ gui/muc_room_config_assistant_navigation_item.go
@@ -11,12 +11,18 @@ func (n navigationItemIconName) String() string {
 }
 
 const (
-	basicInformationIconName navigationItemIconName = "room_config_basic_information"
-	accessIconName           navigationItemIconName = "room_config_access"
-	permissionsIconName      navigationItemIconName = "room_config_permissions"
-	positionsIconName        navigationItemIconName = "room_config_positions"
-	otherIconName            navigationItemIconName = "room_config_others"
-	sumaryIconName           navigationItemIconName = "room_config_summary"
+	basicInformationIconName     navigationItemIconName = "room_config_basic_information"
+	basicInformationIconNameDark navigationItemIconName = "room_config_basic_information_dark"
+	accessIconName               navigationItemIconName = "room_config_access"
+	accessIconNameDark           navigationItemIconName = "room_config_access_dark"
+	permissionsIconName          navigationItemIconName = "room_config_permissions"
+	permissionsIconNameDark      navigationItemIconName = "room_config_permissions_dark"
+	positionsIconName            navigationItemIconName = "room_config_positions"
+	positionsIconNameDark        navigationItemIconName = "room_config_positions_dark"
+	otherIconName                navigationItemIconName = "room_config_others"
+	otherIconNameDark            navigationItemIconName = "room_config_others_dark"
+	sumaryIconName               navigationItemIconName = "room_config_summary"
+	sumaryIconNameDark           navigationItemIconName = "room_config_summary_dark"
 )
 
 var assistantNavigationIconByPage = map[mucRoomConfigPageID]navigationItemIconName{
@@ -26,6 +32,15 @@ var assistantNavigationIconByPage = map[mucRoomConfigPageID]navigationItemIconNa
 	roomConfigPositionsPageIndex:   positionsIconName,
 	roomConfigOthersPageIndex:      otherIconName,
 	roomConfigSummaryPageIndex:     sumaryIconName,
+}
+
+var assistantNavigationDarkIconByPage = map[mucRoomConfigPageID]navigationItemIconName{
+	roomConfigInformationPageIndex: basicInformationIconNameDark,
+	roomConfigAccessPageIndex:      accessIconNameDark,
+	roomConfigPermissionsPageIndex: permissionsIconNameDark,
+	roomConfigPositionsPageIndex:   positionsIconNameDark,
+	roomConfigOthersPageIndex:      otherIconNameDark,
+	roomConfigSummaryPageIndex:     sumaryIconNameDark,
 }
 
 type roomConfigAssistantNavigationItem struct {

576426679e1cc4da65f0144262b333b948754008
Ola Bini <ola@olabini.se>
2021-12-20 20:06:37 +0000

    No point in having the color sets be returned from functions. Make them global variables instead

--- gui/color_set.go
+++ gui/color_set.go
@@ -24,49 +24,45 @@ type colorSet struct {
 
 func (cm *hasColorManagement) currentColorSet() colorSet {
 	if cm.isDarkThemeVariant() {
-		return defaultDarkColorSet()
+		return defaultDarkColorSet
 	}
-	return defaultLightColorSet()
+	return defaultLightColorSet
 }
 
-func defaultLightColorSet() colorSet {
-	return colorSet{
-		rosterPeerBackground:                      rgbFromHex("#ffffff"),
-		rosterPeerOfflineForeground:               rgbFromHex("#aaaaaa"),
-		rosterPeerOnlineForeground:                rgbFromHex("#000000"),
-		rosterGroupBackground:                     rgbFromHex("#e9e7f3"),
-		rosterAccountOnlineBackground:             rgbFromHex("#918caa"),
-		rosterAccountOfflineBackground:            rgbFromHex("#d5d3de"),
-		conversationOutgoingUserForeground:        rgbFromHex("#3465a4"),
-		conversationIncomingUserForeground:        rgbFromHex("#a40000"),
-		conversationOutgoingTextForeground:        rgbFromHex("#000000"),
-		conversationIncomingTextForeground:        rgbFromHex("#000000"),
-		conversationStatusTextForeground:          rgbFromHex("#aaaaaa"),
-		conversationOutgoingDelayedUserForeground: rgbFromHex("#aaaaaa"),
-		conversationOutgoingDelayedTextForeground: rgbFromHex("#aaaaaa"),
-		conversationLockTypingBackground:          rgbFromHex("#e0e0e0"),
-		conversationUnlockTypingBackground:        rgbFromHex("#f9f9f9"),
-		timestampForeground:                       rgbFromHex("#aaaaaa"),
-	}
+var defaultLightColorSet = colorSet{
+	rosterPeerBackground:                      rgbFromHex("#ffffff"),
+	rosterPeerOfflineForeground:               rgbFromHex("#aaaaaa"),
+	rosterPeerOnlineForeground:                rgbFromHex("#000000"),
+	rosterGroupBackground:                     rgbFromHex("#e9e7f3"),
+	rosterAccountOnlineBackground:             rgbFromHex("#918caa"),
+	rosterAccountOfflineBackground:            rgbFromHex("#d5d3de"),
+	conversationOutgoingUserForeground:        rgbFromHex("#3465a4"),
+	conversationIncomingUserForeground:        rgbFromHex("#a40000"),
+	conversationOutgoingTextForeground:        rgbFromHex("#000000"),
+	conversationIncomingTextForeground:        rgbFromHex("#000000"),
+	conversationStatusTextForeground:          rgbFromHex("#aaaaaa"),
+	conversationOutgoingDelayedUserForeground: rgbFromHex("#aaaaaa"),
+	conversationOutgoingDelayedTextForeground: rgbFromHex("#aaaaaa"),
+	conversationLockTypingBackground:          rgbFromHex("#e0e0e0"),
+	conversationUnlockTypingBackground:        rgbFromHex("#f9f9f9"),
+	timestampForeground:                       rgbFromHex("#aaaaaa"),
 }
 
-func defaultDarkColorSet() colorSet {
-	return colorSet{
-		rosterPeerBackground:                      rgbFromHex("#7f7f7f"),
-		rosterPeerOfflineForeground:               rgbFromHex("#aaaaaa"),
-		rosterPeerOnlineForeground:                rgbFromHex("#e5e5e5"),
-		rosterGroupBackground:                     rgbFromHex("#b8b6bf"),
-		rosterAccountOnlineBackground:             rgbFromHex("#d5d3de"),
-		rosterAccountOfflineBackground:            rgbFromHex("#918caa"),
-		conversationOutgoingUserForeground:        rgbFromHex("#3465a4"),
-		conversationIncomingUserForeground:        rgbFromHex("#a40000"),
-		conversationOutgoingTextForeground:        rgbFromHex("#7f7f7f"),
-		conversationIncomingTextForeground:        rgbFromHex("#7f7f7f"),
-		conversationStatusTextForeground:          rgbFromHex("#4e9a06"),
-		conversationOutgoingDelayedUserForeground: rgbFromHex("#444444"),
-		conversationOutgoingDelayedTextForeground: rgbFromHex("#444444"),
-		conversationLockTypingBackground:          rgbFromHex("#e0e0e0"),
-		conversationUnlockTypingBackground:        rgbFromHex("#252a2c"),
-		timestampForeground:                       rgbFromHex("#444444"),
-	}
+var defaultDarkColorSet = colorSet{
+	rosterPeerBackground:                      rgbFromHex("#7f7f7f"),
+	rosterPeerOfflineForeground:               rgbFromHex("#aaaaaa"),
+	rosterPeerOnlineForeground:                rgbFromHex("#e5e5e5"),
+	rosterGroupBackground:                     rgbFromHex("#b8b6bf"),
+	rosterAccountOnlineBackground:             rgbFromHex("#d5d3de"),
+	rosterAccountOfflineBackground:            rgbFromHex("#918caa"),
+	conversationOutgoingUserForeground:        rgbFromHex("#3465a4"),
+	conversationIncomingUserForeground:        rgbFromHex("#a40000"),
+	conversationOutgoingTextForeground:        rgbFromHex("#7f7f7f"),
+	conversationIncomingTextForeground:        rgbFromHex("#7f7f7f"),
+	conversationStatusTextForeground:          rgbFromHex("#4e9a06"),
+	conversationOutgoingDelayedUserForeground: rgbFromHex("#444444"),
+	conversationOutgoingDelayedTextForeground: rgbFromHex("#444444"),
+	conversationLockTypingBackground:          rgbFromHex("#e0e0e0"),
+	conversationUnlockTypingBackground:        rgbFromHex("#252a2c"),
+	timestampForeground:                       rgbFromHex("#444444"),
 }

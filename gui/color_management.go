4dca91bada00e936fcc6df8c5a994bba14b3cc44
Ola Bini <ola@olabini.se>
2021-12-22 18:13:12 +0000

    Extract the actual calculation of theme variant in order to prepare for testing

--- gui/color_management.go
+++ gui/color_management.go
@@ -16,43 +16,43 @@ type hasColorManagement struct {
 const darkThemeVariantName = "dark"
 const lightThemeVariantName = "light"
 
-func (cm *hasColorManagement) getThemeVariant() string {
-	cm.calculateThemeVariant.Do(func() {
-		cm.themeVariant = lightThemeVariantName
-		gtkTheme := os.Getenv("GTK_THEME")
-		if gtkTheme != "" {
-			toks := strings.Split(gtkTheme, ":")
-			variant := toks[len(toks)-1:][0]
-			if variant == darkThemeVariantName {
-				cm.themeVariant = variant
-				return
-			}
-		}
-
-		settings, err := g.gtk.SettingsGetDefault()
-		if err != nil {
-			panic(err)
-		}
-
-		prefDark, _ := settings.GetProperty("gtk-application-prefer-dark-theme")
-		if val, ok := prefDark.(bool); val && ok {
-			cm.themeVariant = darkThemeVariantName
+func (cm *hasColorManagement) actuallyCalculateThemeVariant() {
+	cm.themeVariant = lightThemeVariantName
+	gtkTheme := os.Getenv("GTK_THEME")
+	if gtkTheme != "" {
+		toks := strings.Split(gtkTheme, ":")
+		variant := toks[len(toks)-1:][0]
+		if variant == darkThemeVariantName {
+			cm.themeVariant = variant
 			return
 		}
+	}
 
-		// TODO: we should do two things here
-		// - check the current theme name, and see if it ends with -dark or _dark - not just splitting on the ":" as above
+	settings, err := g.gtk.SettingsGetDefault()
+	if err != nil {
+		panic(err)
+	}
 
-		bgcd := newBackgroundColorDetectionInvisibleListBox()
-		styleContext, _ := bgcd.lb.GetStyleContext()
-		bc, _ := styleContext.GetProperty2("background-color", gtki.STATE_FLAG_NORMAL)
-		bgcd.lb.Destroy()
-		if rgbFromGetters(bc.(rgbaGetters)).isDark() {
-			cm.themeVariant = darkThemeVariantName
-		}
+	prefDark, _ := settings.GetProperty("gtk-application-prefer-dark-theme")
+	if val, ok := prefDark.(bool); val && ok {
+		cm.themeVariant = darkThemeVariantName
+		return
+	}
 
-	})
+	// TODO: we should do two things here
+	// - check the current theme name, and see if it ends with -dark or _dark - not just splitting on the ":" as above
 
+	bgcd := newBackgroundColorDetectionInvisibleListBox()
+	styleContext, _ := bgcd.lb.GetStyleContext()
+	bc, _ := styleContext.GetProperty2("background-color", gtki.STATE_FLAG_NORMAL)
+	bgcd.lb.Destroy()
+	if rgbFromGetters(bc.(rgbaGetters)).isDark() {
+		cm.themeVariant = darkThemeVariantName
+	}
+}
+
+func (cm *hasColorManagement) getThemeVariant() string {
+	cm.calculateThemeVariant.Do(cm.actuallyCalculateThemeVariant)
 	return cm.themeVariant
 }
 
